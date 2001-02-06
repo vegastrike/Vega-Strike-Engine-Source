e666894391ebf5b2573c8ede4424b474121314f8
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-02-06 14:10:15 +0000

    *** empty log message ***

--- src/gfx_bsp.h
+++ src/gfx_bsp.h
@@ -18,7 +18,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-ifndef GFX_BSP
+#ifndef GFX_BSP
 #define GFX_BSP
 
 //With all fairness, front should be outside and back should be inside
