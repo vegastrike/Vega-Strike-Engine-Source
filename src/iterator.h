244cf3dda9fbca5461c896ed6acf4189b045e2b0
ashieh <ashieh@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-02-18 13:53:45 +0000

    Initial rev of iterator class

---
+++ src/iterator.h
@@ -0,0 +1,13 @@
+#ifndef _ITERATOR_H_
+#define _ITERATOR_H_
+
+class Iterator {
+ public:
+  virtual void insert(Unit *unit) = 0;
+  virtual Unit *current() = 0;
+  virtual Unit *advance() = 0;
+};
+
+
+#endif
+
