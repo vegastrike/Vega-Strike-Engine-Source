7c697dc1284cd4eaaf60c963b23413b695e4fec0
ashieh <ashieh@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-03-31 05:33:35 +0000

    fixed typo

--- src/physics.h
+++ src/physics.h
@@ -24,7 +24,7 @@
 
 #include "gfx_transform_vector.h"
 
-#define SIMULATION_ATOM (1.0/10F)
+#define SIMULATION_ATOM (1.0/10.0)
 
 static const float oocc = (float)0.0000000000000000111265005605; //   1/c^2
 static const float c = (float)299792458.0;
