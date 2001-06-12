b2b21dd51ee1a220bbb53bf48e5bc947c1803857
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-06-12 09:35:09 +0000

    damn BUG

--- src/cmd_collide.cpp
+++ src/cmd_collide.cpp
@@ -153,6 +153,7 @@ void Unit::CollideAll() {
 	Position().j-radial_size<COLQ[i].Maxi.j&&
 	Position().k+radial_size>COLQ[i].Mini.k&&
 	Position().k-radial_size<COLQ[i].Maxi.k) {
+      //continue;
       switch (COLQ[i].type) {
       case LineCollide::UNIT://other units!!!
 	((Unit*)COLQ[i].object)->Collide(this);
