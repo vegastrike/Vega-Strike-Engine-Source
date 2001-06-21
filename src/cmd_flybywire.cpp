2f507ae7c297116f16c6b808ba8962742c6f08bf
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-06-21 06:41:52 +0000

    tupidity, utter stupidity

--- src/cmd_flybywire.cpp
+++ src/cmd_flybywire.cpp
@@ -6,5 +6,5 @@ FlyByWire::FlyByWire (float max_spd): Order(), max_speed(max_spd) {
 
 }
 AI * FlyByWire::Execute () {
-  
+  return NULL;
 } 
--- src/cmd_flybywire.h
+++ src/cmd_flybywire.h
@@ -1,7 +1,8 @@
+#include "cmd_order.h"
 class FlyByWire : public Order {
   float max_speed;
   
  public:
   FlyByWire (float max_spd);
   AI * Execute();
-}
+};
