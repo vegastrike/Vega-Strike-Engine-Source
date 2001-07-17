031a5b254c3915e12825377cc4aed43ef0f020dd
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-07-17 05:46:48 +0000

    autogenerate the environment maps

---
+++ src/gfx_env_map_gent.h
@@ -0,0 +1,5 @@
+#ifndef _GFX_ENV_MAP_GENT_H_
+#define _GFX_ENV_MAP_GENT_H_
+void EnvironmentMapGeneratorMain(const char * inpt, const char *outpt, float a=100, float m=1, float p=1, bool w=false);
+
+#endif
