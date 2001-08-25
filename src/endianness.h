b0cc5e6bc2c2b914399b00bc863121b752bd7a7f
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-08-25 07:37:27 +0000

    added BIG_ENDIAN check

--- src/endianness.h
+++ src/endianness.h
@@ -1,17 +1,16 @@
 #ifndef _ENDIANNESS_H
 
 #define _ENDIANNESS_H
+
 #include <endian.h>
 
-#if __BYTE_ORDER == __BIG_ENDIAN
-
-# include <byteswap.h>
-# define le32_to_cpu(x) (bswap_32(x))
-# define le16_to_cpu(x) (bswap_16(x))
-
-#else
-
-# define le32_to_cpu(x) (x)
-# define le16_to_cpu(x) (x)
+# if __BYTE_ORDER == __BIG_ENDIAN
+#  include <byteswap.h>
+#  define le32_to_cpu(x) (bswap_32(x))
+#  define le16_to_cpu(x) (bswap_16(x))
+# else
+#  define le32_to_cpu(x) (x)
+#  define le16_to_cpu(x) (x)
+# endif
 
 #endif	// _ENDIANNESS_H
