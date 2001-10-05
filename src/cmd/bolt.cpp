252dec3f0a4c3d3c8ca434b54178b6e7534f6507
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-10-05 05:52:08 +0000

    modified texture coords of weapon

--- src/cmd/bolt.cpp
+++ src/cmd/bolt.cpp
@@ -19,17 +19,21 @@ namespace bolt_draw {
   static vector <vector <Bolt *> > bolts;
   static vector <vector <Bolt *> > balls;
   static void CreateBoltMesh () {
-    GFXVertex vtx[8];
+    GFXVertex vtx[12];
     #define V(ii,xx,yy,zz,ss,tt) vtx[ii].x=xx;vtx[ii].y=yy;vtx[ii].z=zz;vtx[ii].i=0;vtx[ii].j=0;vtx[ii].k=1;vtx[ii].s=ss;vtx[ii].t=tt;
-    V(0,0,0,-.9,0,0);
-    V(1,0,-1,0,0,1);
-    V(2,0,0,.1,1,1);
-    V(3,0,1,0,1,0);
-    V(4,0,0,-.9,0,0);
-    V(5,-1,0,0,0,1);
-    V(6,0,0,.1,1,1);
-    V(7,1,0,0,1,0);
-    boltmesh = new GFXVertexList (GFXQUAD,8,vtx,NULL,false);//not mutable;
+    V(0,0,0,-.875,0,.5);
+    V(1,0,-1,0,.875,1);
+    V(2,0,0,.125,1,.5);
+    V(3,0,1,0,.875,0);
+    V(4,0,0,-.875,0,.5);
+    V(5,-1,0,0,.875,1);
+    V(6,0,0,.125,1,.5);
+    V(7,1,0,0,.875,0);
+    V(8,1,0,0,.1875,0);
+    V(9,0,1,0,.375,.1875);
+    V(10,-1,0,0,.1875,.375);
+    V(11,0,-1,0,0,.1875);
+    boltmesh = new GFXVertexList (GFXQUAD,12,vtx,NULL,false);//not mutable;
   }
 
 }
