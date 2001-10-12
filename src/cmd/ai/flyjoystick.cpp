45d7b53a5bd66f4856cf227e0e138c3fec2b431d
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-10-12 09:47:59 +0000

    new data

--- src/cmd/ai/flyjoystick.cpp
+++ src/cmd/ai/flyjoystick.cpp
@@ -30,31 +30,13 @@ void FlyByJoystick::Execute() {
   if (joystick[which_joystick]->isAvailable()) {
     JoyStick *joy=joystick[which_joystick];
 
-    //    joy->GetJoyStick(x,y,z,buttons);
-    //printf("flybyjoy x=%f y=%f\n",x,y);
+    //    joy->GetJoyStick(x,y,z,buttons);//don't want state updates in ai function
+    fprintf(stderr,"flybyjoy x=%f y=%f z=%f\n",joy->joy_x,joy->joy_y,joy->joy_z);
 
     Up(-joy->joy_y);   // pretty easy
     Right(-joy->joy_x);
     RollRight (-joy->joy_z);
-#if 0
-    for(int i=0;i<joystick[0]->NumButtons();i++){
-      if( ( (1<<i) & buttons )==(1<<i)){
-	printf("button %d pressed\n",i);
-	joyBindings[i](-1,DOWN);
-      }
-    }
-#endif
 
-    /*
-#if 1
-    for(int i=0;i<joystick[0]->NumButtons();i++){
-      if( ( (1<<i) & buttons )==(1<<i)){
-	printf("button %d pressed\n",i);
-	buttonState[i]=DOWN;
-      }
-    }
-#endif
-    */
 
   }
   FlyByKeyboard::Execute(false);
