9e1d1819bd0b45ae51f7886c0a7fe65d761ba342
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-10-20 08:31:17 +0000

    added some audio stuff

--- src/aldrv/al_init.cpp
+++ src/aldrv/al_init.cpp
@@ -1,2 +1,106 @@
 #include "audiolib.h"
+#include <AL/al.h>
+#include <AL/alc.h>
+#include <AL/alut.h>
 
+
+
+static void fixup_function_pointers(void) {
+  /* UNCOMMENT AS NEEDED
+	talcGetAudioChannel = (ALfloat (*)(ALuint channel))
+				GP("alcGetAudioChannel_LOKI");
+	talcSetAudioChannel = (void (*)(ALuint channel, ALfloat volume))
+				GP("alcSetAudioChannel_LOKI");
+
+	talMute   = (void (*)(ALvoid)) GP("alMute_LOKI");
+	talUnMute = (void (*)(ALvoid)) GP("alUnMute_LOKI");
+
+	talReverbScale = (void (*)(ALuint sid, ALfloat param))
+		GP("alReverbScale_LOKI");
+	talReverbDelay = (void (*)(ALuint sid, ALfloat param))
+		GP("alReverbDelay_LOKI");
+
+	talBombOnError = (void (*)(void))
+		GP("alBombOnError_LOKI");
+
+	if(talBombOnError == NULL) {
+		fprintf(stderr,
+			"Could not GetProcAddress alBombOnError_LOKI\n");
+		exit(1);
+	}
+	
+	talBufferi = (void (*)(ALuint, ALenum, ALint ))
+		GP("alBufferi_LOKI");
+
+	if(talBufferi == NULL) {
+		fprintf(stderr,
+			"Could not GetProcAddress alBufferi_LOKI\n");
+		exit(1);
+	}
+
+	alCaptureInit    = (ALboolean (*)( ALenum, ALuint, ALsizei )) GP("alCaptureInit_EXT");
+	alCaptureDestroy = (ALboolean (*)( ALvoid )) GP("alCaptureDestroy_EXT");
+	alCaptureStart   = (ALboolean (*)( ALvoid )) GP("alCaptureStart_EXT");
+	alCaptureStop    = (ALboolean (*)( ALvoid )) GP("alCaptureStop_EXT");
+	alCaptureGetData = (ALsizei (*)( ALvoid*, ALsizei, ALenum, ALuint )) GP("alCaptureGetData_EXT");
+
+	talBufferAppendData = (ALuint (*)(ALuint, ALenum, ALvoid *, ALint, ALint)) GP("alBufferAppendData_LOKI");
+	talBufferAppendWriteData = (ALuint (*)(ALuint, ALenum, ALvoid *, ALint, ALint, ALenum)) GP("alBufferAppendWriteData_LOKI");
+
+	talGenStreamingBuffers = (void (*)(ALsizei n, ALuint *bids )) GP("alGenStreamingBuffers_LOKI");
+	if( talGenStreamingBuffers == NULL ) {
+		fprintf( stderr, "Could not GP alGenStreamingBuffers_LOKI\n");
+		exit(1);
+	}
+	
+	talutLoadRAW_ADPCMData = (ALboolean (*)(ALuint bid,ALvoid *data, ALuint size, ALuint freq,ALenum format)) GP("alutLoadRAW_ADPCMData_LOKI");
+	if( talutLoadRAW_ADPCMData == NULL ) {
+		fprintf( stderr, "Could not GP alutLoadRAW_ADPCMData_LOKI\n");
+		exit(1);
+	}
+
+	talutLoadIMA_ADPCMData = (ALboolean (*)(ALuint bid,ALvoid *data, ALuint size, ALuint freq,ALenum format)) GP("alutLoadIMA_ADPCMData_LOKI");
+	if( talutLoadIMA_ADPCMData == NULL ) {
+		fprintf( stderr, "Could not GP alutLoadIMA_ADPCMData_LOKI\n");
+		exit(1);
+	}
+
+	talutLoadMS_ADPCMData = (ALboolean (*)(ALuint bid,ALvoid *data, ALuint size, ALuint freq,ALenum format)) GP("alutLoadMS_ADPCMData_LOKI");
+	if( talutLoadMS_ADPCMData == NULL ) {
+		fprintf( stderr, "Could not GP alutLoadMS_ADPCMData_LOKI\n");
+		exit(1);
+	}
+
+  */
+	return;
+}
+///I don't think we'll need to switch contexts or devices in vegastrike
+static ALCdevice *dev;
+static void *context_id;
+
+bool AUDInit () {
+
+	int attrlist[] = { ALC_FREQUENCY, 22050, 0 };
+
+	dev = alcOpenDevice( NULL );
+	if( dev == NULL ) {
+		return 1;
+	}
+
+	/* Initialize ALUT. */
+	context_id = alcCreateContext( dev, attrlist );
+	if(context_id == NULL) {
+		alcCloseDevice( dev );
+		return 1;
+	}
+
+	alcMakeContextCurrent( context_id );
+
+	fixup_function_pointers();
+}
+void AUDDestroy() {
+  //Go through and delete all loaded wavs
+  alcDestroyContext(context_id);
+  alcCloseDevice( dev );
+
+}
--- src/audiolib.h
+++ src/audiolib.h
@@ -1,8 +1,16 @@
 #ifndef _AUDIO_LIB_H_
 #define _AUDIO_LIB_H_
-
-
-
-
-
+#include "gfx/vec.h"
+bool AUDInit();
+void AUDDestroy();
+void AUDListener (const Vector &pos, const Vector &vel);
+void AUDListenerOrientation (const Vector & i, const Vector &j, const Vector &k);
+void AUDListenerGain (const float gain);
+int AUDCreateSound (const char *, const bool LOOP);
+void AUDAdjustSound (const Vector &pos, const Vector &vel);
+void AUDSoundGain (const int sound, const float gain);
+bool AUDIsPlaying (const int sound);
+void AUDStopPlaying (const int sound);
+void AUDStartPlaying (const int sound);
+void AUDPausePlaying (const int sound);
 #endif
