3929494ee88c0fdf6033616bed145403f03ea98d
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-10-21 00:41:12 +0000

    added some playonce functions

--- src/audiolib.h
+++ src/audiolib.h
@@ -7,9 +7,13 @@ void AUDListener (const Vector &pos, const Vector &vel);
 void AUDListenerOrientation (const Vector & i, const Vector &j, const Vector &k);
 void AUDListenerGain (const float gain);
 ///creates a buffer if one doesn't already exists, and then creates a source
-int AUDCreateSound (const char *, const bool LOOP);
-
+int AUDCreateSound (const char *, const bool LOOP=false);
+///copies other sound loaded through AUDCreateSound
+int AUDCreateSound (int sound,const bool LOOP=false);
+int AUDDeleteSound (int sound);
 void AUDAdjustSound (const Vector &pos, const Vector &vel);
+void AUDPlayOnce(int sound, const Vector &pos, const Vector &vel);
+void AUDPlayOnce (const char *sound, const Vector &pos, const Vector &vel);
 void AUDSoundGain (const int sound, const float gain);
 bool AUDIsPlaying (const int sound);
 void AUDStopPlaying (const int sound);
