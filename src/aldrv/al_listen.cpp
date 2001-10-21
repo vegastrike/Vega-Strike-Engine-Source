82f1f2012b351b43a060af620cacdf639944729d
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-10-21 03:04:36 +0000

    added ifdefs

--- src/aldrv/al_listen.cpp
+++ src/aldrv/al_listen.cpp
@@ -2,16 +2,22 @@
 #include <AL/al.h>
 #include <stdio.h>
 void AUDListener (const Vector & pos, const Vector & vel) {
+#ifdef HAVE_AL
   alListener3f (AL_POSITION, pos.i,pos.j,pos.k);
   alListener3f (AL_VELOCITY, vel.i,pos.j,pos.k);
   //  printf ("(%f,%f,%f) <%f %f %f>\n",pos.i,pos.j,pos.k,vel.i,vel.j,vel.k);
+#endif
 }
+
 void AUDListenerOrientation (const Vector & p, const Vector & q, const Vector & r) {
-  
+#ifdef HAVE_AL  
   ALfloat orient [] = {r.i,r.j,r.k,q.i,q.j,q.k};
   //  printf ("R%f,%f,%f>Q<%f %f %f>",r.i,r.j,r.k,q.i,q.j,q.k);
   alListenerfv (AL_ORIENTATION,orient);
+#endif
 }
 void AUDListenerGain (const float gain) {
+#ifdef HAVE_AL
   alListenerf (AL_GAIN,gain);
+#endif
 }
