571188911049943cfcf0fe1e4961e850b337f004
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-10-26 23:46:05 +0000

    added some documentation to script.h and poor documentation to event_xml.h

--- src/cmd/ai/event_xml.h
+++ src/cmd/ai/event_xml.h
@@ -4,12 +4,23 @@
 #include <string>
 #include <vector>
 #include <list>
-
+/**
+ * General namespace that does nothing on its own, but
+ * Deals with the parsing of an XML file tha tcontains a number of
+ * event "if" statements
+ * Each statement can have a minimum, max value and a "not" flag to invert it
+ * and the type references the enum in the class using this one, and 
+ * is eventually only used for the "not" tag
+ */
 namespace AIEvents {
+  ///A struct indicating an event that may or may not be executed
   struct AIEvresult {
-    int type;//will never be zero...negative indicates "not"
-    float max, min;//values that will cause this event
-	std::string script;
+    ///will never be zero...negative indicates "not"
+    int type;
+    ///The maximum/minimum values that will cause this event
+    float max, min;
+    ///The string indicating what type of thing this event evaluates
+    std::string script;
     AIEvresult (int type, float const min, const float max, const std::string &aiscript);
     bool Eval (const float eval) const {//return ((eval>=min)&&(eval<max)&&type>0)||(eval>=max&&eval<min&&type<0);
       if (eval>=min)
--- src/cmd/ai/script.h
+++ src/cmd/ai/script.h
@@ -3,22 +3,41 @@
 #include "order.h"
 #include "navigation.h"
 
+
+/**
+ * Loads a script from a given XML file
+ * FIXME: This data is not cached and is streamed
+ * from harddrive and expat'ed each time a script is loaded
+ */
 struct AIScriptXML;
 class AIScript : public Order {
+  ///File name the AI script takes, to be loaded upon first execute (needs ref to parent)
   char * filename;
+  ///Temporary data to hold while AI script loads
   AIScriptXML * xml;
+  ///Loads the XML file, filename when Execute() is called
   void LoadXML(); //load the xml
+  ///Internal functions to expat
   static void beginElement(void *userData, const XML_Char *name, const XML_Char **atts);
+  ///internal functions for use of expat
   static void endElement(void *userData, const XML_Char *name);
+  ///The top float on the current stack 
   float& topf();
+  ///Rid of the top float on the current stack
   void popf();
+  ///The top vector on the current stack
   Vector& topv();
+  ///Pop the top vector of teh current stack
   void popv ();
+  ///member function begin elements... deals with pushing vectors on stack
   void beginElement(const string &name, const AttributeList &attributes);
+  ///member function end elements...deals with calling AI scripts from the stack
   void endElement(const string &name);
 public:
+  ///saves scriptname in the filename var
   AIScript(const char * scriptname);
   ~AIScript();
+  ///Loads the AI script from the hard drive, or executes if loaded
   void Execute();
 };
 
