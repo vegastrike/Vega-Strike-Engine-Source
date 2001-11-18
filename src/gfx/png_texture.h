ba76af90c7c8fe52b8f94bfb1a46ebdd36a9d3d6
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-11-18 22:45:41 +0000

    added header file for png assistance library

---
+++ src/gfx/png_texture.h
@@ -0,0 +1,7 @@
+extern int PNG_HAS_PALETTE;
+extern int PNG_HAS_COLOR;
+extern int PNG_HAS_ALPHA;
+typedef unsigned char * (textureTransform) (int &bpp, int &color_type, unsigned int &width, unsigned int &height, unsigned char ** row_pointers);
+textureTransform heightmapTransform;
+textureTransform terrainTransform;
+unsigned char * readImage (const char * name, int &bpp, int &format, unsigned int &width, unsigned int &height, unsigned char *palette, textureTransform *tt);
