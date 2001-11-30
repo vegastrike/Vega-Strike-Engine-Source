31b2f0e2fed512f7823b7f1953b7aaaf14bbec18
stefanst <stefanst@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-11-30 11:49:59 +0000

    Added byte swapping macros for SGI systems (big endian)

--- src/endianness.h
+++ src/endianness.h
@@ -1,20 +1,36 @@
 #ifndef _ENDIANNESS_H
-
 #define _ENDIANNESS_H
-#ifndef _WIN32
-#include <endian.h>
+
+#if defined(IRIX)
+# include <sys/endian.h>
+
+#elif !defined(_WIN32)
+# include <endian.h>
 #else
-#define __BIG_ENDIAN 1
-#define __LITTLE_ENDIAN 0
-#define __BYTE_ORDER 0
+# define __BIG_ENDIAN 1
+# define __LITTLE_ENDIAN 0
+# define __BYTE_ORDER 0
 #endif
-# if __BYTE_ORDER == __BIG_ENDIAN
-#  include <byteswap.h>
-#  define le32_to_cpu(x) (bswap_32(x))
-#  define le16_to_cpu(x) (bswap_16(x))
+
+#if defined(IRIX)
+# if BYTE_ORDER == BIG_ENDIAN	/* depends on MIPSEB or MIPSEL and SGIAPI */
+#  define le32_to_cpu(x) (((x) << 24) | \
+			 (((x) << 8) & 0x00FF0000) | \
+			 (((uint32_t)(x) >> 8) & 0x0000FF00) | \
+			 ((uint32_t)(x) >> 24))
+#  define le16_to_cpu(x) (((x&0xFF)<<8) | ((unsigned short)(x)>>8))
 # else
 #  define le32_to_cpu(x) (x)
 #  define le16_to_cpu(x) (x)
 # endif
+
+#elif __BYTE_ORDER == __BIG_ENDIAN
+# include <byteswap.h>
+# define le32_to_cpu(x) (bswap_32(x))
+# define le16_to_cpu(x) (bswap_16(x))
+#else
+# define le32_to_cpu(x) (x)
+# define le16_to_cpu(x) (x)
+#endif
 
 #endif	// _ENDIANNESS_H
