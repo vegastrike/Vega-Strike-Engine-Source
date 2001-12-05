a87756d8b227e45b2bb6d67e1828db4a6d1d3f90
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-05 13:26:15 +0000

    added fox

---
+++ src/gldrv/gl_fog.cpp
@@ -0,0 +1,35 @@
+#include "gfxlib.h"
+
+
+void GFXFogMode (const FOGMODE fog) {
+  switch (fog) {
+  case FOG_OFF:
+    glDisable (GL_FOG);
+    break;
+  case FOG_EXP:
+    glEnable(GL_FOG);
+    glFogi (GL_FOG_MODE,GL_EXP);
+    break;
+  case FOG_EXP2:
+    glEnable (GL_FOG);
+    glFogi (GL_FOG_MODE,GL_EXP2);
+    break;
+  case FOG_LINEAR:
+    glEnable (GL_FOG);
+    glFogi (GL_FOG_MODE,GL_LINEAR);
+    break;
+  }
+}
+void GFXFogDensity (const float fogdensity) {
+  glFogf (GL_FOG_DENSITY,fogdensity); 
+}
+void GFXFogLimits (const float fognear, const float fogfar) {
+  glFogf (GL_FOG_START,fognear); 
+  glFogf (GL_FOG_END,fogfar); 
+}
+void GFXFogColor (GFXColor c) {
+  glFogfv (GL_FOG_COLOR, &c.r);
+}
+void GFXFogIndex (const int index){
+  glFogi (GL_FOG_INDEX, index);
+}
