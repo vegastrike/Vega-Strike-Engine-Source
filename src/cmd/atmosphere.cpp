d2c0072725669de1dc766081fbe7ac46a100ce1a
ashieh <ashieh@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-05 08:58:04 +0000

    initial version

---
+++ src/cmd/atmosphere.cpp
@@ -0,0 +1,157 @@
+#include "atmosphere.h"
+#include "vegastrike.h"
+#include "star_system.h"
+#include "gfx/sphere.h"
+#include "gfx/matrix.h"
+#include "cmd/unit.h"
+#include "cmd/planet.h"
+#include "gfxlib.h"
+
+int divisions = 16;
+int l0,l1,l2;
+
+void setArray(float c0[4], const GFXColor&c1) {
+	c0[0]=c1.r;
+	c0[1]=c1.g;
+	c0[2]=c1.b;
+	c0[3]=c1.a;
+}
+void setArray1(float c0[3], const GFXColor&c1) {
+	c0[0]=c1.r;
+	c0[1]=c1.g;
+	c0[2]=c1.b;
+}
+
+Atmosphere::Atmosphere(const Parameters &params) : user_params(params) {
+	dome = new SphereMesh(params.radius, divisions, divisions, "white.bmp", 
+					NULL,true,ONE,ZERO,false,
+					0,M_PI);
+}
+
+Atmosphere::~Atmosphere() {
+	int a;
+
+	for(a=0; a<sunboxes.size(); a++) {
+		delete sunboxes[a];
+	}
+}
+
+const Atmosphere::Parameters &Atmosphere::parameters()
+{
+	return user_params;
+}
+
+void Atmosphere::SetParameters(const Parameters &params)
+{
+	user_params = params;
+}
+
+void Atmosphere::Update(const Vector &position, const Matrix tmatrix)
+{
+	int a;
+	Planet *currPlanet;
+	StarSystem *system = _Universe->activeStarSystem();
+
+	for(a=0; a<sunboxes.size(); a++) {
+		delete sunboxes[a];
+	}
+	sunboxes.clear();
+	Vector localDir;
+	float rho1;
+	for(a=0;a<system->numprimaries;a++) {
+		if(system->primaries[a]->isUnit()==PLANETPTR && 
+			(currPlanet = (Planet*)system->primaries[a])->hasLights()) {
+			const std::vector <int> & lights = currPlanet->activeLights();
+			/* for now just assume all planets with lights are really bright */
+			Vector direction = currPlanet->Position()-position;
+			float rho = direction * TransformNormal(tmatrix,Vector(0,1,0));
+			if(rho > 0) { /* above the horizon */
+				Vector localDirection = InvTransformNormal(tmatrix,direction);
+				
+				
+				localDir = localDirection; /* bad */
+				rho1=rho;
+
+				/* need a function for the sunbox size. for now, say it takes up a quarter
+				   of the screen */
+				/* drop the z value and find the theta */
+				Vector lprime = localDirection;
+				lprime.k = 0;
+				lprime.Normalize();
+				float theta = atan2(lprime.i,lprime.j);
+				float size = .125;
+				sunboxes.push_back(new SunBox(NULL /*
+					new SphereMesh(user_params.radius, divisions, divisions, "", 
+					NULL,true,ONE,ZERO,false,
+					rho-size,rho+size,theta-size,theta+size)*/));
+				break;
+			}
+		}
+	}
+	if(!sunboxes.empty()) {
+		float rho=acos(rho1)/(PI/2)-0.1;
+		float radius = user_params.radius;
+		/* index 0 is the top color, index 1 is the bottom color */
+		GFXLight light0 = GFXLight();
+		setArray(light0.ambient, rho*user_params.high_ambient_color[0] + (1-rho)*user_params.low_ambient_color[0]);
+		setArray(light0.diffuse, rho*user_params.high_color[0] + (1-rho)*user_params.low_color[0]);
+		setArray(light0.attenuate, GFXColor(0,0.5,0));
+		setArray(light0.vect, GFXColor(0,1.1*radius,0,1));
+
+		/* do a linear interpolation between this and the next one */
+		
+		GFXLight light1 = GFXLight();
+		setArray(light1.ambient, (1-rho)*user_params.high_ambient_color[1] + rho*user_params.low_ambient_color[1]);
+		setArray(light1.diffuse, (1-rho)*user_params.high_color[1] + rho*user_params.low_color[1]);
+		setArray(light1.attenuate, GFXColor(0,0,0.5));
+		setArray(light1.vect, GFXColor(0,-1.1*radius,0,1));
+
+		/* Note!! make sure that this light never goes too far around the sphere */
+		GFXLight light2 = light1; /* -80 degree declination from sun position */
+		Matrix m;
+		Vector r;
+		ScaledCrossProduct(Vector(0,1,0),localDir,r);
+		Rotate(m,r,-80*(PI/180));
+		r = Transform(m,Vector(0,0,1));
+		float sradius = 1.1 * radius;
+		setArray(light2.vect, GFXColor(sradius * r.i,sradius * r.j,sradius * r.k,1));
+
+		GFXCreateLight(l0,light0,true);
+		GFXCreateLight(l1,light1,true);
+		GFXCreateLight(l2,light2,true);
+	}
+}
+
+void Atmosphere::Draw(const Vector &position, const Matrix tmatrix)
+{
+	Update(position,tmatrix);
+	GFXLoadMatrix(MODEL,tmatrix);
+
+	/*
+[ 1 0 0 0 ]
+[ 0 0 1 0 ]
+[ 0 -1 0 0 ]
+*/
+	Matrix rot = { 1, 0, 0, 0,
+		0, 0, -1, 0,
+		0, 1, 0, 0,
+		0, 0, 0, 1};
+	GFXMultMatrix(MODEL,rot);
+	GFXMaterial a = {0,0,0,0,
+					1,1,1,1,
+					0,0,0,0,
+					0,0,0,0,
+					0};
+	GFXDisable(DEPTHWRITE);
+	dome->DrawNow(10,TRUE,identity_transformation,tmatrix);
+	GFXEnable(DEPTHWRITE);
+
+	GFXDeleteLight(l0);
+	GFXDeleteLight(l1);
+	GFXDeleteLight(l2);
+}
+
+void Atmosphere::DrawAtmospheres()
+{
+	abort();
+}
--- src/cmd/atmosphere.h
+++ src/cmd/atmosphere.h
@@ -1,4 +1,52 @@
+#include <vector>
+#include "gfxlib.h"
 
-class Atmosphere{
+class Mesh;
+class Atmosphere {
+public:
+	struct Parameters {
+	/* High level parameters */
+	/* coefficients for capturing scattering effects of the 
+	   atmospheric composition/density */
+	float radius;
+	GFXColor low_color[2]; /* low angle */
+	GFXColor low_ambient_color[2]; /* low angle */
+	GFXColor high_color[2]; /* high angle */
+	GFXColor high_ambient_color[2]; /* high angle */
+	float scattering; /* "scattering" coefficient, basically 
+	                 how much the low_color spreads around */
+	Parameters() { };
+	};
+private:
+	Parameters user_params;
 
+	struct SunBox {
+		Mesh *sunbox;
+		SunBox(Mesh *m) : sunbox(m) { }
+		~SunBox() { delete sunbox; }
+	};
+
+	/* Internal state information */
+	Mesh *dome; /* entire sky; for texture mapping */
+	/* 3 rendering passes, to preserve framerate */
+	Mesh *cap; /* mid to zenith */
+	Mesh *stack; /* horizon to zenith */
+	std::vector <SunBox *> sunboxes; /* box around the sun (single star for now) */
+
+	/* lighting contexts for the above 3 */
+public:
+	Atmosphere(const Parameters &params);
+	~Atmosphere();
+	const Parameters &parameters();
+	void SetParameters(const Parameters &params);
+
+	/* Update the precomputed mesh to the new parameters. This is 
+	   expensive, so let the upper layers decide when to do this
+	*/
+
+	/* ugh, make update just change the lighting state */
+	void Update(const Vector &position, const Matrix tmatrix);
+	void Draw(const Vector &position, const Matrix tmatrix); /* inherit the orientation of 
+	                                    the transformation matrix  */
+	static void DrawAtmospheres();
 };
