6325a366e2672a0ca963f666faf39dd1d453d02a
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2001-12-20 23:40:50 +0000

    made it so inline doesn't break things

--- src/gfx/camera.cpp
+++ src/gfx/camera.cpp
@@ -19,6 +19,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 #include "camera.h"
+#include "unit.h" ///for GetUnit ();
 #include "matrix.h"
 
 //Remove GL specific stuff here
