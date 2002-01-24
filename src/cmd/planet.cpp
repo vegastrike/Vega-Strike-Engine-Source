9367e4f437a97925bfbbfebe22fd09d77d33002d
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-01-24 12:34:10 +0000

    blarh

--- src/cmd/planet.cpp
+++ src/cmd/planet.cpp
@@ -277,8 +277,11 @@ void Planet::reactToCollision(Unit * un, const Vector & biglocation, const Vecto
     un->DeactivateJumpDrive();
     _Universe->activeStarSystem()->JumpTo (un, std::string(destination[un->JumpDrive()%destination.size()]));
   }
+#if 0
+  //screws with earth having an atmosphere... blahrgh
   if (!terrain&&destination.empty()) {//no place to go and acts like a ship
     Unit::reactToCollision (un,biglocation,bignormal,smalllocation,smallnormal,dist);
+#endif
   }
   //nothing happens...you fail to do anythign :-)
   //maybe air reisstance here? or swithc dynamics to atmos mode
