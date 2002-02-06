67152f99ffd7614ea22214a13971d783a8c1b971
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-02-06 04:18:54 +0000

    yay asteroidgen works for tetrahedron

--- objconv/asteroidgen.cpp
+++ objconv/asteroidgen.cpp
@@ -23,11 +23,11 @@ public:
     this->s=s;
     this->t=t;
   }
-
+  float Mag () {return sqrtf(i*i+j*j+k*k);}
   void Yaw(float rad) //only works with unit vector
   {
     float theta;
-    
+    float m = Mag();
     if (i>0)
       theta = (float)atan(k/i);
     else if (i<0)
@@ -38,14 +38,14 @@ public:
 	  theta = M_PI/2;
     
     theta += rad;
-    i = cosf(theta);
-    k = sinf(theta); 
+    i = m*cosf(theta);
+    k = m*sinf(theta); 
   }
   
   void Roll(float rad)
   {
     float theta;
-    
+    float m = Mag();
     if (i>0)
       theta = (float)atan(j/i);
     else if (i<0)
@@ -56,14 +56,14 @@ public:
       theta = M_PI/2;
     
     theta += rad; 
-    i = cosf(theta);
-    j = sinf(theta); 
+    i = m*cosf(theta);
+    j = m*sinf(theta); 
   }
   
   void Pitch(float rad)
   {
     float theta;
-    
+    float m = Mag();
     if (k>0)
       theta = (float)atan(j/k);
     else if (k<0)
@@ -74,8 +74,8 @@ public:
       theta = M_PI/2;
     
     theta += rad;
-    k = cosf(theta);
-    j = sinf(theta);
+    k = m*cosf(theta);
+    j = m*sinf(theta);
   }
 };
 class Tri {
@@ -89,20 +89,20 @@ public:
   float sc,tc;
   int d;
   float sd, td;
-  Tri (int x, int y, int z) {a=x;b=y;c=z; quad=false;}
-  Tri (int x, int y, int z, int w) {a=x;b=y;c=z;d=w;}
+  Tri (int x, int y, int z) {c=x;b=y;a=z; quad=false;}
+  Tri (int x, int y, int z, int w) {d=x;c=y;b=z;a=w;}
   void Write (FILE * fp) {
     if (!quad)
       fprintf (fp,"<Tri>\n");
     else
       fprintf (fp,"<Quad>\n");
-    fprintf (fp,"<Vertex point=\"%d\" s=\"%f\" s=\"%f\"/>\n",a,sa,ta);
-    fprintf (fp,"<Vertex point=\"%d\" s=\"%f\" s=\"%f\"/>\n",b,sb,tb);
-    fprintf (fp,"<Vertex point=\"%d\" s=\"%f\" s=\"%f\"/>\n",c,sc,tc);
+    fprintf (fp,"<Vertex point=\"%d\" s=\"%f\" t=\"%f\"/>\n",a,sa,ta);
+    fprintf (fp,"<Vertex point=\"%d\" s=\"%f\" t=\"%f\"/>\n",b,sb,tb);
+    fprintf (fp,"<Vertex point=\"%d\" s=\"%f\" t=\"%f\"/>\n",c,sc,tc);
     if (!quad) {
       fprintf (fp,"</Tri>\n");
     }else {
-      fprintf (fp,"<Vertex point=\"%d\" s=\"%f\" s=\"%f\"/>\n",d,sd,td);
+      fprintf (fp,"<Vertex point=\"%d\" s=\"%f\" t=\"%f\"/>\n",d,sd,td);
       fprintf (fp,"</Quad>\n");
     }
   }
@@ -114,6 +114,9 @@ struct asteroid {
   int num_polys;
   vector <Vector> points;
   vector <Tri> polygon;
+  asteroid (): center (0,0,0),YawPitchRoll(0,0,0){
+
+  }
   void CenterAndRotate () {
     for (unsigned int i=0;i<points.size();i++) {
       points[i].Pitch (YawPitchRoll.i);
@@ -127,7 +130,7 @@ struct asteroid {
   }
 };
 
-char texture [100] ="asteroid";
+char texture [100] ="Asteroid.bmp";
 float scale=1;
 void determine_centers_and_radii (vector <asteroid> & field, const Vector &cube_sides, const float radiusmin, const float radiusmax, const int poly_min, const int poly_max) {
   for (unsigned int i=0;i<field.size();i++) {
@@ -143,7 +146,17 @@ void determine_centers_and_radii (vector <asteroid> & field, const Vector &cube_
     field[i].YawPitchRoll.i = 2*M_PI*((float)rand())/RAND_MAX;
     field[i].YawPitchRoll.j = 2*M_PI*((float)rand())/RAND_MAX;
     field[i].YawPitchRoll.k = 2*M_PI*((float)rand())/RAND_MAX;
-
+    bool insideanother=false;
+    for (unsigned int j=0;j<i;j++) {
+      if (fabs (field[j].center.i-field[i].center.i)<2*(field[j].radius+field[i].radius)&&
+	  fabs (field[j].center.j-field[i].center.j)<2*(field[j].radius+field[i].radius)&&
+	  fabs (field[j].center.j-field[i].center.j)<2*(field[j].radius+field[i].radius)) {
+	insideanother =true;
+	break;
+      }
+    }
+    if (insideanother)
+      i--;
   }
 }
 
@@ -168,7 +181,7 @@ void generateTet (vector <Vector> &v, vector <Tri> & p, const float minr, const 
   r = rD;
   v.push_back (Vector (0,r,h*r,0,1));
   p.push_back (Tri(0,5,1));//AFB
-  p.push_back (Tri(3,4,2));//DEC
+  p.push_back (Tri(3,2,4));//DCE
   p.push_back (Tri(4,2,1));//ECB
   p.push_back (Tri(1,5,4));//BFE
 }
@@ -225,21 +238,15 @@ void createShapes (asteroid & a, float dev) {
     generateNTet (a.points,a.polygon, a.radius*(1-dev),a.radius*(1+dev),a.num_polys/12,a.num_polys%12+6);
   }
 }
-void createShapes (vector <asteroid> &field, float deviation) {
-  for (unsigned int i=0;i<field.size();i++) {
-    createShapes (field[i],deviation);
-    field[i].CenterAndRotate();
-  }
-}
 
 void write_mesh (FILE * fp, vector <asteroid> &field) {
-  fprintf (fp,"<MESH texture=\"%s\" scale=\"%d\">\n<Points>\n",texture, scale);
+  fprintf (fp,"<Mesh texture=\"%s\" sharevertex=\"1\" scale=\"%f\">\n<Points>\n",texture, scale);
   unsigned int i;
   unsigned int counter=0;
   for (i=0;i<field.size();i++) {
     unsigned int j;
     for (j=0;j<field[i].points.size();j++) {
-      fprintf (fp,"<Point>\n\t<Location x=\"%f\" y=\"%f\" z=\"%f\"/>\n</Point>\n",field[i].points[i].i,field[i].points[i].j,field[i].points[i].k);
+      fprintf (fp,"<Point>\n\t<Location x=\"%f\" y=\"%f\" z=\"%f\" s=\"%f\" t=\"%f\"/>\n</Point>\n",field[i].points[j].i,field[i].points[j].j,field[i].points[j].k,field[i].points[j].s,field[i].points[j].t);
     }
     for (j=0;j<field[i].polygon.size();j++) {
       field[i].polygon[j].sa=field[i].points[field[i].polygon[j].a].s;
@@ -248,13 +255,13 @@ void write_mesh (FILE * fp, vector <asteroid> &field) {
       field[i].polygon[j].sb=field[i].points[field[i].polygon[j].b].s;
       field[i].polygon[j].tb=field[i].points[field[i].polygon[j].b].t;
       field[i].polygon[j].b+=counter;
-      field[i].polygon[j].c+=counter;
       field[i].polygon[j].sc=field[i].points[field[i].polygon[j].c].s;
       field[i].polygon[j].tc=field[i].points[field[i].polygon[j].c].t;
+      field[i].polygon[j].c+=counter;
       if (field[i].polygon[j].quad) {
-	      field[i].polygon[j].d+=counter;
 	      field[i].polygon[j].sd=field[i].points[field[i].polygon[j].d].s;
 	      field[i].polygon[j].td=field[i].points[field[i].polygon[j].d].t;
+	      field[i].polygon[j].d+=counter;
       }
     }
     counter+=field[i].points.size();
@@ -266,14 +273,61 @@ void write_mesh (FILE * fp, vector <asteroid> &field) {
       field[i].polygon[j].Write (fp);
     }
   }
-  fprintf (fp,"</Polygons>\n<Material reflect=\"%d\">\n<Specular red=\"%f\" green=\"%f\" blue=\"%f\" alpha=\"%f\"/>\n</Material>\n</Mesh>",0,0,0,0,1);
+  fprintf (fp,"</Polygons>\n<Material reflect=\"%d\">\n<Specular red=\"%f\" green=\"%f\" blue=\"%f\" alpha=\"%f\"/>\n</Material>\n</Mesh>\n",0,0,0,0,1);
 }
 
 
+void createShapes (vector <asteroid> &field, float deviation) {
+  for (unsigned int i=0;i<field.size();i++) {
+    createShapes (field[i],deviation);
+    field[i].CenterAndRotate();
+  }
+}
 
 
-int main () {
-  
+int main (int argc, char ** argv) {
+  vector <asteroid> field;
+  Vector cube_sides (1024,1024,1024);
+  float radiusmin,radiusmax;
+  int poly_min,poly_max;
+  float deviation;
+
+  char filename[16384];
+  int numroids;
+  if (argc>=9) {
+    sscanf (argv[1],"%s",filename);
+    sscanf (argv[2],"%f",&cube_sides.i);
+    cube_sides.j= cube_sides.k = cube_sides.i;
+    sscanf (argv[3],"%d", &numroids);
+    sscanf (argv[4],"%f",&radiusmin);
+    sscanf (argv[5],"%f",&radiusmax);
+    sscanf (argv[6],"%f",&deviation);
+    sscanf (argv[7],"%d",&poly_min);
+    sscanf (argv[8],"%d",&poly_max);
+
+  }else {
+    printf ("Enter Ouput File: ");
+
+    scanf ("%s",filename);
+    printf ("Enter Size of field?\n");
+    scanf ("%f",&cube_sides.i);
+    cube_sides.j= cube_sides.k = cube_sides.i;
+    printf ("Enter number of asteroids\n");
+    scanf ("%d", &numroids);
+    printf ("Enter asteroid minimum and max radius\n");
+    scanf ("%f %f",&radiusmin,&radiusmax);
+    printf ("Enter cragginess\n");  
+    scanf ("%f",&deviation);
+    printf ("Enter minimum number of polys and max num polys\n");
+    scanf ("%d %d",&poly_min,&poly_max);
+  }
+  for (int i=0;i<numroids;i++) {
+    field.push_back (asteroid());
+  }
+  determine_centers_and_radii (field, cube_sides,radiusmin, radiusmax, poly_min, poly_max);
+  createShapes(field,deviation);
+  FILE * fp= fopen (filename,"w");
+  write_mesh (fp,field);
+  fclose (fp);
   return 0;
-
 }
