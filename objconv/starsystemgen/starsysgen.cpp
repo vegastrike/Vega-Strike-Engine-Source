94285ce73f13270a7fc12552b95a711b26baa447
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-02-11 20:44:08 +0000

    added some text files

--- objconv/starsystemgen/starsysgen.cpp
+++ objconv/starsystemgen/starsysgen.cpp
@@ -1,13 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <string>
 
 using namespace std;
 
 
 
 
+const int PLANET = 2;
+const int GAS = 1;
+const int STAR = 0;
+const int MOON=3;
 
-vector <string> rocky_planets;
-
-
+vector <string> entities[4];
+vector <string> units [2];
+vector <string> background;
 
 
 void CreateStarSystem (int numsuns, int numplanets, float nummoons) {
@@ -16,9 +24,28 @@ void CreateStarSystem (int numsuns, int numplanets, float nummoons) {
 }
 
 
-int main (int argc, char ** argv) {
-  
+void readentity (vector <string> & entity,const char * filename) {
+  FILE * fp= fopen (filename,"r");
+  if (!fp) {
+    return;
+  }
+  ///warning... obvious vulnerability
+  char input_buffer[1000];
+  while (1==fscanf (fp,"%s", input_buffer)) {
+    entity.push_back (input_buffer);
+  }
+  fclose (fp);
+}
 
+int main (int argc, char ** argv) {
+  readentity (entities[0],"stars.txt");
+  readentity (entities[1],"planets.txt");
+  readentity (entities[2],"gas_giants.txt");
+  readentity (entities[3],"moons.txt");
+  readentity (units[0],"bigunits.txt");
+  readentity (units[1],"smallunits.txt");
+  readentity (background,"background.txt");
+  
 
 
 
