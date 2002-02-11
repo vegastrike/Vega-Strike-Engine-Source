b5fa831b202464e8bdb5f998efb68d0b76edbc48
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-02-11 19:53:54 +0000

    made own directory for multifile subprogram

---
+++ objconv/starsystemgen/starsysgen.cpp
@@ -0,0 +1,25 @@
+
+using namespace std;
+
+
+
+
+
+vector <string> rocky_planets;
+
+
+
+
+void CreateStarSystem (int numsuns, int numplanets, float nummoons) {
+
+
+}
+
+
+int main (int argc, char ** argv) {
+  
+
+
+
+
+}
