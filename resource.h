f0db81915df7cd426e85b6d13d6536f874482f18
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-02-18 21:15:06 +0000

    Added a better icon

--- resource.h
+++ resource.h
@@ -4,12 +4,13 @@
 //
 #define IDI_ICON1                       101
 #define IDD_START                       102
+#define IDI_ICON3                       105
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
