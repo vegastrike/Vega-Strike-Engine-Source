974895b68e1f35fff370aba65a77f8537c7266e1
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-02-28 12:41:45 +0000

    added cargo file

---
+++ src/cmd/unit_cargo.cpp
@@ -0,0 +1,103 @@
+#include "unit.h"
+#include "images.h"
+#include "xml_serializer.h"
+#include <algorithm>
+#include "vs_globals.h"
+#include "config_xml.h"
+using XMLSupport::tostring;
+std::string CargoToString (const Cargo& cargo) {
+  return tostring(cargo.mass)+string("\" price=\"") +tostring(cargo.price)+ string("\" volume=\"")+tostring(cargo.volume)+string("\" quantity=\"")+tostring(cargo.quantity)+string("\" file=\"")+cargo.content;
+}
+
+std::string Unit::cargoSerializer (const XMLType &input, void * mythis) {
+  Unit * un= (Unit *)mythis;
+  if (un->image->cargo.size()==0) {
+    return string("0");
+  }
+  std::sort (un->image->cargo.begin(),un->image->cargo.end());
+  for (unsigned int i=0;i+1<un->image->cargo.size();i++) {
+    if (un->image->cargo[i].content==un->image->cargo[i+1].content) {
+      float tmpmass = un->image->cargo[i].quantity*un->image->cargo[i].mass+un->image->cargo[i+1].quantity*un->image->cargo[i+1].mass;
+      float tmpvolume = un->image->cargo[i].quantity*un->image->cargo[i].volume+un->image->cargo[i+1].quantity*un->image->cargo[i+1].volume;
+      un->image->cargo[i].quantity+=un->image->cargo[i+1].quantity;
+      tmpmass/=un->image->cargo[i].quantity;
+      tmpvolume/=un->image->cargo[i].quantity;
+      un->image->cargo[i].volume=tmpvolume;
+      un->image->cargo[i].mass=tmpmass;
+      un->image->cargo.erase(un->image->cargo.begin()+(i+1));//group up similar ones
+    }
+
+  }
+  string retval= CargoToString(un->image->cargo[0]);
+
+  for (unsigned int i=1;i<un->image->cargo.size();i++) {
+    retval+= string ("\"/><Cargo mass=\"")+CargoToString(un->image->cargo[i]);
+  }
+  return retval;
+}
+
+
+
+bool Unit::CanAddCargo (const Cargo &carg)const {
+  float total_volume=carg.quantity*carg.volume;
+  for (unsigned int i=0;i<image->cargo.size();i++) {
+    total_volume+=image->cargo[i].quantity*image->cargo[i].volume;
+  }
+  return (total_volume<=image->cargo_volume);
+}
+
+void Unit::AddCargo (const Cargo &carg) {
+  mass+=carg.quantity*carg.mass;
+  image->cargo.push_back (carg);
+}
+void Unit::RemoveCargo (unsigned int i) {
+  assert (i<image->cargo.size());
+  mass-=image->cargo[i].quantity*image->cargo[i].mass;
+  image->cargo.erase (image->cargo.begin()+i);
+}
+
+float Unit::PriceCargo (const std::string &s) {
+  Cargo tmp;
+  tmp.content=s;
+  vector <Cargo>::iterator mycargo = std::find (image->cargo.begin(),image->cargo.end(),tmp);
+  float price;
+  if (mycargo==image->cargo.end()) {
+    static float spacejunk=parse_float (vs_config->getVariable ("cargo","space_junk_price","10"));
+    price = spacejunk;
+  } else {
+    price = (*mycargo).price;
+  }
+  return price;
+}
+bool Unit::SellCargo (unsigned int i, float &creds, Cargo & carg, Unit *buyer){
+  if (i<0||i>=image->cargo.size()||!buyer->CanAddCargo(image->cargo[i])||mass<image->cargo[i].mass)
+    return false;
+  carg = image->cargo[i];
+  creds+=carg.quantity*buyer->PriceCargo (image->cargo[i].content);
+  RemoveCargo (i);
+  return true;
+}
+bool Unit::SellCargo (const std::string &s, float & creds, Cargo &carg, Unit *buyer){
+  Cargo tmp;
+  tmp.content=s;
+  vector <Cargo>::iterator mycargo = std::find (image->cargo.begin(),image->cargo.end(),tmp);
+  if (mycargo==image->cargo.end())
+    return false;
+
+  return SellCargo (mycargo-image->cargo.begin(),creds,carg,buyer);
+}
+unsigned int Unit::numCargo ()const {
+  return image->cargo.size();
+}
+Cargo& Unit::GetCargo (unsigned int i) {
+  return image->cargo[i];
+}
+bool Unit::BuyCargo (const Cargo &carg, float & creds){
+  if (!CanAddCargo(carg)||creds<carg.quantity*carg.price) {
+    return false;    
+  }
+  AddCargo (carg);
+  creds-=carg.quantity*carg.price;
+  mass+=carg.quantity*carg.mass;
+  return true;
+}
