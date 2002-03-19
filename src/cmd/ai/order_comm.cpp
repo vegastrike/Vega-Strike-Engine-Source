e7082f9b49060366f8cffe2bebafc20440aaafba
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-03-19 11:42:48 +0000

    added stubbed out functions

---
+++ src/cmd/ai/order_comm.cpp
@@ -0,0 +1,14 @@
+#include "order.h"
+#include "communication.h"
+
+void Order::AdjustRelationTo (Unit * un, float factor) {
+
+}
+
+void Order::Communicate (const CommunicationMessage &c) {
+  
+}
+void Order::ProcessCommunicationMessages() {
+
+
+}
