2a678352932aec4dd35dd47235461703dc5233d0
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-04-29 10:50:13 +0000

    added

---
+++ src/cmd/script/flightgroup.cpp
@@ -0,0 +1,74 @@
+#include "gfx/aux_texture.h"
+
+#include "mission.h"
+#include "flightgroup.h"
+#include <stdio.h>
+Flightgroup::Flightgroup ():pos(0,0,0) {//betterto have a flightgroup constructor
+  fprintf (stderr,"constructing FG 0x%x\n",this);
+  squadLogo=NULL;
+  orderlist=NULL;
+  nr_waves_left=nr_ships_left=nr_ships=flightgroup_nr=0;
+  leader_decision=-1;
+}
+
+void Flightgroup::Decrement(Unit * un) {
+  nr_ships--;
+  nr_ships_left--;
+  if (leader.GetUnit()==un) {
+    leader.SetUnit (NULL);
+  }
+}
+Flightgroup * Flightgroup::newFlightgroup (const std::string &name, 
+					   const std::string &type,
+					   const std::string &faction,
+					   const std::string & order,
+					   int num_ships,
+					   int num_waves,
+					   const std::string &logo_tex,
+					   const std::string &logo_alp,
+					   Mission * mis) {
+  Flightgroup *fg=mis->findFlightgroup (name,faction);
+  bool new_fg =false;
+  if (fg==NULL) {
+    new_fg=true;
+    fg =new Flightgroup;
+    fg->name=name;
+    fg->faction= faction;
+  }else {
+    new_fg = (fg->nr_waves_left==0&&fg->nr_ships_left==0);
+  }
+  if (!logo_tex.empty()) {
+    
+    if (logo_alp.empty()) {
+      fg->squadLogo=new Texture (logo_tex.c_str(),0,MIPMAP);
+    }else {
+      fg->squadLogo=new Texture (logo_tex.c_str(),logo_alp.c_str(),0,MIPMAP);
+    }
+  }
+
+
+  if (new_fg) {
+    fg->nr_waves_left=num_waves-1;
+    mission->AddFlightgroup (fg);
+  }
+  fg->ainame=order;
+  fg->type = type;
+  fg->nr_ships+=num_ships;
+  fg->nr_ships_left+=num_ships;
+  return fg;
+}
+Flightgroup::~Flightgroup() {
+  fprintf (stderr,"deleting FG 0x%x %s %s\n",this,name.c_str(),faction.c_str());
+  if (squadLogo){
+    delete squadLogo;
+  }
+
+}
+
+Flightgroup& Flightgroup::operator = (Flightgroup & other) {
+  printf ("warning: may not work properly");
+  if (squadLogo) {
+    squadLogo = other.squadLogo->Clone();
+  }
+  return other;
+}
---
+++ src/cmd/script/flightgroup.h
@@ -0,0 +1,55 @@
+#ifndef _FLIGHTGROUP_H_
+#define _FLIGHTGROUP_H_
+#include "cmd/container.h"
+#include <string>
+#include <vector>
+struct Texture;
+
+
+class Flightgroup {
+ public:
+  UnitContainer leader;//I'm on the leader
+  int leader_decision;//-1 if decision is made
+  Texture * squadLogo;//null if not there
+  std::string directive;//"a" for attack target "b" for attack at will "h" for help out "f" for form up.... missions can get this..and set this
+  std::string name; // flightgroup name
+  std::string type;
+  std::string ainame; // .agg.xml and .int.xml get appended to this
+  std::string faction;
+  int flightgroup_nr; // running fg number
+  int nr_ships; // total ships nr
+  Vector pos;
+  int nr_ships_left;
+  int nr_waves_left;
+
+  std::map<std::string,std::string> ordermap;
+  std::vector <class varInst *> *orderlist;
+  //removes a ship from the flightgroup below
+  void Decrement(Unit * trashed);
+  static Flightgroup * newFlightgroup (const std::string &name, 
+					const std::string &type,
+					const std::string &faction,
+					const std::string &order,
+					int num_ships,
+					int num_waves,
+					const std::string &texname,
+					const std::string &alphname,
+					Mission * mis);
+  Flightgroup ();
+  Flightgroup (Flightgroup & other) {
+    *this = other;
+  }
+  Flightgroup& operator =(Flightgroup &);
+  ~Flightgroup();
+};
+class CreateFlightgroup {
+ public:
+  Flightgroup * fg;
+  int terrain_nr;// which terrain to use. -1 for normal unit -2 for mission ter
+  enum {UNIT,VEHICLE, BUILDING} unittype;
+  int waves,nr_ships; // number of waves, number of ships per wave
+  float rot[3];
+  easyDomNode * domnode;
+};
+
+#endif
