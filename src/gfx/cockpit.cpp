20e861ddaf22b52ef25bdd6e449d0adf3571dbba
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-06-02 10:44:57 +0000

    fixed the gauges disappearing

--- src/gfx/cockpit.cpp
+++ src/gfx/cockpit.cpp
@@ -690,16 +690,6 @@ void Cockpit::RestoreGodliness() {
 }
 
 void Cockpit::InitStatic () {  
-  int i;
-  for (i=0;i<NUMGAUGES;i++) {
-    gauges[i]=NULL;
-  }
-  for (i=0;i<NUMGAUGES;i++) {
-    gauge_time[i]=0;
-  }
-  for (i=0;i<MAXVDUS;i++) {
-    vdu_time[i]=0;
-  }
   radar_time=0;
   cockpit_time=0;
 }
@@ -711,6 +701,17 @@ Cockpit::Cockpit (const char * file, Unit * parent,const std::string &pilot_name
     headtrans.push_back (Matrix());
     Identity(headtrans.back());
   }
+  int i;
+  for (i=0;i<NUMGAUGES;i++) {
+    gauges[i]=NULL;
+  }
+  for (i=0;i<NUMGAUGES;i++) {
+    gauge_time[i]=0;
+  }
+  for (i=0;i<MAXVDUS;i++) {
+    vdu_time[i]=0;
+  }
+
   InitStatic();
   mesh=NULL;
   ejecting=false;
