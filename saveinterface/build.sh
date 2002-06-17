c74cd9da2b5007d71df21be3c3e79c715896901a
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-06-17 09:09:45 +0000

    modified the build script

--- saveinterface/build.sh
+++ saveinterface/build.sh
@@ -1,1 +1,3 @@
-g++ -I/usr/lib/glib/include/ -I/usr/include/gtk-1.2 -I/usr/include/glib-1.2 -o/tmp/data/save saveinterface.cpp -lgtk -lglib -lgdk -g
+#!/bin/sh
+#thank you Thunderbird@#nvidia
+g++ -I/usr/lib/glib/include/ -I/usr/include/gtk-1.2 -I/usr/include/gtk-2.0 -I/usr/include/glib-1.2 -I/usr/include/glib-2.0 -olauncher saveinterface.cpp -lgtk -lglib -lgdk -g
