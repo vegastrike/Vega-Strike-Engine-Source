775751ad9870fa9ecb098308d78613854733868d
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-06-29 09:33:22 +0000

    heh

---
+++ src/common/common.h
@@ -0,0 +1,27 @@
+/***************************************************************************
+                          common.h  -  description
+                             -------------------
+    begin                : Wed Jun 26 2002
+    copyright            : (C) 2002 by jhunt
+    email                : jhunt@jaja
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/* This file is for common (as in shared between vegastrike, vssetup as vsconfig) stuff
+ * that is not import enought to warrant it's own file.
+ */
+
+#ifndef _WIN32
+
+// Returns where the data directory is. Returns the cwd if it can't find the data dir.
+// Note: When it returns it has already changed dir to where the data directory is
+string getdatadir();
+
+#endif
