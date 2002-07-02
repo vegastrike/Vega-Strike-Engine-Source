66f8ab6192b8ef641462e11f8123f0bcc8a5be24
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-07-02 06:20:22 +0000

    touched

--- src/python/python_class.h
+++ src/python/python_class.h
@@ -40,7 +40,6 @@ int main (int argc,char *argv[]) {
     boost::python::class_builder <SuperClass,PythonClass< SuperClass > > BaseClass (name,myclass); \
     BaseClass.def (boost::python::constructor<>()); \
     BaseClass.def (&SuperClass::Execute,"Execute",PythonClass< SuperClass >::default_Execute); \
-    BaseClass.def (&SuperClass::IncRef,"IncRef"); \
 }
 //    boost::python::class_builder <SuperClass> TempClass (name,"SuperClass"); 
 
@@ -55,11 +54,8 @@ template <class SuperClass> class PythonClass:public SuperClass {
   static PythonClass< SuperClass > * last_instance;
   PythonClass (PyObject * self_):SuperClass() {
     self = self_;
-    last_instance=this;
-  }
-  virtual void IncRef() {
     Py_XINCREF(self);
-    boost::python::callback<void>::call_method(self, "IncRef");
+    last_instance=this;
   }
   virtual void Execute () {
     boost::python::callback <void>::call_method (self,"Execute");
