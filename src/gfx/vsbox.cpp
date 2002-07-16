41d1668fe145b6b48b9eeaf02f71b6b042f2874d
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-07-16 22:27:43 +0000

    oope vsbox

--- src/gfx/vsbox.cpp
+++ src/gfx/vsbox.cpp
@@ -30,7 +30,7 @@ Box::Box(const Vector &corner1, const Vector &corner2) : corner_min(corner1), co
     }
   int a=0;
   GFXVertex *vertices = new GFXVertex[18];
-#define VERTEX(i,j,k) { vertices[a].x = i; vertices[a].y = j; vertices[a].z = k;vertices[a].i=i;vertices[a].j=j;vertices[a].k=k;vertices[a].s=0;vertices[a].t=0;a++;}
+#define VERTEX(ax,ay,az) { vertices[a].x = ax; vertices[a].y = ay; vertices[a].z = az;vertices[a].i=ax;vertices[a].j=ay;vertices[a].k=az;vertices[a].s=0;vertices[a].t=0;a++;}
 
   VERTEX(corner_max.i,corner_min.j,corner_max.k);
   VERTEX(corner_min.i,corner_min.j,corner_max.k);
