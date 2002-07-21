9065167570c351622d265e5cd22d9687e12c12d7
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-07-21 06:24:40 +0000

    ring.h mpi

--- src/gfx/ring.h
+++ src/gfx/ring.h
@@ -1,6 +1,7 @@
 #ifndef _RING_H_
 #define _RING_H_
 #include "mesh.h"
+#include "vegastrike.h"
 class RingMesh:public Mesh {
   virtual Mesh * AllocNewMeshesEachInSizeofMeshSpace (int num) {return new RingMesh[num];}
   virtual void InitRing (float iradius,float oradius, int slices,const char *texture, const QVector & r, const QVector & s, int texwrapx=1, int texwrapy=1,const BLENDFUNC a=ONE, const BLENDFUNC b=ZERO, bool envMap=false, float theta_min=0.0, float theta_max=2*M_PI, FILTER mipmap=MIPMAP);
