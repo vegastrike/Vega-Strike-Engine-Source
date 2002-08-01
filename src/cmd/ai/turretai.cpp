95d19cc1fb0f48f82c2455d5c222f4097e7512e2
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-08-01 10:53:55 +0000

    makes sure it's locked hte misisle

--- src/cmd/ai/turretai.cpp
+++ src/cmd/ai/turretai.cpp
@@ -13,7 +13,7 @@ void TurretAI::Execute () {
   Unit * targ = parent->Target();
   if (targ) {
     static float dot_cutoff = XMLSupport::parse_float (vs_config->getVariable ("AI","Firing","TurretDotCutoff",".4"));
-    static float missile_prob = XMLSupport::parse_float (vs_config->getVariable ("AI","Firing","MissileProbability",".01"));
+    static float missile_prob = XMLSupport::parse_float (vs_config->getVariable ("AI","Firing","TurretMissileProbability",".05"));
     FaceTarget::Execute();
     if (parent->GetNumMounts()>0) {
       if (range==-1) {
@@ -28,7 +28,11 @@ void TurretAI::Execute () {
       if (mag-targ->rSize()-parent->rSize()<1.2*range&&dot>dot_cutoff) {
 	parent->Fire(false);
 	if (missile_prob*SIMULATION_ATOM*RAND_MAX<rand()) {
-	  parent->Fire(true);
+	  int locked = parent->LockMissile();
+	  if (locked==-1) {
+	    parent->Fire(true);
+	    parent->ToggleWeapon(true);//change missiles to only fire 1
+	  }
 	}
       }else {
 	parent->UnFire();
