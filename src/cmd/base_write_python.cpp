ee27d6e859465ff2a5e41bedc6b5f31f673d1153
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-08-04 00:34:12 +0000

    added python base writer!

---
+++ src/cmd/base_write_python.cpp
@@ -0,0 +1,129 @@
+#include "base.h"
+#ifdef BASE_MAKER
+#ifndef BASE_XML
+#include <stdio.h>
+
+void Base::Room::Link::EndXML (FILE *fp) {
+	fprintf(fp,"room, '%s', %g, %g, %g, %g, '%s'",index,x,y,wid,hei,text);
+}
+
+void Base::Room::Goto::EndXML (FILE *fp) {
+	fprintf(fp,"Base.Link (");
+	Link::EndXML(fp);
+	fprintf(fp,", %d)\n", Goto::index);
+}
+
+void Base::Room::Python::EndXML (FILE *fp) {
+	fprintf(fp,"Base.Python (");
+	Link::EndXML(fp);
+	fprintf(fp,", '%s')\n",file);
+}
+
+void Base::Room::Talk::EndXML (FILE *fp) {
+	char randstr[100];
+	sprintf(randstr,"NEW_SCRIPT_%d.py",(int)(rand()));
+	fprintf(fp,"Base.Python (");
+	Link::EndXML(fp);
+	fprintf(fp,", '%s')\n",randstr);
+	FILE *py=fopen(randstr,"wt");
+	fprintf(py,"import Base\nimport VS\nimport rnadom\n\nrandnum=random.randrange(0,%d)\n",say.size());
+	for (int i=0;i<say.size();i++) {
+		fprintf(fp,"if (randnum==%d):\n",i);
+		for (int j=0;j<say[i].size();j++) {
+			if (say[i][j]=='\n') {
+				say[i][j]='\\';
+				static const char *ins="n";
+				say[i].insert(j,ins);
+			}
+		}
+		fprintf(fp,"  Base.Message ('%s')\n",say[i].c_str());
+		if (!(soundfiles[i].empty()))
+			fprintf(fp,"  VS.playSound ('%s', (0,0,0), (0,0,0))\n",soundfiles[i].c_str());
+	}
+	//obolete... creates a file that uses the Python function instead.
+}
+
+void Base::Room::Launch::EndXML (FILE *fp) {
+	fprintf(fp,"Base.Launch (");
+	Link::EndXML(fp);
+	fprintf(fp,")\n");
+}
+
+void Base::Room::Comp::EndXML (FILE *fp) {
+	fprintf(fp,"Base.Comp (");
+	Link::EndXML(fp);
+	fwrite(", '",3,1,fp);
+	for (int i=0;i<modes.size();i++) {
+		char *mode=NULL;
+		switch(modes[i]) {
+		case UpgradingInfo::NEWSMODE:
+			mode="NewsMode";
+			break;
+		case UpgradingInfo::SHIPDEALERMODE:
+			mode="ShipMode";
+			break;
+		case UpgradingInfo::UPGRADEMODE:
+			mode="UpgradeMode";
+			break;
+		case UpgradingInfo::DOWNGRADEMODE:
+			mode="DowngradeMode";
+			break;
+		case UpgradingInfo::BRIEFINGMODE:
+			mode="BriefingMode";
+			break;
+		case UpgradingInfo::MISSIONMODE:
+			mode="MissionMode";
+			break;
+		case UpgradingInfo::SELLMODE:
+			mode="SellMode";
+			break;
+		case UpgradingInfo::BUYMODE:
+			mode="BuyMode";
+			break;
+		}
+		if (mode)
+			fprintf(fp,"%s ",mode);
+		if ((i+1)==(modes.size()))
+			fprintf(fp,"'");
+	}
+	fprintf(fp,")\n");
+}
+
+void Base::Room::BaseObj::EndXML (FILE *fp) {
+//		Do nothing
+}
+
+void Base::Room::BaseShip::EndXML (FILE *fp) {
+	fprintf(fp,"Base.Ship (room, '%s', (%lg,%lg,%lg), (%g, %g, %g), (%g, %g, %g))\n",index.c_str()
+			,mat.p.i,mat.p.j,mat.p.k
+			,mat.getR().i,mat.getR().j,mat.getR().k
+			,mat.getQ().i,mat.getQ().j,mat.getQ().k);
+}
+
+void Base::Room::BaseSprite::EndXML (FILE *fp) {
+	float x,y;
+	spr.GetPosition(x,y);
+	fprintf(fp,"Base.Texture (room, '%s', '%s', %g, %g)\n",index.c_str(),texfile.c_str(),x,y);
+}
+
+void Base::Room::EndXML (FILE *fp) {
+	fprintf(fp,"room = Base.Room ('%s')\n",deftext.c_str());
+	int i;
+	for (i=0;i<links.size();i++) {
+		links[i]->EndXML(fp);
+	}
+	for (i=0;i<objs.size();i++) {
+		objs[i]->EndXML(fp);
+	}
+	fwrite("\n",2,1,fp);
+}
+
+void Base::EndXML (FILE *fp) {
+	fprintf(fp,"import base\n\n");
+	for (int i=0;i<rooms.size();i++) {
+		rooms[i]->EndXML(fp);
+	}
+}
+
+#endif
+#endif
