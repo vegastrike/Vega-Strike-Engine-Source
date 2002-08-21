9d988065d45943a4a57c6c784acafa79e8eedad8
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-08-21 05:49:18 +0000

    added better collision...now it's velocity mag

--- src/cmd/unit_physics.cpp
+++ src/cmd/unit_physics.cpp
@@ -670,8 +670,8 @@ void Unit::reactToCollision(Unit * smalle, const QVector & biglocation, const Ve
     smalle->ApplyForce (bignormal*.4*bouncepercent*smalle->GetMass()*fabs(bignormal.Dot (((smalle->GetVelocity()-this->GetVelocity())/SIMULATION_ATOM))+fabs (dist)/(SIMULATION_ATOM*SIMULATION_ATOM)));
     this->ApplyForce (smallnormal*.4*bouncepercent*(smalle->GetMass()*smalle->GetMass()/this->GetMass())*fabs(smallnormal.Dot ((smalle->GetVelocity()-this->GetVelocity()/SIMULATION_ATOM))+fabs (dist)/(SIMULATION_ATOM*SIMULATION_ATOM)));
     
-    smalle->ApplyDamage (biglocation.Cast(),bignormal,g_game.difficulty*(  .5*fabs(bignormal.Dot(smalle->GetVelocity()-this->GetVelocity()))*this->mass*SIMULATION_ATOM),smalle,GFXColor(1,1,1,2),NULL);
-    this->ApplyDamage (smalllocation.Cast(),smallnormal, g_game.difficulty*(.5*fabs(smallnormal.Dot(smalle->GetVelocity()-this->GetVelocity()))*smalle->mass*SIMULATION_ATOM),this,GFXColor(1,1,1,2),NULL);
+    smalle->ApplyDamage (biglocation.Cast(),bignormal,g_game.difficulty*(  .5*fabs((smalle->GetVelocity()-this->GetVelocity()).MagnitudeSquared())*this->mass*SIMULATION_ATOM),smalle,GFXColor(1,1,1,2),NULL);
+    this->ApplyDamage (smalllocation.Cast(),smallnormal, g_game.difficulty*(.5*fabs((smalle->GetVelocity()-this->GetVelocity()).MagnitudeSquared())*smalle->mass*SIMULATION_ATOM),this,GFXColor(1,1,1,2),NULL);
 #endif
     
   //each mesh with each mesh? naw that should be in one way collide
