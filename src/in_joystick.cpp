6211b2544695a410a41e57920eec981b99a42aef
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-09-13 17:38:05 +0000

    hehe

--- src/in_joystick.cpp
+++ src/in_joystick.cpp
@@ -37,6 +37,7 @@ JoyStick::JoyStick () {
   for (int j=0;j<MAX_AXES;++j) {
     axis_axis[j]=-1;
     axis_inverse[j]=false;
+    joy_axis[j]=axis_axis[j]=0;
   }
 }
 
@@ -92,6 +93,7 @@ JoyStick::JoyStick(int which): mouse(which==MOUSE_JOYSTICK) {
   for (int j=0;j<MAX_AXES;++j) {
     axis_axis[j]=-1;
     axis_inverse[j]=false;
+    joy_axis[j]=axis_axis[j]=0;
   }
   player=which;//by default bind players to whichever joystick it is
   debug_digital_hatswitch=XMLSupport::parse_bool(vs_config->getVariable("joystick","debug_digital_hatswitch","false"));
