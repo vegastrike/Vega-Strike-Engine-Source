882525b87cd51d070e68a46d21b98d86c3a08452
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-10-04 08:39:31 +0000

    unti will upgrade

--- src/cmd/unit_xml.cpp
+++ src/cmd/unit_xml.cpp
@@ -560,7 +560,7 @@ void GameUnit::beginElement(const string &name, const AttributeList &attributes)
       }
     }    
     GameUnit *upgradee =UnitFactory::createUnit(filename.c_str(),true,FactionUtil::GetFaction("upgrades"));
-	Upgrade (upgradee,soffset,moffset,GetModeFromName (filename.c_str()),true,percent,NULL);
+	Unit::Upgrade (upgradee,soffset,moffset,GetModeFromName (filename.c_str()),true,percent,NULL);
     upgradee->Kill();
     }
     break;
