88533ffb87da36291c3eccc31a8d07a9619e9112
surfdargent <surfdargent@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-10-09 09:57:18 +0000

    - split

---
+++ src/cmd/unit_util_generic.cpp
@@ -0,0 +1,227 @@
+#include <string>
+#include "cmd/unit_generic.h"
+#include "config_xml.h"
+#include "cmd/unit_util.h"
+#include "configxml.h"
+#include "vs_globals.h"
+using std::string;
+
+namespace UnitUtil {
+	string getFactionName (Unit *my_unit) {
+		if (!my_unit)return "";
+		return FactionUtil::GetFaction(my_unit->faction);
+    }
+	int getFactionIndex (Unit *my_unit) {
+		if (!my_unit)return 0;
+		return my_unit->faction;
+    }
+	void setFactionIndex (Unit *my_unit,int factionname) {
+		if (!my_unit)return;
+		my_unit->SetFaction(factionname);
+    }
+	void setFactionName (Unit *my_unit,string factionname) {
+		if (!my_unit)return;
+		my_unit->SetFaction(FactionUtil::GetFaction(factionname.c_str()));
+    }
+	string getName(Unit *my_unit){
+		if (!my_unit)return "";
+		return my_unit->name;
+	}
+	float getCredits(Unit *my_unit) {
+		if (!my_unit)return 0;
+		Cockpit * tmp;
+		float viret=0;
+		if ((tmp=_Universe.isPlayerStarship (my_unit))) {
+			viret=tmp->credits;
+		}
+		return viret;
+	}
+	void addCredits(Unit *my_unit,float credits) {
+		if (!my_unit)return;
+		Cockpit * tmp;
+		if ((tmp=_Universe.isPlayerStarship (my_unit))) {
+			tmp->credits+=credits;
+		}
+	}
+    string getFlightgroupName(Unit *my_unit){
+		if (!my_unit)return "";
+		string fgname;
+		Flightgroup *fg=my_unit->getFlightgroup();
+		if(fg){
+			fgname=fg->name;
+		}
+		return fgname;
+	}
+	Unit *getFlightgroupLeader (Unit *my_unit) {
+		if (!my_unit)return 0;
+		class Flightgroup * fg=my_unit->getFlightgroup();
+		Unit *ret_unit = fg?fg->leader.GetUnit():my_unit;
+		if (!ret_unit)
+			ret_unit=0;
+		return ret_unit;
+	}
+	bool setFlightgroupLeader (Unit *my_unit, Unit *un) {
+		if (!my_unit||!un)return false;		 
+		if (my_unit->getFlightgroup()) {
+			my_unit->getFlightgroup()->leader.SetUnit(un);
+			return true;
+		} else {
+			return false;
+		}
+	}
+	string getFgDirective(Unit *my_unit){
+		if (!my_unit)return "";
+		string fgdir ("b");
+		if (my_unit->getFlightgroup())
+			fgdir = my_unit->getFlightgroup()->directive;
+		return fgdir;
+	}
+	bool setFgDirective(Unit *my_unit,string inp){
+		if (!my_unit)return false;
+		if (my_unit->getFlightgroup()!=NULL) {
+			my_unit->getFlightgroup()->directive = inp;
+			return true;
+		}
+		return false;
+	}
+	int getFgSubnumber(Unit *my_unit){
+		if (!my_unit)return -1;
+		return my_unit->getFgSubnumber();
+	}
+	int removeCargo(Unit *my_unit,string s, int quantity, bool erasezero){
+		if (!my_unit)return 0;
+		unsigned int index;
+		if (my_unit->GetCargo(s,index)) {
+			quantity = my_unit->RemoveCargo (index,quantity,erasezero);
+		}else {
+			quantity=0;
+		}
+		return quantity;
+	}
+	float upgrade(Unit *my_unit, string file,int mountoffset,int subunitoffset, bool force,bool loop_through_mounts) {
+		if (!my_unit)return 0;
+		double percentage=0;
+		printf ("upgrading %s %s %d %d %s\n",my_unit->name.c_str(),file.c_str(),mountoffset, subunitoffset,loop_through_mounts?"true":"false");
+		fflush (stdout);
+		percentage=my_unit->Upgrade (file,mountoffset,subunitoffset,force,loop_through_mounts);
+		my_unit->SetTurretAI();
+		return percentage;
+	}
+	int addCargo (Unit *my_unit,Cargo carg) {
+	  if (!my_unit)return 0;
+	  int i;
+	  for (i=carg.quantity;i>0&&!my_unit->CanAddCargo(carg);i--) {
+	    carg.quantity=i;
+	  }
+	  if (i>0) {
+	    carg.quantity=i;
+	    my_unit->AddCargo(carg);
+	  }else {
+	    carg.quantity=0;
+	  }
+	  return carg.quantity; 
+	}
+	int hasCargo (Unit * my_unit, string mycarg) {
+	  if (!my_unit) return 0;
+		unsigned int i;
+		Cargo * c = my_unit->GetCargo (mycarg,i);
+		if (c==NULL)
+			return 0;
+		return c->quantity;
+	}
+	bool JumpTo (Unit * unit, string system) {
+		if (unit!=NULL)
+			return unit->getStarSystem()->JumpTo(unit,NULL,system);
+		else
+			return false;
+	}
+        string getUnitSystemFile (Unit * un) {
+	  if (!un) {
+	    return _Universe.activeStarSystem()->getFileName();
+	  }
+	  StarSystem * ss = un->getStarSystem();
+	  return ss->getFileName();
+        }
+	bool incrementCargo(Unit *my_unit,float percentagechange,int quantity){
+		if (!my_unit)return false;
+		if (my_unit->numCargo()>0) {
+			unsigned int index;
+			index = rand()%my_unit->numCargo();
+			Cargo c(my_unit->GetCargo(index));	  
+			c.quantity=quantity;
+			if (my_unit->CanAddCargo(c)) {
+				my_unit->AddCargo(c);
+				my_unit->GetCargo(index).price*=percentagechange;
+				return true;
+			}
+		}
+		return false;
+	}
+	bool decrementCargo(Unit *my_unit,float percentagechange){
+		if (!my_unit)return false;
+		if (my_unit->numCargo()>0) {
+			unsigned int index;
+			index = rand()%my_unit->numCargo();
+			if (my_unit->RemoveCargo(index,1,false)) {
+				my_unit->GetCargo(index).price*=percentagechange;
+			}
+			return true;
+		}
+		return false;
+	}
+
+
+  
+	Cargo GetCargoIndex (Unit *my_unit, int index) {
+	  if (my_unit) {
+	    if (index>=0&&index<my_unit->numCargo()) {
+	      return my_unit->GetCargo(index);
+	    }
+	  }
+	  Cargo ret;
+	  ret.quantity=0;
+	  return ret;
+	}
+
+	Cargo GetCargo (Unit *my_unit, std::string cargname) {
+		if (my_unit) {
+			unsigned int indx=0;
+			Cargo *cargptr=my_unit->GetCargo(cargname,indx);
+			if (cargptr&&indx>=0) {
+				return *cargptr;
+			}
+		}
+		Cargo ret;
+		ret.quantity=0;
+		return ret;
+	}
+	float getDistance(Unit *my_unit,Unit *un){
+	  if (my_unit==NULL||un==NULL)
+	    return FLT_MAX;
+	  return (my_unit->Position()-un->Position()).Magnitude()-my_unit->rSize()-un->rSize();
+	}
+	float getPlanetRadiusPercent () {
+		static float planet_rad_percent =  XMLSupport::parse_float (vs_config->getVariable ("physics","auto_pilot_planet_radius_percent",".75"));
+		return planet_rad_percent;
+	}
+	float getSignificantDistance (Unit *un, Unit *sig) {
+	  if (un==NULL||sig==NULL)
+	    return FLT_MAX;
+	  
+		float dist = getDistance (un,sig);
+		
+		float planetpct=getPlanetRadiusPercent();
+		if (sig->isPlanet ())
+			dist = dist - (sig->rSize()*planetpct);
+		if (un->isPlanet ())
+			dist = dist - (un->rSize()*planetpct);
+		return dist;
+	}
+        int isPlayerStarship (Unit * un) {
+                Cockpit * cp = _Universe.isPlayerStarship (un);
+                if (cp==NULL) {
+                        return -1;
+                }
+                return (cp - _Universe.AccessCockpit(0));
+        }
+}
---
+++ src/cmd/unit_util_server.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include "unit_generic.h"
+#include "config_xml.h"
+#include "unit_util.h"
+#include "configxml.h"
+#include "vs_globals.h"
+using std::string;
+
+namespace UnitUtil {
+	void switchFg(Unit *my_unit,string arg){
+		if (!my_unit)return;
+		string type= my_unit->name;
+		int nr_waves_left=0;
+		int nr_ships=1;
+		string order("default");
+		Flightgroup *fg = my_unit->getFlightgroup();
+		if (fg) {
+			type = fg->type;
+			nr_waves_left = fg->nr_waves_left;
+			nr_ships =1;
+			fg->Decrement(my_unit);
+			order = fg->ainame;
+		}
+		fg = Flightgroup::newFlightgroup (arg,type,FactionUtil::GetFaction(my_unit->faction),order,nr_ships,nr_waves_left,"","",mission);
+		my_unit->SetFg (fg,fg->nr_ships_left-1);
+	}
+	bool isSignificant(Unit *my_unit){ return false;}
+	bool isSun(Unit *my_unit){ return false;}
+	int communicateTo(Unit *my_unit,Unit *other_unit,float mood){return 0;}
+	bool commAnimation(Unit *my_unit,string anim){ return false;}
+}
