5470f36f31c599752e1f9c4a393815b00aaa05e8
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-10-21 05:43:47 +0000

    fixed detail unknown

--- src/gfx/halo_system.cpp
+++ src/gfx/halo_system.cpp
@@ -72,7 +72,8 @@ namespace CAR {
   enum ALL_TYPES {
     RUNNINGLIGHTS,HEADLIGHTS,LEFTBLINK,RIGHTBLINK,BRAKE,REVERSE,SIREN
   };
-  const EnumMap::Pair type_names[7] = {
+  const EnumMap::Pair type_names[8] = {
+    EnumMap::Pair("UNKNOWN",RUNNINGLIGHTS),
     EnumMap::Pair("RUNNINGLIGHTS",RUNNINGLIGHTS),
     EnumMap::Pair("HEADLIGHTS", HEADLIGHTS),
     EnumMap::Pair("LEFTBLINK",LEFTBLINK),
@@ -81,7 +82,7 @@ namespace CAR {
     EnumMap::Pair("REVERSE",REVERSE),
     EnumMap::Pair("SIREN",SIREN)
   };
-  const EnumMap type_map (type_names,7);
+  const EnumMap type_map (type_names,8);
 }
 
 unsigned int HaloSystem::AddHalo (const char * filename, const QVector & loc, const Vector &size, const GFXColor & col, std::string type) {
