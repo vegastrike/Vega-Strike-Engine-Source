436f746d7165c0e3d5550460e01b3b0a92b5e718
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-10-22 05:30:45 +0000

    fly by wire committed

--- src/cmd/ai/flybywire.cpp
+++ src/cmd/ai/flybywire.cpp
@@ -138,7 +138,13 @@ MatchVelocity::~MatchVelocity () {
 #endif
 }
 
-FlyByWire::FlyByWire (): MatchVelocity(Vector(0,0,0),Vector(0,0,0),true,false,false), sheltonslide(false),controltype(!XMLSupport::parse_bool (vs_config->getVariable ("physics","CarControl","false"))){
+FlyByWire::FlyByWire (): MatchVelocity(Vector(0,0,0),Vector(0,0,0),true,false,false), sheltonslide(false),controltype(!XMLSupport::parse_bool (vs_config->getVariable ("physics","CarControl",
+#ifdef CAR_SIM
+"true"
+#else
+"false"
+#endif
+))){
   DesiredThrust= Vector(0,0,0);
 }
 
@@ -256,15 +262,14 @@ void FlyByWire::Execute () {
     parent->GetComputerData().set_speed = desired_velocity.Magnitude();
 
     desireThrust=true;
-  }
-  if (!controltype) {
-    if (desired_velocity.k<0) {
-      desired_velocity.k=0;
-      parent->GetComputerData().set_speed = 0;
-  
+    if (!controltype) {
+      if (desired_velocity.k<0) {
+	desired_velocity.k=0;
+	parent->GetComputerData().set_speed = 0;
+      }
     }
   }
-  if (sheltonslide&&(!desireThrust)&&controltype) {
+  if ((sheltonslide||!controltype)&&(!desireThrust)) {
     MatchAngularVelocity::Execute();//only match turning, keep velocity same
   }else {
     MatchVelocity::Execute();
