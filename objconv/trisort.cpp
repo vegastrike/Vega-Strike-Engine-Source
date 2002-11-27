e604e248827e6a8c363c4a3ac9305d73226e7601
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2002-11-27 09:34:47 +0000

    trisort!!!

--- objconv/trisort.cpp
+++ objconv/trisort.cpp
@@ -3,46 +3,7 @@
 #include <vector>
 #include <string>
 #include <ctype.h>
-class Vector {
-public:
-    double x,y,z;
-    Vector (double xx, double yy, double zz):x(xx),y(yy),z(zz) {}
-    
-};
-#ifdef __APPLE__
-int iswhitespace(int c) {
-    return isspace (c)||isblank(c)||c=='\n'||c=='\r';
-}
-#endif
-class Index {
-public:
-    Vector x;
-    int p,n,t,c;
-    Index (Vector xyz,int p, int n, int t, int c):x(xyz),p(p),n(n),t(t),c(c){}
-
-    void write (FILE * fp) const;
-};
-class Face {
-public:
-std::vector <Index> p;
-    bool operator < (const class Face &) const{return true;}
-};
-class Mesh {
-    void processline (char * line);
-    Index Mesh::processfacevertex(char * chunk) const;
-    Face Mesh::processface( char * line)const;
-std::vector <Vector> p;
-std::vector <Vector> n;
-std::vector <Vector> t;//tex coords i=s j=t
-std::vector <Vector> c;
-std::vector <Face> f;    
-public:
-    Mesh (const char * filename);
-    void sort();
-    void write(const char * filename) const;
-    void writeIndex (FILE * fp,const Index&)const;
-};
-
+#include "trisort.h"
 void Mesh::sort () {
 std::sort (f.begin(),f.end());
 }
---
+++ objconv/trisort.h
@@ -0,0 +1,45 @@
+class Vector {
+public:
+    double x,y,z;
+    Vector (double xx, double yy, double zz):x(xx),y(yy),z(zz) {}
+
+};
+#ifdef __APPLE__
+int iswhitespace(int c) {
+    return isspace (c)||isblank(c)||c=='\n'||c=='\r';
+}
+#endif
+class Index {
+public:
+    Vector x;
+    int p,n,t,c;
+    Index (Vector xyz,int p, int n, int t, int c):x(xyz),p(p),n(n),t(t),c(c){}
+
+    void write (FILE * fp) const;
+};
+class Plane {public:
+    int a,b,c,d;
+    Plane (int a,int b, int c, int d): a(a),b(b),c(c),d(d){}
+};
+class Face {
+public:
+std::vector <Index> p;
+    Plane planeEqu()const;
+    bool operator < (const class Face &) const;
+};
+class Mesh {
+    void processline (char * line);
+    Index Mesh::processfacevertex(char * chunk) const;
+    Face Mesh::processface( char * line)const;
+std::vector <Vector> p;
+std::vector <Vector> n;
+std::vector <Vector> t;//tex coords i=s j=t
+std::vector <Vector> c;
+std::vector <Face> f;
+public:
+        Mesh (const char * filename);
+    void sort();
+    void write(const char * filename) const;
+    void writeIndex (FILE * fp,const Index&)const;
+};
+
