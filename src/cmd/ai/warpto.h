0f97982ba30c6e21141d4dee341b211a0f817cd4
mamiyaotaru <mamiyaotaru@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-03-24 01:53:00 +0000

    WarpToP should be void instead of bool

--- src/cmd/ai/warpto.h
+++ src/cmd/ai/warpto.h
@@ -2,4 +2,4 @@ class Unit;
 bool DistanceWarrantsWarpTo (Unit * parent, float sigdistance);
 bool DistanceWarrantsTravelTo (Unit * parent, float sigdistance);
 bool TargetWorthPursuing(Unit * parent, Unit* target);
-bool WarpToP(Unit * parent, Unit * target);
+void WarpToP(Unit * parent, Unit * target);
