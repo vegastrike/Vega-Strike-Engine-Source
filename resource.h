813d4ebdcdfe8b94ae96ca7c4a0f931eae1a30da
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-05-19 01:00:00 +0000

    Added a Version Info

--- resource.h
+++ resource.h
@@ -5,12 +5,15 @@
 #define IDI_ICON1                       101
 #define IDD_START                       102
 #define IDI_ICON3                       105
+#define IDC_CURSOR1                     106
+#define IDI_ICON2                       107
+#define IDD_DIALOG1                     108
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        106
+#define _APS_NEXT_RESOURCE_VALUE        109
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
