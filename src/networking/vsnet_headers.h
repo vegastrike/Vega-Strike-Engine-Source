d783e62ad71616155d52c6b29adb3f32fb1dadf8
surfdargent <surfdargent@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-07-21 08:43:54 +0000

    -

---
+++ src/networking/vsnet_headers.h
@@ -0,0 +1,22 @@
+#if defined(_WIN32) && !defined(__CYGWIN__)
+	//#warning "Win32 platform"
+	#define in_addr_t unsigned long
+	#ifdef USE_WINSOCK2
+	#define _WIN32_WINNT 0x0400
+	#endif
+	#include <windows.h>
+#else
+	#include <netdb.h>
+ 	#include <string.h>
+	#include <sys/socket.h>
+	#include <sys/time.h>
+	#include <netinet/in.h>
+	#include <arpa/inet.h>
+	#include <unistd.h>
+    #define SOCKET_ERROR -1
+#endif
+#ifdef __APPLE__
+#include <sys/types.h>
+#endif
+#include <stdlib.h>
+
