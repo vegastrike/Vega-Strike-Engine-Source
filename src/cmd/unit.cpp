4f3182eeae5410434546dcd797a1e8d18ec0d6c4
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-10-10 04:23:42 +0000

    arrgh fixed the double thing

--- src/cmd/unit.cpp
+++ src/cmd/unit.cpp
@@ -394,16 +394,16 @@ void GameUnit<UnitType>::Draw(const Transformation &parent, const Matrix &parent
 #endif
 
       //      VSFileSystem::vs_fprintf (stderr,"%s %d ",name.c_str(),i);
-      float d = GFXSphereInFrustum(TransformedPosition,
+      double d = GFXSphereInFrustum(TransformedPosition,
 				   minmeshradius+meshdata[i]->clipRadialSize()
 #ifdef VARIABLE_LENGTH_PQR
 				   *SizeScaleFactor
 #endif 
 				   );
-      float lod;
+      double lod;
       //      VSFileSystem::vs_fprintf (stderr,"\n");
       if (d) {  //d can be used for level of detail shit
-	d = (TransformedPosition-_Universe->AccessCamera()->GetPosition().Cast()).Magnitude();
+	d = (TransformedPosition-_Universe->AccessCamera()->GetPosition()).Magnitude();
 	if ((lod =g_game.detaillevel*g_game.x_resolution*2*meshdata[i]->rSize()/GFXGetZPerspective((d-meshdata[i]->rSize()<g_game.znear)?g_game.znear:d-meshdata[i]->rSize()))>=g_game.detaillevel) {//if the radius is at least half a pixel (detaillevel is the scalar... so you gotta make sure it's above that
 		float currentFrame = meshdata[i]->getCurrentFrame();
 		meshdata[i]->Draw(lod,this->WarpMatrix(*ctm),d,cloak,(_Universe->AccessCamera()->GetNebula()==nebula&&nebula!=NULL)?-1:0,chardamage);//cloakign and nebula		
