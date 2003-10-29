ebc4bfa3ace50077a9c2fa06db95071633070b21
griwodz <griwodz@9476613a-4e0a-0410-bcee-947800e9c4d1>
2003-10-29 06:49:29 +0000

    linecollide not initialized in networked mode

--- src/linecollide.h
+++ src/linecollide.h
@@ -2,6 +2,8 @@
 #define _LINECOLLIDE_H_
 
 #include <stdlib.h>
+#include "gfx/vec.h"
+
 /**
  * Line Collide provides a complete container for a unit that is put in a collide hashtable
  * For collisions. The mini, maxi vectors of the line collide are taken
@@ -10,29 +12,87 @@ class Unit;
 class Beam;
 class Bolt;
 
-struct LineCollide {
-  ///The object that this LineCollide approximates
+struct LineCollide
+{
+private:
   union OBJECT {
     Unit *u;
     Beam *b;
     Bolt *blt;
     int i;
-  }object;
-  ///The minimum x,y,z that this object has
-  QVector Mini;
-  ///The maximum x,y,z that this object has
-  QVector Maxi;
-  /**
-   * The last item that checked this for collisions
-   * to prevent duplicate selection 
-   */
-  void * lastchecked;
-  ///Which type of unit it is. Used for subsequently calling object's Accurate collide func
-  enum collidables {UNIT, BEAM,BALL,BOLT,PROJECTILE} type;
-  ///If this object was saved as a huge object (hhuge for dos oddities)
-  bool hhuge;
-  LineCollide(): Mini(0,0,0), Maxi(0,0,0), type(UNIT){object.u=NULL;hhuge=false;lastchecked=NULL;}
-  LineCollide (void * objec, enum collidables typ,const QVector &st, const QVector &en) {this->object.u=(Unit *)objec;this->type=typ;this->Mini=st;this->Maxi=en;hhuge=false; lastchecked=NULL;}
-  LineCollide (const LineCollide &l) {object=l.object; type=l.type; Mini=l.Mini;Maxi=l.Maxi;hhuge=l.hhuge; lastchecked=NULL;}      
+  };
+
+public:
+    enum collidables {UNIT, BEAM,BALL,BOLT,PROJECTILE};
+
+    ///The object that this LineCollide approximates
+    OBJECT object;
+    ///The minimum x,y,z that this object has
+    QVector Mini;
+    ///The maximum x,y,z that this object has
+    QVector Maxi;
+    /**
+     * The last item that checked this for collisions
+     * to prevent duplicate selection 
+     */
+    void * lastchecked;
+
+    ///Which type of unit it is. Used for subsequently calling object's Accurate collide func
+    collidables type;
+
+    ///If this object was saved as a huge object (hhuge for dos oddities)
+    bool hhuge;
+
+    LineCollide()
+        : Mini(0,0,0)
+        , Maxi(0,0,0)
+        , lastchecked( NULL )
+        , type(UNIT)
+        , hhuge( false )
+    {
+        object.u=NULL;
+    }
+
+    LineCollide( void * objec, collidables typ,const QVector &st, const QVector &en)
+        : Mini( st )
+        , Maxi( en )
+        , lastchecked( NULL )
+        , type( typ )
+        , hhuge( false )
+    {
+        this->object.u=(Unit *)objec;
+    }
+
+    LineCollide( const LineCollide& l )
+        : Mini( l.Mini )
+        , Maxi( l.Maxi )
+        , lastchecked( NULL )
+        , type( l.type )
+        , hhuge( l.hhuge )
+    {
+        object=l.object;
+    }      
+
+    LineCollide( )
+    {
+        object.u    = NULL;
+        type        = UNIT;
+        Mini        = QVector(-1,-1,-1);
+        Maxi        = QVector(-1,-1,-1);
+        hhuge       = false;
+        lastchecked = NULL;
+    }
+
+    LineCollide& operator=( const LineCollide& l )
+    {
+        object=l.object;
+        type=l.type;
+        Mini=l.Mini;
+        Maxi=l.Maxi;
+        hhuge=l.hhuge;
+        lastchecked=NULL;
+        return *this;
+    }
 };
+
 #endif
