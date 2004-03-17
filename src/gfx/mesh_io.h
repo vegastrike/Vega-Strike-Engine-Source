15fc5d079b3efcd8f230d0df7324c955be24d422
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2004-03-17 08:06:54 +0000

    op

---
+++ src/gfx/mesh_io.h
@@ -0,0 +1,32 @@
+#define int32bit int
+#define float32bit float
+#define char8bit unsigned char
+#if 0
+enum BLENDFUNC{
+    ZERO            = 1,
+	ONE             = 2, 
+    SRCCOLOR        = 3,
+	INVSRCCOLOR     = 4, 
+    SRCALPHA        = 5,
+	INVSRCALPHA     = 6, 
+    DESTALPHA       = 7,
+	INVDESTALPHA    = 8, 
+    DESTCOLOR       = 9,
+	INVDESTCOLOR    = 10, 
+    SRCALPHASAT     = 11,
+    CONSTALPHA    = 12, 
+    INVCONSTALPHA = 13,
+    CONSTCOLOR = 14,
+    INVCONSTCOLOR = 15
+};
+#endif
+struct Mesh_vec3f {
+   float x;
+   float y;
+   float z;
+};
+enum textype{
+	ALPHAMAP,
+	ANIMATION,
+	TEXTURE
+};
