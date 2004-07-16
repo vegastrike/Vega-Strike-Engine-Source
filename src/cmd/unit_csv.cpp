fa6f16afcf53dc293ff804d6576478c34172642e
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2004-07-16 19:50:32 +0000

    num anim points

--- src/cmd/unit_csv.cpp
+++ src/cmd/unit_csv.cpp
@@ -497,6 +497,9 @@ void Unit::LoadRow(CSVRow &row,string modification, string * netxml) {
     image->hudImage = createVSSprite(tmpstr.c_str());
   }  
   combat_role=ROLES::getRole(row["Combat_Role"]);
+  graphicOptions.NumAnimationPoints=stoi(row["Num_Animation_Stages"],0);
+  if (graphicOptions.NumAnimationPoints>0)
+    graphicOptions.Animating=0;
   xml.unitscale = stoi(row["Unit_Scale"],1);
   if (!xml.unitscale) xml.unitscale=1;
   image->unitscale=xml.unitscale;
