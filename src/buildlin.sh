2e0e90b5adab633d916ab83dca61a6a9ac59a8ba
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2004-10-09 09:52:49 +0000

    could work--porolly not

---
+++ src/buildlin.sh
@@ -0,0 +1,48 @@
+g++  -Xlinker -export-dynamic -Xlinker  -nomultidefs  -o vegastrike debug_vs.o			\
+	gfxlib_struct.o		\
+	in_joystick.o			\
+	force_feedback.o		\
+	faction_util.o		\
+	in_kb.o				\
+	in_sdl.o				\
+	in_mouse.o			\
+	in_main.o				\
+	main.o				\
+	in_handler.o			\
+	main_loop.o			\
+	physics.o				\
+	star_system_jump.o	\
+	star_system.o 		\
+	universe.o			\
+	universe_util.o		\
+	config_xml.o			\
+    macosx_math.o			\
+	cg_global.o                   \
+	posh.o						\
+	stardate.o				\
+	star_system_xml.o		\
+	star_system_generic.o 	\
+	universe_generic.o		\
+	universe_util_generic.o 	\
+	galaxy.o					\
+	galaxy_xml.o				\
+	galaxy_gen.o				\
+	faction_generic.o			\
+	hashtable.o				\
+	configxml.o				\
+	easydom.o					\
+	xml_serializer.o			\
+	xml_support.o				\
+	lin_time.o				\
+	endianness.o				\
+	faction_util_generic.o	\
+	load_mission.o			\
+	savegame.o				\
+	vs_globals.o				\
+	vsfilesystem.o					\
+	pk3.o					\
+aldrv/libaldrv.a gfx/nav/libnav.a common/libvscommon.a networking/libnetclient.a cmd/script/script_call_briefing.o cmd/script/libscript.a cmd/script/c_alike/libc_alike.a python/briefing_wrapper.o cmd/libcmd.a cmd/base_init.o python/libpython.a gfx/libgfx.a cmd/ai/libai.a gldrv/libgldrv.a gui/libgui.a networking/libnet.a networking/lowlevel/libnetlowlevel.a  cmd/collide/libcollide.a  boost/libboost_python.a /usr/lib/libpthread.a   /usr/lib/libglut.a /usr/lib/libexpat.a /usr/lib/libjpeg.a /usr/lib/python2.2/config/libpython2.2.a /usr/lib/libpng.a /usr/lib/libz.a  /usr/lib/libSDL.a /usr/lib/libopenal.a /usr/lib/libGLU.a /usr/lib/libutil.a  /home/daniel/install/Mesa-6.2/lib/libGL.so -ldl
+#/usr/lib/gcc-lib/i386-linux/2.95.4/libstdc++.a /usr/lib/libdl.a  
+#/usr/lib/libc.a /usr/lib/gcc-lib/i386-linux/2.95.4/libgcc.a /usr/lib/gcc-lib/i386-linux/2.95.4/crtbegin.o /usr/lib/gcc-lib/i386-linux/2.95.4/crtend.o /usr/lib/crt1.o
+
+#-nostartfiles -nostdlib
