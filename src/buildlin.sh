b065ae2c4407495975ae32f1bff1db1aa1a80ce8
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2004-10-09 10:12:49 +0000

    added some cool new options

--- src/buildlin.sh
+++ src/buildlin.sh
@@ -1,4 +1,4 @@
-g++  -Xlinker -export-dynamic -Xlinker   -ovegastrike debug_vs.o			\
+gcc -Xlinker -export-dynamic -Xlinker   -ovegastrike debug_vs.o			\
 	gfxlib_struct.o		\
 	in_joystick.o			\
 	force_feedback.o		\
@@ -41,8 +41,8 @@ g++  -Xlinker -export-dynamic -Xlinker   -ovegastrike debug_vs.o			\
 	vs_globals.o				\
 	vsfilesystem.o					\
 	pk3.o					\
-aldrv/libaldrv.a gfx/nav/libnav.a common/libvscommon.a networking/libnetclient.a cmd/script/script_call_briefing.o cmd/script/libscript.a cmd/script/c_alike/libc_alike.a python/briefing_wrapper.o cmd/libcmd.a cmd/base_init.o python/libpython.a gfx/libgfx.a cmd/ai/libai.a gldrv/libgldrv.a gui/libgui.a networking/libnet.a networking/lowlevel/libnetlowlevel.a  cmd/collide/libcollide.a  boost/libboost_python.a    /usr/lib/libglut.a /usr/lib/libexpat.a /usr/lib/libjpeg.a /usr/lib/python2.2/config/libpython2.2.a /usr/lib/libpng.a /usr/lib/libz.a  /usr/lib/libSDL.a /usr/lib/libopenal.a /usr/lib/libGLU.a /usr/lib/libutil.a  /home/daniel/install/Mesa-6.2/lib/libGL.so -ldl
-#/usr/lib/gcc-lib/i386-linux/2.95.4/libstdc++.a /usr/lib/libpthread.a /usr/lib/libdl.a   -lpthread
-#/usr/lib/libc.a /usr/lib/gcc-lib/i386-linux/2.95.4/libgcc.a /usr/lib/gcc-lib/i386-linux/2.95.4/crtbegin.o /usr/lib/gcc-lib/i386-linux/2.95.4/crtend.o /usr/lib/crt1.o
+aldrv/libaldrv.a gfx/nav/libnav.a common/libvscommon.a networking/libnetclient.a cmd/script/script_call_briefing.o cmd/script/libscript.a cmd/script/c_alike/libc_alike.a python/briefing_wrapper.o cmd/libcmd.a cmd/base_init.o python/libpython.a gfx/libgfx.a cmd/ai/libai.a gldrv/libgldrv.a gui/libgui.a networking/libnet.a networking/lowlevel/libnetlowlevel.a  cmd/collide/libcollide.a  boost/libboost_python.a    /usr/lib/libglut.a /usr/lib/libexpat.a /usr/lib/libjpeg.a /usr/lib/python2.2/config/libpython2.2.a /usr/lib/libpng.a /usr/lib/libz.a  /usr/lib/libSDL.a /usr/lib/libopenal.a /usr/lib/libGLU.a /usr/lib/libutil.a /usr/lib/gcc-lib/i386-linux/2.95.4/libstdc++.a /home/daniel/install/Mesa-6.2/lib/libGL.so -ldl /usr/lib/libpthread.a  /usr/lib/gcc-lib/i386-linux/2.95.4/libgcc.a 
+#/usr/lib/gcc-lib/i386-linux/2.95.4/libstdc++.a  /usr/lib/libdl.a   -lpthread
+# /usr/lib/gcc-lib/i386-linux/2.95.4/crtbegin.o /usr/lib/gcc-lib/i386-linux/2.95.4/crtend.o 
 
 #-nostartfiles -nostdlib
