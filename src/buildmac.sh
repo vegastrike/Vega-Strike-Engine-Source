7374eba558437c7539ba5d6ba874b3a6974cabcc
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2004-10-18 09:27:06 +0000

    made the soundserver work with nonSDL version

--- src/buildmac.sh
+++ src/buildmac.sh
@@ -43,6 +43,8 @@ gcc -Xlinker -force_flat_namespace  -Xlinker -nomultidefs  -o vegastrike debug_v
 	pk3.o					\
 aldrv/libaldrv.a common/libvscommon.a networking/libnetclient.a cmd/script/script_call_briefing.o cmd/script/libscript.a cmd/script/c_alike/libc_alike.a python/briefing_wrapper.o cmd/libcmd.a cmd/base_init.o python/libpython.a gfx/nav/libnav.a gfx/libgfx.a cmd/ai/libai.a gldrv/libgldrv.a gui/libgui.a networking/libnet.a networking/lowlevel/libnetlowlevel.a cmd/collide/libcollide.a  boost/libboost_python.a  -lpthread   -framework OpenGL -framework GLUT /sw/lib/libexpat.a /sw/lib/libjpeg.a /Users/daniel/Install/libpng/libpng.a /users/daniel/install/zlib/libz.a  /usr/lib/gcc/darwin/3.1/libstdc++.a -framework AppKit /users/daniel/Vega/openal/linux/src/libopenal.a  -ldl /Users/daniel/Vega/Python-2.2.3/libpython2.2.a  -framework IOKit -framework Foundation -framework CoreFoundation -framework ApplicationServices -framework CoreServices  -framework Carbon -lcups -lbsm -framework SystemConfiguration -framework CoreAudio
 
+gcc -Xlinker -force_flat_namespace  -Xlinker -nomultidefs  -o soundserver -DHAVE_SDL=1 -D__APPLE -I.. networking/soundserver.cpp networking/inet.cpp  -I/sw/include /Users/daniel/Install/SDL_mixer-1.2.5/.libs/libSDL_mixer.a /sw/lib/libvorbisfile.a /sw/lib/libvorbis.a /sw/lib/libogg.a /Users/daniel/Install/SDL-1.2.7/src/.libs/libSDL.a  /usr/lib/gcc/darwin/3.1/libstdc++.a -framework AppKit -framework OpenGL  -framework Quicktime
+
 #/sw/lib/python2.2/config/libpython2.2.a 
 #/usr/lib/dyld
 #
