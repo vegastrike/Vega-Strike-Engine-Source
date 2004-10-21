0ca29f62bed5b0bebc623893fdc81929ad0dc371
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2004-10-21 10:43:23 +0000

    finally got it buildin on OS X 10.3 for 10.2

--- src/buildmac.sh
+++ src/buildmac.sh
@@ -6,7 +6,9 @@ g++ -o replace ../objconv/replace.cpp
 ./replace __ZNSs6assignEPKcm __ZNSs6assi7nEPKcm cmd/libcmd.a cmd/libcmd.a
 ranlib cmd/ai/libai.a >&/dev/null
 ranlib cmd/libcmd.a >&/dev/null
-gcc -Xlinker -force_flat_namespace  -Xlinker -multiply_defined -Xlinker warning  -o vegastrike debug_vs.o			\
+export MACOSX_DEPLOYMENT_TARGET=10.1
+export NEXT_ROOT=/Developer/SDKs/MacOSX10.2.8.sdk/
+gcc -Xlinker -Y -Xlinker 16384 -Xlinker -force_flat_namespace  -Xlinker -multiply_defined -Xlinker warning  -o vegastrike debug_vs.o			\
 	gfxlib_struct.o		\
 	in_joystick.o			\
 	force_feedback.o		\
@@ -49,10 +51,9 @@ gcc -Xlinker -force_flat_namespace  -Xlinker -multiply_defined -Xlinker warning 
 	vs_globals.o				\
 	vsfilesystem.o					\
 	pk3.o					\
-aldrv/libaldrv.a common/libvscommon.a networking/libnetclient.a cmd/script/script_call_briefing.o cmd/script/libscript.a cmd/script/c_alike/libc_alike.a python/briefing_wrapper.o cmd/libcmd.a cmd/base_init.o python/libpython.a gfx/nav/libnav.a gfx/libgfx.a cmd/ai/libai.a gldrv/libgldrv.a gui/libgui.a networking/libnet.a networking/lowlevel/libnetlowlevel.a cmd/collide/libcollide.a  boost/libboost_python.a  -lpthread   -framework OpenGL -framework GLUT /sw/lib/libexpat.a /sw/lib/libjpeg.a /Users/daniel/Install/libpng/libpng.a /users/daniel/install/zlib/libz.a  /usr/lib/gcc/darwin/3.1/libstdc++.a -framework AppKit /users/daniel/Vega/openal/linux/src/libopenal.a  -ldl /Users/daniel/Vega/Python-2.2.3/libpython2.2.a  -framework IOKit -framework Foundation -framework CoreFoundation -framework ApplicationServices -framework CoreServices  -framework Carbon -lcups -lbsm -framework SystemConfiguration -framework CoreAudio
-
-gcc -Xlinker -force_flat_namespace  -Xlinker -nomultidefs  -o soundserver -DHAVE_SDL=1 -D__APPLE -I.. networking/soundserver.cpp networking/inet.cpp  -I/sw/include /Users/daniel/Install/SDL_mixer-1.2.5/.libs/libSDL_mixer.a /sw/lib/libvorbisfile.a /sw/lib/libvorbis.a /sw/lib/libogg.a /Users/daniel/Install/SDL-1.2.7/src/.libs/libSDL.a  /usr/lib/gcc/darwin/3.1/libstdc++.a -framework AppKit -framework OpenGL  -framework Quicktime
-
+aldrv/libaldrv.a common/libvscommon.a networking/libnetclient.a cmd/script/script_call_briefing.o cmd/script/libscript.a cmd/script/c_alike/libc_alike.a python/briefing_wrapper.o cmd/libcmd.a cmd/base_init.o python/libpython.a gfx/nav/libnav.a gfx/libgfx.a cmd/ai/libai.a gldrv/libgldrv.a gui/libgui.a networking/libnet.a networking/lowlevel/libnetlowlevel.a cmd/collide/libcollide.a  boost/libboost_python.a -L/Developer/SDKs/MacOSX10.2.8.sdk/usr/lib/gcc/darwin/3.1 -lgcc -lpthread   -framework OpenGL -framework GLUT /sw/lib/libexpat.a /sw/lib/libjpeg.a /Users/daniel/Install/libpng/libpng.a /users/daniel/install/zlib/libz.a  /Developer/SDKs/MacOSX10.2.8.sdk/usr/lib/gcc/darwin/3.1/libstdc++.a -framework AppKit /users/daniel/Vega/openal/linux/src/libopenal.a   /Users/daniel/Vega/Python-2.2.3/libpython2.2.a  -framework IOKit -framework Foundation -framework CoreFoundation -framework ApplicationServices -framework CoreServices  -framework Carbon -framework SystemConfiguration -framework CoreAudio -lobjc -lc
+gcc -Xlinker -force_flat_namespace  -Xlinker -nomultidefs  -o soundserver -DHAVE_SDL=1 -D__APPLE -I.. networking/soundserver.cpp networking/inet.cpp  -I/sw/include /Users/daniel/Install/SDL_mixer-1.2.5/.libs/libSDL_mixer.a /sw/lib/libvorbisfile.a /sw/lib/libvorbis.a /sw/lib/libogg.a /Users/daniel/Install/SDL-1.2.7/src/.libs/libSDL.a  /usr/lib/gcc/darwin/3.1/libstdc++.a -framework AppKit -framework OpenGL  -framework Quicktime -framework AudioUnit -framework AudioToolbox
+cp vegastrike /Users/daniel/Vega/Priv-Package/priv,exe.app/Contents/MacOS/vegastrike
 #/sw/lib/python2.2/config/libpython2.2.a 
 #/usr/lib/dyld
 #
--- src/macosx_math.cpp
+++ src/macosx_math.cpp
@@ -1,6 +1,23 @@
 #include <macosx_math.h>
-
 #if defined(__APPLE__) || defined(MACOSX)
+extern "C" {
+  char * ctermid_r(char *buf) {
+    if (buf) {
+      buf[0]='/';
+      buf[1]='d';
+      buf[2]='e';
+      buf[3]='v';
+      buf[4]='/';
+      buf[5]='t';
+      buf[6]='t';
+      buf[7]='y';
+      buf[8]='\0';
+    }else {
+      static char ret[]="/dev/tty";
+      return ret;
+    }
+  }
+}
 
 float sqrtf(float v)
 {
