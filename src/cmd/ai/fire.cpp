85e343a935945aa42dd20970c433a0243d0b3e4c
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2005-03-01 07:26:46 +0000

    dont fire at jump

--- src/cmd/ai/fire.cpp
+++ src/cmd/ai/fire.cpp
@@ -371,7 +371,8 @@ bool FireAt::ShouldFire(Unit * targ, bool &missilelock) {
   }
   static float firewhen = XMLSupport::parse_float (vs_config->getVariable ("AI","Firing","InWeaponRange","1.2"));
   bool temp=parent->TrackingGuns(missilelock);
-  return (dist<firewhen&&angle>1/agg)||(temp&&dist<firewhen&&angle>0);
+  bool isjumppoint=targ->isUnit()==PLANETPTR&&!(Planet*)targ)->GetDestinations().empty();
+  return ((dist<firewhen&&angle>1/agg)||(temp&&dist<firewhen&&angle>0))&&!isjumppoint;
 }
 
 FireAt::~FireAt() {
