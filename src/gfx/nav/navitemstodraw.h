47f17bb7e4f4a615ae745f12c836c1a43ecd3b35
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2005-07-20 06:46:35 +0000

    draw grid false

--- src/gfx/nav/navitemstodraw.h
+++ src/gfx/nav/navitemstodraw.h
@@ -50,6 +50,9 @@ void NavigationSystem::DrawCursor (float x, float y, float wid, float hei, const
 //	**********************************
 void NavigationSystem::DrawGrid(float &x1, float &x2, float &y1, float &y2, const GFXColor &col)
 {
+  static bool draw_grid =XMLSupport::parse_bool(vs_config->getVariable("graphics","hud","draw_nav_grid","true"));
+  if(!draw_grid)
+    return;
 	GFXColorf(col);
 	GFXDisable(TEXTURE0);
 	GFXDisable(LIGHTING);
