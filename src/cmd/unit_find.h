f0b368d7daadbc5dc3680aff0645e1fb16fa114f
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2005-09-30 10:19:02 +0000

    modularized unit find into unit_find.h so more people can easily use this new map thing... finding units with any criterion is really quite simple now

---
+++ src/cmd/unit_find.h
@@ -0,0 +1,104 @@
+template <class Locator> void findObjects (StarSystem * ss,CollideMap::iterator location, Locator *check) {
+    if (location!=null_collide_map.begin()) {
+      QVector thispos = (**location).GetPosition();
+      float thisrad=fabs((*location)->radius);
+      CollideMap::iterator tless=location;
+      CollideMap::iterator tmore=location;
+      
+      ++tmore;
+      CollideMap *cm=ss->collidemap;             
+      check->init(cm,location);
+      bool workA=true;
+      bool workB=true;
+      
+      if (tless!=cm->begin())
+        --tless;
+      else 
+        workA=false;      
+      while(workA||workB){
+        if (workA
+            &&!check->cullless(tless)) {
+          if (check->BoltsOrUnits()||(check->UnitsOnly()==(*tless)->radius>0)) {
+            float trad = check->NeedDistance()?((*tless)->GetPosition()-thispos).Magnitude()-fabs((*tless)->radius)-thisrad:0;
+            if (!check->acquire(trad,tless)) {
+              workA=false;
+            }
+          }
+          if (tless!=cm->begin()) 
+            tless--;
+          else
+            workA=false;
+        }else workA=false;
+        if (workB
+            &&tmore!=cm->end()
+            &&!check->cullmore(tmore)) {
+          if (check->BoltsOrUnits()||(check->UnitsOnly()==(*tmore)->radius>0)) {
+            float trad = check->NeedDistance()?((*tmore)->GetPosition()-thispos).Magnitude()-fabs((*tmore)->radius)-thisrad:0;
+            if (!check->acquire(trad,tmore)) {
+              workB=false;
+            }
+          }
+          tmore++;
+        }else workB=false;
+      }      
+    }
+}
+
+class NearestUnitLocator {
+  CollideMap::iterator location;
+  float startkey;
+  float rad;
+public:
+  Collidable::CollideRef retval;
+  bool BoltsOrUnits() {
+    return false;
+  }
+  bool UnitsOnly() {
+    return true;
+  }
+  bool NeedDistance() {
+    return true;
+  }
+  NearestUnitLocator() {
+    retval.unit=NULL;
+  }
+  void init (CollideMap * cm, CollideMap::iterator parent) {
+    this->location=parent;
+    startkey = sqrt((*location)->GetMagnitudeSquared());
+    rad=FLT_MAX;
+    retval.unit=NULL;
+  }
+  bool cullless (CollideMap::iterator tless) {
+    return rad!=FLT_MAX&&(startkey-rad)*(startkey-rad)>(*tless)->GetMagnitudeSquared();
+  }
+  bool cullmore (CollideMap::iterator tmore) {
+    return rad!=FLT_MAX&&(startkey+rad)*(startkey+rad)<(*tmore)->GetMagnitudeSquared();
+  }
+  bool acquire(float distance, CollideMap::iterator i) {
+    if (distance<rad) {
+      rad=distance;
+      retval=(*i)->ref;
+    }
+    return true;
+  }
+};
+class NearestBoltLocator :public NearestUnitLocator{
+public:
+  bool UnitsOnly() {
+    return false;
+  }
+};
+class NearestObjectLocator:public NearestUnitLocator{
+public:
+  bool isUnit;
+  bool UnitsOnly() {return false;}
+  bool BoltsOrUnits() {return true;}
+  bool acquire(float distance, CollideMap::iterator i) {  
+    Collidable::CollideRef lastret=retval;
+    bool retval=NearestUnitLocator::acquire(distance,i);
+    if (memcmp((void*)&retval,(void*)&lastret,sizeof(Collidable::CollideRef))){
+      isUnit=(*i)->radius>0;
+    }
+    return retval;
+  }
+};
--- src/cmd/unit_generic.cpp
+++ src/cmd/unit_generic.cpp
@@ -41,6 +41,7 @@
 #define strcasecmp stricmp
 #endif
 #include "config.h"
+#include "unit_find.h"
 using namespace Orders;
 extern void DestroyMount(Mount*);
 
@@ -2019,6 +2020,7 @@ void Unit::UpdatePhysics (const Transformation &trans, const Matrix &transmat, c
     UpdateCollideQueue();
   }
 }
+
 void Unit::AddVelocity(float difficulty) {
    static float warprampuptime=XMLSupport::parse_float (vs_config->getVariable ("physics","warprampuptime","5")); // for the heck of it.    
    static float warprampdowntime=XMLSupport::parse_float (vs_config->getVariable ("physics","warprampdowntime","0.5"));     
@@ -2046,51 +2048,10 @@ void Unit::AddVelocity(float difficulty) {
 	   float minmultiplier=warpMultiplierMax;
 	   Unit * planet;
            Unit * testthis=NULL;
-           QVector thispos=this->Position();
-           if (this->location!=null_collide_map.begin()) {
-             CollideMap::iterator tless=this->location;
-             CollideMap::iterator tmore=this->location;
-             float startkey=sqrt((*this->location)->GetMagnitudeSquared());
-             ++tmore;
-             float rad=FLT_MAX;
-             CollideMap *cm=_Universe->activeStarSystem()->collidemap;             
-             if (tless!=cm->begin()) {
-               if ((**cm->begin()).radius>0) { 
-                 testthis=(**cm->begin()).ref.unit;
-                 rad=(thispos-testthis->Position()).Magnitude()-testthis->rSize();
-               }
-               --tless;
-             }
-             bool workA=true;
-             bool workB=true;
-
-             while(workA||workB){
-               if (workA
-                   &&tless!=cm->begin()
-                   &&(startkey-rad)*(startkey-rad)<(*tless)->GetMagnitudeSquared()) {
-                 
-                 if ((*tless)->radius>0) {
-                   float trad = ((*tless)->GetPosition()-thispos).Magnitude()-(*tless)->radius;
-                   if (trad<rad) {
-                     rad=trad;
-                     testthis = (*tless)->ref.unit;
-                   }
-                 }
-                 tless--;
-               }else workA=false;
-               if (workB
-                   &&tmore!=cm->end()
-                   &&(startkey+rad)*(startkey+rad)>(*tmore)->GetMagnitudeSquared()) {
-                 if ((*tmore)->radius>0) {
-                   float trad = ((*tmore)->GetPosition()-thispos).Magnitude()-(*tmore)->radius;
-                   if (trad<rad) {
-                     rad=trad;
-                     testthis=(*tmore)->ref.unit;
-                   }
-                 }
-                 tmore++;
-               }else workB=false;
-             }
+           {
+             NearestUnitLocator locatespec;
+             findObjects(_Universe->activeStarSystem(),this->location,&locatespec);
+             testthis=locatespec.retval.unit;
            }
 	   for (un_iter iter = _Universe->activeStarSystem()->gravitationalUnits().createIterator();(planet=*iter)||testthis;++iter) {
 		 if (planet==NULL) {
