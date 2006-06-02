ca9735513baaf1b9ae7277185efcb1d5a4f010d6
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2006-06-02 20:50:16 +0000

    fixed up extra qualification errors

--- objconv/basemaker/base_maker.cpp
+++ objconv/basemaker/base_maker.cpp
@@ -711,7 +711,7 @@ public:
 //	static void Base::beginElement(void *userData, const XML_Char *names, const XML_Char **atts);
 //	void Base::beginElement(const string &name, const AttributeList attributes);
 //	static void Base::endElement(void *userData, const XML_Char *name);
-	void Base::Load(const char * filename, const char * faction);
+	void Load(const char * filename, const char * faction);
 	static void ClickWin (int x, int y, int button, int state);
 	void Click (int x, int y, int button, int state);
 	static void PassiveMouseOverWin (int x, int y);
