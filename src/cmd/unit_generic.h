c54e28d1449aa0999bffacebe0c0ca5768b02d55
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2006-07-28 21:27:36 +0000

    got rid of controversial calls to std::min... cxx did not like it

--- src/cmd/unit_generic.h
+++ src/cmd/unit_generic.h
@@ -820,7 +820,8 @@ public:
     float tp=(lref.i==0)?0:fabs(Limits().lateral/lref.i);
     float tq=(lref.j==0)?0:fabs(Limits().vertical/lref.j);
     float tr=(lref.k==0)?0:fabs(((lref.k>0)?Limits().forward:Limits().retro)/lref.k);
-    float tm=std::min(tp,std::min(tr,tq));
+	float trqmin=(tr<tq)?tr:tq;
+	float tm=tp<trqmin?tp:trqmin;
     return lref.Magnitude()*tm/GetMass();
   }
   ///Transforms a orientation vector Up a coordinate level. Does not take position into account
