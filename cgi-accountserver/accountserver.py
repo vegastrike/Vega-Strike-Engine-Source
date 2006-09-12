0ad696a5bacd0829f1b43811115207b98a0ec368
hellcatv <hellcatv@9476613a-4e0a-0410-bcee-947800e9c4d1>
2006-09-12 07:26:16 +0000

    now this accountserver doesnt print a stray newline

--- cgi-accountserver/accountserver.py
+++ cgi-accountserver/accountserver.py
@@ -148,8 +148,8 @@ def getLoginInfo(conn, user, passwd, dologin):
 get_form=False
 post_form=False
 def execute(conn,url,post):
-	print '\n'
-        # Can't write to stderr since on IIS it is concatenated with stdout.
+	#print '\n'
+    # Can't write to stderr since on IIS it is concatenated with stdout.
 	#sys.stderr.write('Executing post query: '+post+'\n')
 	try:
 		packet = Packet(conn, post) #if it crashes we dont care..this is python
