8a642033a7213743c3a90f8c0f52442d74659c47
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2007-10-03 02:36:57 +0000

    Made vssetup search multiple directories.

--- setup/src/c/setup.cpp
+++ setup/src/c/setup.cpp
@@ -26,10 +26,12 @@
 #include <unistd.h>
 #include <pwd.h>
 #include <sys/types.h>
+#include <vector>
 
 #endif
 #include <string>
 using std::string;
+using std::vector;
 char origpath[65536];
 #if defined(_WINDOWS)&&defined(_WIN32)
 typedef char FileNameCharType [65536];
@@ -44,22 +46,80 @@ int main(int argc, char *argv[]) {
 #endif
 #if 1
 //def _WIN32
+	getcwd (origpath,65535);
+	origpath[65535]=0;
         int i;
 	for (i=strlen(argv[0]);argv[0][i]!='\\'&&argv[0][i]!='/'&&i>=0;i--) {
 	}
 	argv[0][i+1]='\0';
-	chdir(argv[0]);
+	if (i>=0) {
+		chdir(argv[0]);
+	}
 #endif
-#ifdef _WIN32
-	chdir ("..");//gotta check outside bin dir
-#endif	
-	getcwd (origpath,65535);
-	origpath[65535]=0;
 	
+	{	
+		vector<string>	data_paths;
+#ifdef DATA_DIR
+		data_paths.push_back( DATA_DIR);
+#endif
+		data_paths.push_back( origpath);
+		data_paths.push_back( string(origpath)+"/..");
+		data_paths.push_back( string(origpath)+"/../data4.x");
+		data_paths.push_back( string(origpath)+"/../../data4.x");
+		data_paths.push_back( string(origpath)+"/data4.x");
+		data_paths.push_back( string(origpath)+"/data");
+		data_paths.push_back( string(origpath)+"/../data");
+		getcwd (origpath,65535);
+		origpath[65535]=0;
+		data_paths.push_back( ".");
+		data_paths.push_back( "..");
+		data_paths.push_back( "../data4.x");
+		data_paths.push_back( "../../data4.x");
+		data_paths.push_back( "../data");
+		data_paths.push_back( "../../data");
+		data_paths.push_back( "../Resources");
+		data_paths.push_back( "../Resources/data");
+		data_paths.push_back( "/usr/share/local/vegastrike/data");
+		data_paths.push_back( "/usr/local/share/vegastrike/data");
+		data_paths.push_back( "/usr/local/vegastrike/data");
+		data_paths.push_back( "/usr/share/vegastrike/data");
+		data_paths.push_back( "/usr/local/games/vegastrike/data");
+		data_paths.push_back( "/usr/games/vegastrike/data");
+		data_paths.push_back( "/opt/share/vegastrike/data");
+		data_paths.push_back( "../Resources/data4.x");
+		data_paths.push_back( "/usr/share/local/vegastrike/data4.x");
+		data_paths.push_back( "/usr/local/share/vegastrike/data4.x");
+		data_paths.push_back( "/usr/local/vegastrike/data4.x");
+		data_paths.push_back( "/usr/share/vegastrike/data4.x");
+		data_paths.push_back( "/usr/local/games/vegastrike/data4.x");
+		data_paths.push_back( "/usr/games/vegastrike/data4.x");
+		data_paths.push_back( "/opt/share/vegastrike/data4.x");
+		
+		// Win32 data should be "."
+		char tmppath[16384];
+		for( vector<string>::iterator vsit=data_paths.begin(); vsit!=data_paths.end(); vsit++)
+		{
+			// Test if the dir exist and contains config_file
+			chdir(origpath);
+			chdir((*vsit).c_str());
+			FILE *setupcfg = fopen("setup.config","r");
+			if (!setupcfg)
+				continue;
+			fclose(setupcfg);
+			setupcfg = fopen("Version.txt","r");
+			if (!setupcfg)
+				continue;
+			getcwd (origpath,65535);
+			origpath[65535]=0;
+			printf("Found data in %s\n",origpath);
+			break;
+		}
+	}
 #ifndef _WIN32
 	struct passwd *pwent;
 	pwent = getpwuid (getuid());
         string HOMESUBDIR=".vegastrike";
+	
 	FILE *version=fopen("Version.txt","r");
 	if (!version)
 		version=fopen("../Version.txt","r");
