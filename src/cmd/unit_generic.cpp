307daaa285aed3a0a5ff7d337a3b19470d55f0dd
jacks <jacks@9476613a-4e0a-0410-bcee-947800e9c4d1>
2008-03-24 05:50:59 +0000

    Fix for Bugtracker #1904563: crash on basic repair of escape pod.

--- src/cmd/unit_generic.cpp
+++ src/cmd/unit_generic.cpp
@@ -8149,7 +8149,7 @@ int Unit::RepairUpgrade ()
 	const Unit * temprate = makeFinalBlankUpgrade (name,faction);
 	int success=0;
 	double pct=0;
-	if (temprate->name!=string("LOAD_FAILED")) {
+	if (temprate&&temprate->name!=string("LOAD_FAILED")) {
 		success = Upgrade(temprate,-1,-1,0,false,pct,NULL,false)?1:0;
 		if (pct>0)
 			success=1;
