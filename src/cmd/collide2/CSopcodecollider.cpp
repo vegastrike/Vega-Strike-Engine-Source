f054ac3919b1f8545d6fce79e7690cb9221bc3e1
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2008-04-04 22:01:51 +0000

    Fixed collisions--I set the models

--- src/cmd/collide2/CSopcodecollider.cpp
+++ src/cmd/collide2/CSopcodecollider.cpp
@@ -134,7 +134,9 @@ bool csOPCODECollider::Collide( csOPCODECollider &otherCollider,
 {
 	csOPCODECollider* col2 = (csOPCODECollider*) &otherCollider;
 
-
+	ColCache.ResetCache();
+	ColCache.Model0 = this->m_pCollisionModel;
+	ColCache.Model1 = col2->m_pCollisionModel;
 	csMatrix3 m1;
 	if (trans1) m1 = trans1->GetT2O ();
 	csMatrix3 m2;
