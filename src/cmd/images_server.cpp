8875d1eff570353f7d24c02fc10e8603ec944a3b
safemode <safemode@9476613a-4e0a-0410-bcee-947800e9c4d1>
2010-02-27 12:16:53 +0000

    merge safemode branch -r12656, merge mucking fixed. vegaserver compiles again.  also, not sure how the mesher cmake setup got lost but it's back

--- src/cmd/images_server.cpp
+++ src/cmd/images_server.cpp
@@ -1,20 +1,13 @@
 #include "cmd/images.h"
 
 template < typename BOGUS >
-UnitImages<BOGUS>::UnitImages()
-{
-    VSCONSTRUCT1( 'i' )
-    pHudImage = NULL;
-    pExplosion = NULL;
-}
-template < typename BOGUS >
 UnitImages<BOGUS>::~UnitImages()
 {
-    delete pExplosion;
-//    delete pHudImage;
+    if (pExplosion)
+        delete pExplosion;
     VSDESTRUCT1
 }
 
 //explicit instantiation needed
 template struct UnitImages<void>;
-
+static UnitImages<void> bleh;
