d61df1e59398394b61a9a74e089095b0d284ee72
klaussfreire <klaussfreire@9476613a-4e0a-0410-bcee-947800e9c4d1>
2010-03-09 03:39:43 +0000

    Ticket #18: removed use of deprecated libpng functions

--- src/gfx/vsimage.cpp
+++ src/gfx/vsimage.cpp
@@ -138,15 +138,14 @@ unsigned char* VSImage::ReadImage( VSFile *f, textureTransform *t, bool strip, V
 
 VSError VSImage::CheckPNGSignature( VSFile *file )
 {
-    VSError ret = Ok;
-
     //Do standard reading of the file
     unsigned char sig[8];
     file->Begin();
     file->Read( sig, 8 );
-    if ( !png_check_sig( sig, 8 ) )
-        ret = BadFormat;
-    return ret;
+    if ( png_sig_cmp( sig, 0, 8 ) )
+        return BadFormat;
+    else
+        return Ok;
 }
 
 VSError VSImage::CheckJPEGSignature( VSFile *file )
@@ -323,7 +322,7 @@ unsigned char* VSImage::ReadPNG()
         if (strip_16 && this->img_color_type == PNG_COLOR_TYPE_PALETTE)
             png_set_palette_to_rgb( png_ptr );
         if (this->img_color_type == PNG_COLOR_TYPE_GRAY && this->img_depth < 8)
-            png_set_gray_1_2_4_to_8( png_ptr );
+            png_set_expand_gray_1_2_4_to_8( png_ptr );
         png_set_expand( png_ptr );
         png_read_update_info( png_ptr, info_ptr );
         this->sizeX     = 1;
