18ca6f6193f43799e0f630cbc6081fe80bc7769c
ace123 <ace123@9476613a-4e0a-0410-bcee-947800e9c4d1>
2010-09-25 05:10:17 +0000

    vegaserver is able to load factions again.

--- src/cmd/faction_xml_server.cpp
+++ src/cmd/faction_xml_server.cpp
@@ -4,10 +4,15 @@
 #include "faction_generic.h"
 
 //by chuck starchaser
+/*
 void Faction::LoadXML( const char *filename, char *xmlbuffer, int buflength )
 {
 }
+*/
 //Now, that is fast! :)
+
+// Server crashes on startup with empty LoadXML! Factions are important.
+#include "faction_xml.cpp"
 
 #endif
 
