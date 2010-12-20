fcaea2002f396e167fa3e4d4ace2ace93570a927
pheonixstorm <pheonixstorm@9476613a-4e0a-0410-bcee-947800e9c4d1>
2010-12-20 09:27:12 +0000

    Final change to vsimage. Removed ifdef since png_set_expand_gray_1_2_4_to_8 is used by both linux and windows now. Change in ifdef _WIN32 was a temporary fix. Unless png code changes again this should be final.

--- src/gfx/vsimage.cpp
+++ src/gfx/vsimage.cpp
@@ -322,12 +322,7 @@ unsigned char* VSImage::ReadPNG()
         if (strip_16 && this->img_color_type == PNG_COLOR_TYPE_PALETTE)
             png_set_palette_to_rgb( png_ptr );
         if (this->img_color_type == PNG_COLOR_TYPE_GRAY && this->img_depth < 8) {
-#ifdef _WIN32
-            // png_set_gray_1_2_4_to_8( png_ptr ); changed in earlier version of libpng to fix an error
-			png_set_expand_gray_1_2_4_to_8( png_ptr );
-#else
             png_set_expand_gray_1_2_4_to_8( png_ptr );
-#endif
         }
         png_set_expand( png_ptr );
         png_read_update_info( png_ptr, info_ptr );
