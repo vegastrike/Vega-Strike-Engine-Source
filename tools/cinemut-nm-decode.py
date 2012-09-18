222a6caf8a9c8f1a8f88c217f717e98c1bb82412
klaussfreire <klaussfreire@9476613a-4e0a-0410-bcee-947800e9c4d1>
2012-09-18 01:03:06 +0000

    Cinemut encode/decode scripts

---
+++ tools/cinemut-nm-decode.py
@@ -0,0 +1,79 @@
+import sys
+from PIL import Image
+import math
+
+if len(sys.argv) <= 2:
+    print "Usage: ./cinemut-nm-decode.py <inputfile> <outputfile>"
+    sys.exit(0)
+
+try:
+    import numpy
+    def encode(im):
+        im.load()
+        r,g,b,a = im.split()
+        atan = numpy.arctan
+        tan = numpy.tan
+        e = 3.1875
+        
+        u  = numpy.array(r.getdata(), dtype=numpy.float32)
+        u += numpy.array(g.getdata(), dtype=numpy.float32)
+        u += numpy.array(b.getdata(), dtype=numpy.float32)
+        v  = numpy.array(a.getdata(), dtype=numpy.float32)
+        
+        u *= 1.0/(3*255)
+        u -= 0.5
+        
+        v *= 1.0/255
+        v -= 0.5
+        
+        z  = 1.0/numpy.sqrt(0.25*0.25 + u*u + v*v)
+        u *= z
+        v *= z
+        z *= 0.25
+        
+        u *= 0.5
+        v *= 0.5
+        u += 0.5
+        v += 0.5
+        
+        u *= 255
+        v *= 255
+        z *= 255
+        
+        r.putdata(u)
+        g.putdata(v)
+        b.putdata(z)
+        a.putdata(numpy.zeros(len(u))+255)
+        im = Image.merge("RGBA", (r,g,b,a))
+        return im
+except ImportError:
+    print >>sys.stderr, "Warning: numpy not found. Install it to greatly improve performance."
+    def encode(im, int=int):
+        data = im.load()
+        sqrt = math.sqrt
+        e = 3.1875
+        for x in xrange(im.size[0]):
+            for y in xrange(im.size[1]):
+                loc = x,y
+                r,g,b,a = data[loc]
+                u = (r+g+b) / (3*255.0) - 0.5
+                v = a / 255.0 - 0.5
+                z = 1.0/sqrt(0.25*0.25+u*u+v*v)
+                u *= z
+                v *= z
+                z *= 0.25
+                r = (u*0.5+0.5)*255
+                g = (v*0.5+0.5)*255
+                b = z*255
+                a = 255
+                data[loc] = int(r),int(g),int(b),int(a)
+        return im
+
+im = Image.open(sys.argv[1])
+
+if im.mode != "RGBA":
+    im = im.convert("RGBA")
+
+im = encode(im)
+
+im.save(sys.argv[2])
---
+++ tools/cinemut-nm-encode.py
@@ -0,0 +1,78 @@
+import sys
+from PIL import Image
+import math
+
+if len(sys.argv) <= 2:
+    print "Usage: ./cinemut-nm-encode.py <inputfile> <outputfile>"
+    sys.exit(0)
+
+try:
+    import numpy
+    def encode(im):
+        im.load()
+        r,g,b,a = im.split()
+        atan = numpy.arctan
+        tan = numpy.tan
+        e = 3.1875
+        u = numpy.array(r.getdata(), dtype=numpy.float32)
+        v = numpy.array(g.getdata(), dtype=numpy.float32)
+        z = numpy.array(b.getdata(), dtype=numpy.float32)
+        u *= (2.0/255)
+        v *= (2.0/255)
+        z *= (1.0/255)
+        u -= 1
+        v -= 1
+        u /= z
+        v /= z
+        u  = atan(u, u)
+        v  = atan(v, v)
+        u *= 0.5
+        v *= 0.5
+        u  = tan(u, u)
+        v  = tan(v, v)
+        u *= 0.5
+        v *= 0.5
+        u += 0.5
+        v += 0.5
+        u *= 255
+        v *= 255
+        g.putdata(u)
+        u -= e
+        r.putdata(u)
+        u += 2*e
+        b.putdata(u)
+        a.putdata(v)
+        im = Image.merge("RGBA", (r,g,b,a))
+        return im
+except ImportError:
+    print >>sys.stderr, "Warning: numpy not found. Install it to greatly improve performance."
+    def encode(im, int=int):
+        data = im.load()
+        atan = math.atan
+        tan = math.tan
+        e = 3.1875
+        for x in xrange(im.size[0]):
+            for y in xrange(im.size[1]):
+                loc = x,y
+                u,v,z,a = data[loc]
+                if z:
+                    z = z/255.0
+                    u = 2*(u/255.0)-1
+                    v = 2*(v/255.0)-1
+                    u = tan(0.5*atan(u/z))
+                    v = tan(0.5*atan(v/z))
+                    u = (0.5*u+0.5)*255
+                    v = (0.5*v+0.5)*255
+                else:
+                    u=v=z=0
+                data[loc] = int(u-e),int(u),int(u+e),int(v)
+        return im
+
+im = Image.open(sys.argv[1])
+
+if im.mode != "RGBA":
+    im = im.convert("RGBA")
+
+im = encode(im)
+
+im.save(sys.argv[2])
