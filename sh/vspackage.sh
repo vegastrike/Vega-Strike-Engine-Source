29165100cd9db8f2822aea8fc3cb2a3e81ed8bc2
Benjamen Meyer <bm_witness@yahoo.com>
2020-06-10 22:35:13 -0400

    Peer Review: Travis-CI -> Use Deploy section
    Previously marked up the use of the vspackage script as part of
    the build section; it really should be in the `before_deploy`
    section which only gets called on a deployment.
    
    Section is disabled as it's not ready, so this just stages it for
    the next PR to complete it out after the rest of the process is
    defined and understood.
    
    Add a packages directory and copy all the packaging files there.

--- sh/vspackage.sh
+++ sh/vspackage.sh
@@ -15,6 +15,7 @@ ROOT_DIR=$(pwd)
 BUILD_DIR=$ROOT_DIR/build
 BIN_DIR=$ROOT_DIR/bin
 SRC_DIR=$ROOT_DIR/engine
+PACKAGE_DIR=$ROOT_DIR/packages
 VS_EXECUTABLE=${BUILD_DIR}/vegastrike
 
 echo "---------------------------------"
@@ -32,5 +33,29 @@ cd $BUILD_DIR
 
 make package
 make package_source 
+
+# Ensure the package output directory exists
+mkdir -p ${PACKAGE_DIR}
+
+# Copy the Debian Package files
+for DEB_FILE in `ls *.deb`
+do
+	echo "Copying ${DEB_FILE} to ${PACKAGE_DIR}"
+	cp "${DEB_FILE}" "${PACKAGE_DIR}"
+done
+
+# Copy the Red Hat RPM Package files
+for RPM_FILE in `ls *.rpm`
+do
+	echo "Copying ${RPM_FILE} to ${PACKAGE_DIR}"
+	cp "${RPM_FILE}" "${PACKAGE_DIR}"
+done
+
+# Copy the Tarball Package files
+for TAR_FILE in `ls *.tar.*`
+do
+	echo "Copying ${TAR_FILE} to ${PACKAGE_DIR}"
+	cp "${TAR_FILE}" "${PACKAGE_DIR}"
+done
 
 cd $ROOT_DIR
