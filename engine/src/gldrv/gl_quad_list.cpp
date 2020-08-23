797169b02181dc8bf318a1a7a802900d59d48bc0
Stephen G Tuggy <sgt@stephengtuggy.com>
2020-08-23 08:27:44 -0700

    gldrv/gl_quad_list.cpp: Handle quadassignments realloc failure properly

--- engine/src/gldrv/gl_quad_list.cpp
+++ engine/src/gldrv/gl_quad_list.cpp
@@ -1,22 +1,23 @@
 /*
- * Vega Strike
+ * gl_quad_list.cpp
+ *
  * Copyright (C) 2001-2002 Daniel Horn & Alan Shieh
+ * Copyright (C) 2020 pyramid3d, Stephen G. Tuggy, and other Vega Strike contributors
  *
- * http://vegastrike.sourceforge.net/
+ * This file is part of Vega Strike.
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ * Vega Strike is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Vega Strike is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * along with Vega Strike.  If not, see <https://www.gnu.org/licenses/>.
  */
 #include "gl_globals.h"
 
@@ -73,7 +74,13 @@ int GFXQuadList::AddQuad( const GFXVertex *vertices, const GFXColorVertex *color
                 data.vertices = (GFXVertex*) realloc( data.vertices, numVertices*sizeof (GFXVertex) );
             else
                 data.colors = (GFXColorVertex*) realloc( data.colors, numVertices*sizeof (GFXColorVertex) );
-            quadassignments = (int*) realloc( quadassignments, numVertices*sizeof (int)/4 );
+            int* tmp = (int*) realloc( quadassignments, numVertices*sizeof (int)/4 );
+            if (tmp == nullptr) {
+                BOOST_LOG_TRIVIAL(fatal) << "Error reallocating quadassignments!";
+                return -1;
+            } else {
+                quadassignments = tmp;
+            }
         }
         for (int i = numVertices/8; i < numVertices/4; i++)
             quadassignments[i] = -1;
@@ -97,14 +104,14 @@ int GFXQuadList::AddQuad( const GFXVertex *vertices, const GFXColorVertex *color
             Dirty--;
             return i;
         }
-    BOOST_LOG_TRIVIAL(info) << "Fatal Error adding quads";
+    BOOST_LOG_TRIVIAL(fatal) << "Fatal Error adding quads";
     //should NOT get here!
     return -1;
 }
 void GFXQuadList::DelQuad( int which )
 {
     if (quadassignments[which] >= numQuads) {
-        BOOST_LOG_TRIVIAL(info) << "error del";
+        BOOST_LOG_TRIVIAL(error) << "error del";
         return;
     }
     if (which < 0 || which >= numVertices/4 || quadassignments[which] == -1)
