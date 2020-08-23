8110ffaafc959b3652b4d8281ccdb95eda3bf704
Stephen G Tuggy <sgt@stephengtuggy.com>
2020-08-23 07:59:03 -0700

    vsfilesystem.cpp: Properly terminate va_list with va_end; copyleft

--- engine/src/vsfilesystem.cpp
+++ engine/src/vsfilesystem.cpp
@@ -1,3 +1,26 @@
+/**
+ * vsfilesystem.cpp
+ *
+ * Copyright (C) 2020 pyramid3d, Nachum Barcohen, Roy Falk, Stephen G. Tuggy,
+ * and other Vega Strike contributors.
+ *
+ * This file is part of Vega Strike.
+ *
+ * Vega Strike is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vega Strike is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Vega Strike.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 #include <stdio.h>
 #include <assert.h>
 #include <stdarg.h>
@@ -453,7 +476,9 @@ int vs_fprintf( FILE *fp, const char *format, ... )
         va_list ap;
         va_start( ap, format );
 
-        return vfprintf( fp, format, ap );
+        int retVal = vfprintf( fp, format, ap );
+        va_end(ap);
+        return retVal;
     } else {}
     return 0;
 }
@@ -464,6 +489,7 @@ void vs_dprintf( char level, const char *format, ... )
         va_list ap;
         va_start( ap, format );
         vfprintf( stderr, format, ap );
+        va_end(ap);
     }
 }
 
@@ -1781,7 +1807,9 @@ int VSFile::Fprintf( const char *format, ... )
         va_list ap;
         va_start( ap, format );
 
-        return vfprintf( this->fp, format, ap );
+        int retVal = vfprintf( this->fp, format, ap );
+        va_end(ap);
+        return retVal;
     } else {
         BOOST_LOG_TRIVIAL(fatal)<<"!!! ERROR : Writing is not supported within resource/volume files";
         VSExit( 1 );
