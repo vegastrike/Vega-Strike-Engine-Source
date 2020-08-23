0d5a94ae0eff528176a77873e0bb1749ac6de449
Stephen G Tuggy <sgt@stephengtuggy.com>
2020-08-22 22:48:21 -0700

    cmd/collide2/opcodesysdef.h: Copyleft notice

--- engine/src/cmd/collide2/opcodesysdef.h
+++ engine/src/cmd/collide2/opcodesysdef.h
@@ -1,3 +1,25 @@
+/**
+ * opcodesysdef.h
+ *
+ * Copyright (C) 2020 pyramid3d, Stephen G. Tuggy, and other Vega Strike contributors.
+ *
+ * This file is part of Vega Strike.
+ *
+ * Vega Strike is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vega Strike is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Vega Strike.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
 #ifndef CS_COMPAT_H_
 #define CS_COMPAT_H_
 
@@ -9,20 +31,20 @@
 #define CS_NO_QSQRT
 
 
-#ifndef CS_FORCEINLINE 
-# ifdef CS_COMPILER_GCC 
-#  define CS_FORCEINLINE inline __attribute__((always_inline)) 
-#  if (__GNUC__ == 3) && (__GNUC_MINOR__ == 4) 
+#ifndef CS_FORCEINLINE
+# ifdef CS_COMPILER_GCC
+#  define CS_FORCEINLINE inline __attribute__((always_inline))
+#  if (__GNUC__ == 3) && (__GNUC_MINOR__ == 4)
     // Work around a gcc 3.4 issue where forcing inline doesn't always work
-#   define CS_FORCEINLINE_TEMPLATEMETHOD inline 
-#  endif 
-# else 
-#  define CS_FORCEINLINE inline 
-# endif 
-#endif 
-#ifndef CS_FORCEINLINE_TEMPLATEMETHOD 
-# define CS_FORCEINLINE_TEMPLATEMETHOD CS_FORCEINLINE 
-#endif 
+#   define CS_FORCEINLINE_TEMPLATEMETHOD inline
+#  endif
+# else
+#  define CS_FORCEINLINE inline
+# endif
+#endif
+#ifndef CS_FORCEINLINE_TEMPLATEMETHOD
+# define CS_FORCEINLINE_TEMPLATEMETHOD CS_FORCEINLINE
+#endif
 
 
 #include "gfx/quaternion.h"
