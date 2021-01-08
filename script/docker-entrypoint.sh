12df43fd73cecb1256c581a531fe71264f913ff7
Stephen G Tuggy <sgt@stephengtuggy.com>
2021-01-07 18:06:21 -0800

    script/[cibuild,docker-entrypoint.sh]: Fix IS_RELEASE tests

--- script/docker-entrypoint.sh
+++ script/docker-entrypoint.sh
@@ -6,15 +6,15 @@ echo "docker-entrypoint.sh: Flags passed in: $@"
 
 # find /usr -iname '*libboost_python*'
 
-if [ -z "$IS_RELEASE" ]
+if [ $IS_RELEASE ]
 then
+    script/build.sh -DCMAKE_BUILD_TYPE=RelWithDebInfo $@
+    script/package
+else
     if [ "$CC" == "clang" ]
     then
         script/build.sh -DCMAKE_BUILD_TYPE=RelWithDebInfo $@
     else
         script/build.sh -DCMAKE_BUILD_TYPE=Debug $@
     fi
-else
-    script/build.sh -DCMAKE_BUILD_TYPE=RelWithDebInfo $@
-    script/package
 fi
