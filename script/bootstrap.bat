9f101c31e0387b0de19c2c4a432d6b2155877a61
Stephen G Tuggy <sgt@stephengtuggy.com>
2021-05-24 16:29:18 -0700

    Fine-tuning bootstrap.bat

--- script/bootstrap.bat
+++ script/bootstrap.bat
@@ -22,11 +22,12 @@
 set VCKPG_PARENT_DIR=C:\Projects
 set CMAKE_VERSION="3.20.2"
 
-mkdir /p "%VCKPG_PARENT_DIR%"
+mkdir "%VCKPG_PARENT_DIR%"
 pushd "%VCKPG_PARENT_DIR%"
 git clone https://github.com/Microsoft/vcpkg.git
 .\vcpkg\bootstrap-vcpkg.bat -disableMetrics
-set PATH=%PATH%;%VCKPG_PARENT_DIR%\vcpkg\downloads\tools\cmake-%CMAKE_VERSION%-windows\cmake-%CMAKE_VERSION%-windows-i386\bin
-set VCPKG_DEFAULT_TRIPLET=x64-windows
-set PYTHONHOME=%VCKPG_PARENT_DIR%\vcpkg\packages\python3_x64-windows\tools\python3
+setx PATH %PATH%;%VCKPG_PARENT_DIR%\vcpkg\downloads\tools\cmake-%CMAKE_VERSION%-windows\cmake-%CMAKE_VERSION%-windows-i386\bin
+setx VCPKG_DEFAULT_TRIPLET x64-windows
+setx PYTHONHOME "%VCKPG_PARENT_DIR%\vcpkg\packages\python3_x64-windows\tools\python3"
 popd
+refreshenv
