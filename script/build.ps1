57315e8bec772e287c982de6a329e6030b9eb437
Stephen G Tuggy <sgt@stephengtuggy.com>
2021-05-24 17:03:38 -0700

    Convert bootstrap, build .BAT files to PowerShell

---
+++ script/build.ps1
@@ -0,0 +1,25 @@
+# Copyright (C) 2021 Stephen G. Tuggy
+
+# https://github.com/vegastrike/Vega-Strike-Engine-Source
+
+# This file is part of Vega Strike.
+
+# Vega Strike is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# Vega Strike is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Vega Strike.  If not, see <https://www.gnu.org/licenses/>.
+
+cmake -B build -S .\engine\ -DCMAKE_TOOLCHAIN_FILE="$env:VCPKG_ROOT\scripts\buildsystems\vcpkg.cmake" -DCMAKE_BUILD_TYPE=Release -DUSE_PYTHON_3=ON
+cmake --build .\build\ --config Release
+New-Item bin -ItemType Directory -Force
+xcopy .\build\Release\*.* .\bin\
+xcopy .\build\objconv\Release\*.* .\bin\
+xcopy .\build\setup\Release\*.* .\bin\
