e72416f0ca5d26c120c47417cf6d17279af5833b
P-D-E <pademil@tin.it>
2021-06-06 17:34:09 +0200

    New_Game check conformed to previous code

--- engine/src/cmd/basecomputer.cpp
+++ engine/src/cmd/basecomputer.cpp
@@ -5979,7 +5979,7 @@ bool BaseComputer::actionConfirmedLoadGame()
     if (desc) {
         std::string tmp = desc->text();
         if (tmp.length() > 0) {
-           if ((tmp != "New_Game") && (!isUtf8SaveGame(tmp))) {
+            if ((string( "New_Game" ) != tmp) && (!isUtf8SaveGame(tmp))) {
                 showAlert( tmp + " is not UTF-8, convert it before loading" );
                 return true;
             }
