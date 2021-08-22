718c988e9882d5094f27ed0cba76e605e8fe2719
Stephen G Tuggy <sgt@stephengtuggy.com>
2021-08-22 13:03:27 -0700

    macos-ci.yml, build.sh: Try getting the # of CPUs another way

--- script/build.sh
+++ script/build.sh
@@ -13,11 +13,30 @@
 # https://github.com/Taose/Vegastrike-taose/wiki/How-to-Build
 #====================================
 
+# Copyright (C) 2020-2021 Stephen G. Tuggy and other Vega Strike contributors.
+#
+# https://github.com/vegastrike/Vega-Strike-Engine-Source
+#
+# This file is part of Vega Strike.
+#
+# Vega Strike is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Vega Strike is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Vega Strike.  If not, see <https://www.gnu.org/licenses/>.
+
 
 set -e
 
 echo "-----------------------------"
-echo "--- build.sh | 2020-08-17 ---"
+echo "--- build.sh | 2021-08-22 ---"
 echo "-----------------------------"
 
 #----------------------------------
@@ -43,7 +62,7 @@ cmake $@ $SRC_DIR
 #make clean
 
 # compile now using all cpus and show compilation commands
-make -j $(nproc) VERBOSE=1
+make -j $(getconf _NPROCESSORS_ONLN) VERBOSE=1
 
 cd $ROOT_DIR
 
