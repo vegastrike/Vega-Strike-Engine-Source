d6a77ba6a99bf0c4657d8e8af5ff5abf85012ea8
Roy Falk <roy@falk.co.il>
2021-09-18 21:24:53 +0300

    Move applyTechniqueOverride to unit_generic
    and some other refactors.

--- engine/src/cmd/unit.cpp
+++ engine/src/cmd/unit.cpp
@@ -523,72 +523,6 @@ void GameUnit::Draw()
 }
 
 
-static float parseFloat( const std::string &s )
-{
-    if ( s.empty() ) {
-        VS_LOG(info, (boost::format("WARNING: invalid float: %1%") % s));
-        return 0.f;
-    } else {
-        return XMLSupport::parse_floatf( s );
-    }
-}
-
-static void parseFloat4( const std::string &s, float value[4] )
-{
-    string::size_type ini = 0, end;
-    int i = 0;
-    while (i < 4 && ini != string::npos) {
-        value[i++] = parseFloat( s.substr( ini, end = s.find_first_of( ',', ini ) ) );
-        ini = ( (end == string::npos) ? end : (end+1) );
-    }
-    //if (i >= 4 && ini != string::npos) {
-    //    VS_LOG(info, (boost::format("WARNING: invalid float4: %1%") % s));
-    //}
-    while (i < 4) {
-        value[i++] = 0;
-    }
-}
-
-
-void GameUnit::applyTechniqueOverrides(const map<string, string> &overrides)
-{
-    for (vector<Mesh*>::iterator mesh = this->meshdata.begin(); mesh != this->meshdata.end(); ++mesh) {
-        if (*mesh != NULL) {
-            // First check to see if the technique holds any parameter being overridden
-            TechniquePtr technique = (*mesh)->getTechnique();
-            if (technique.get() != NULL) {
-                bool doOverride = false;
-                for (int passno = 0; !doOverride && passno < technique->getNumPasses(); ++passno) {
-                    const Pass &pass = technique->getPass(passno);
-                    for (size_t paramno = 0; !doOverride && paramno < pass.getNumShaderParams(); ++paramno) {
-                        if (overrides.count(pass.getShaderParam(paramno).name) > 0)
-                            doOverride = true;
-                    }
-                }
-
-                if (doOverride) {
-                    // Prepare a new technique with the overrides
-                    // (make sure the technique has been compiled though -
-                    // parameter values don't really need recompilation)
-                    TechniquePtr newtechnique = TechniquePtr(new Technique(*technique));
-                    for (int passno = 0; passno < technique->getNumPasses(); ++passno) {
-                        Pass &pass = technique->getPass(passno);
-                        for (size_t paramno = 0; paramno < pass.getNumShaderParams(); ++paramno) {
-                            Pass::ShaderParam &param = pass.getShaderParam(paramno);
-                            map<string, string>::const_iterator override = overrides.find(param.name);
-                            if (override != overrides.end())
-                                parseFloat4(override->second, param.value);
-                        }
-                    }
-
-                    (*mesh)->setTechnique(newtechnique);
-                }
-            }
-        }
-    }
-}
-
-
 Matrix GameUnit::WarpMatrix( const Matrix &ctm ) const
 {
     if ( this->GetWarpVelocity().MagnitudeSquared() < (game_options.warp_stretch_cutoff * game_options.warp_stretch_cutoff * game_options.game_speed * game_options.game_speed )
@@ -611,24 +545,3 @@ Matrix GameUnit::WarpMatrix( const Matrix &ctm ) const
     }
 }
 
-using Orders::FireAt;
-
-#if 0
-
-void GameUnit::SwapOutHalos()
-{
-    for (int i = 0; i < numhalos; i++)
-        //float x,y;
-        //halos[i]->GetDimensions (x,y);    //halos[i]->SetDimensions (x/(1024),y/(1024));
-        halos[i]->Draw( cumulative_transformation, cumulative_transformation_matrix, 0 );
-}
-
-void GameUnit::SwapInHalos()
-{
-    for (int i = 0; i < numhalos; i++) {
-        //float x,y;
-        //halos[i]->GetDimensions (x,y);
-        //halos[i]->SetDimensions (x*(1024),y*(1024));
-    }
-}
-#endif
