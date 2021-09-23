7fa14a0265ab46fdd2031024e08c84908e207e92
Benjamen Meyer <bm_witness@yahoo.com>
2021-09-22 23:46:03 -0400

    Peer Review
    - Document VS_CAN_BUILD_RPM
    - Move over more functionality
    - Reformat Cmake to standards
    
    NOTE: Manual port of bef98d258e9f7db1b09a511c7cd81766440b227e to 0.8.x

--- engine/FindLinuxDistro.cmake
+++ engine/FindLinuxDistro.cmake
@@ -5,47 +5,89 @@
 # ========================
 #  Example Usage:
 #
-#	FIND_PACKAGE(LinuxDistro, REQUIRED)
+#    FIND_PACKAGE(LinuxDistro, REQUIRED)
 #
 # Output Variables:
-#	LSB_EXISTS
-#		Is the distro an LSB Compliant Distro?
+#    LINUX_ETC_OS_RELEASE_EXISTS
+#       Does /etc/os-release exist?
 #
-#	LSB_LINUX_DISTRIBUTION
-#		Using LSB Tooling, what is the host distro family?
+#    LINUX_ID
+#       Linux Distro Name (semi-authoritative)
+#       Only valid if LINUX_ETC_OS_RELEASE_EXISTS is TRUE
 #
-#	LSB_LINUX_DISTRIBUTION_CODENAME
-#		Using LSB Tooling, what is the host distro code name?
+#    LINUX_CODENAME
+#       Linux Distro Code Name (semi-authoritative)
+#       Only valid if LINUX_ETC_OS_RELEASE_EXISTS is TRUE
 #
-#	VS_CAN_BUILD_DEB
-#		Does the host have tooling installed to build Debian Packages?
+#    LINUX_VERSION_ID
+#       Linux Distro Version (semi-authoritative)
+#       Only valid if LINUX_ETC_OS_RELEASE_EXISTS is TRUE
+#
+#    LSB_EXISTS
+#        Is the distro an LSB Compliant Distro?
+#
+#    LSB_LINUX_DISTRIBUTION
+#       Using LSB Tooling, what is the host distro family? (authoritative)
+#       Only valid if LSB_EXISTS is TRUE
+#
+#    LSB_LINUX_DISTRIBUTION_CODENAME
+#       Using LSB Tooling, what is the host distro code name? (authoritative)
+#       Only valid if LSB_EXISTS is TRUE
+#
+#    VS_CAN_BUILD_DEB
+#       Does the host have tooling installed to build Debian (DEB) Packages?
+#
+#    VS_CAN_BUILD_RPM
+#       Does the host have the tooling installed to build RPM Packages?
 #
 
-# lsb_release -i --> but then it'll find `Ubuntu` instead of Debian so will have to map more
+IF (CMAKE_SYSTEM_NAME STREQUAL Linux)
 
-# we can probably more reliably detect via checking for different packager commands
-# `lsb_release` doesn't always work correctly, so it's easier to read the underlying file
-# if it exists. It doesn't always exist. Debian doesn't use it, but Ubuntu does.
-SET(LSB_EXISTS FALSE)
-SET(LSB_LINUX_DISTRIBUTION "Unknown")
-SET(LSB_LINUX_DISTRIBUTION_CODENAME "Unknown")
+    SET(LINUX_ETC_OS_RELEASE_EXISTS FALSE)
+    SET(LINUX_ID "Unknown")
+    SET(LINUX_CODENAME "Unknown")
+    SET(LINUX_VERSION_ID "Unknown")
+    IF (EXISTS "/etc/os-release")
+        SET(LINUX_ETC_OS_RELEASE_EXISTS TRUE)
+        # Gather info about Linux distro and release (if applicable) for later use down below.
+        EXECUTE_PROCESS(
+            COMMAND sh -c "cat /etc/os-release | grep ^ID= | sed 's/^ID=//' | tr -d '\"\n'"
+            OUTPUT_VARIABLE LINUX_ID)
+        EXECUTE_PROCESS(
+            COMMAND sh -c "cat /etc/os-release | grep ^VERSION_CODENAME= | sed 's/^VERSION_CODENAME=//' | tr -d '\"\n'"
+            OUTPUT_VARIABLE LINUX_CODENAME)
+        EXECUTE_PROCESS(
+            COMMAND sh -c "cat /etc/os-release | grep ^VERSION_ID= | sed 's/^VERSION_ID=//' | tr -d '\"\n'"
+            OUTPUT_VARIABLE LINUX_VERSION_ID)
+    ENDIF (EXISTS "/etc/os-release")
 
-# Lookup the LSB Data - this is known for Ubuntu, but also used by other distros too.
-# and may help with distro-sub-selection
-FIND_PROGRAM(LSB_RELEASE_EXEC lsb_release)
-IF (LSB_RELEASE_EXEC)
-	EXECUTE_PROCESS(COMMAND ${LSB_RELEASE_EXEC} --codename --short
-		OUTPUT_VARIABLE LSB_LINUX_DISTRIBUTION_CODENAME
-		OUTPUT_STRIP_TRAILING_WHITESPACE)
-	EXECUTE_PROCESS(COMMAND ${LSB_RELEASE_EXEC} --id --short
-		OUTPUT_VARIABLE LSB_LINUX_DISTRIBUTION
-		OUTPUT_STRIP_TRAILING_WHITESPACE)
-	SET(LSB_EXISTS TRUE)
-	MESSAGE("Found Linux Distribution Release Name: ${LSB_LINUX_DISTRIBUTION_CODENAME}")
-ENDIF (LSB_RELEASE_EXEC)
+    # lsb_release -i --> but then it'll find `Ubuntu` instead of Debian so will have to map more
 
-# Definitively detect Debian Package Build tools
-FIND_PROGRAM(VS_CAN_BUILD_DEB NAMES dpkg-buildpackage)
+    # we can probably more reliably detect via checking for different packager commands
+    # `lsb_release` doesn't always work correctly, so it's easier to read the underlying file
+    # if it exists. It doesn't always exist. Debian doesn't use it, but Ubuntu does.
+    SET(LSB_EXISTS FALSE)
+    SET(LSB_LINUX_DISTRIBUTION "Unknown")
+    SET(LSB_LINUX_DISTRIBUTION_CODENAME "Unknown")
 
-# Definitively detect RPM Package Build Tools
-FIND_PROGRAM(VS_CAN_BUILD_RPM rpmbuild)
+    # Lookup the LSB Data - this is known for Ubuntu, but also used by other distros too.
+    # and may help with distro-sub-selection
+    FIND_PROGRAM(LSB_RELEASE_EXEC lsb_release)
+    IF (LSB_RELEASE_EXEC)
+        EXECUTE_PROCESS(COMMAND ${LSB_RELEASE_EXEC} --codename --short
+            OUTPUT_VARIABLE LSB_LINUX_DISTRIBUTION_CODENAME
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+        EXECUTE_PROCESS(COMMAND ${LSB_RELEASE_EXEC} --id --short
+            OUTPUT_VARIABLE LSB_LINUX_DISTRIBUTION
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+        SET(LSB_EXISTS TRUE)
+        MESSAGE("Found Linux Distribution Release Name: ${LSB_LINUX_DISTRIBUTION_CODENAME}")
+    ENDIF (LSB_RELEASE_EXEC)
+
+    # Definitively detect Debian Package Build tools
+    FIND_PROGRAM(VS_CAN_BUILD_DEB NAMES dpkg-buildpackage)
+
+    # Definitively detect RPM Package Build Tools
+    FIND_PROGRAM(VS_CAN_BUILD_RPM rpmbuild)
+
+ENDIF (CMAKE_SYSTEM_NAME STREQUAL Linux)
