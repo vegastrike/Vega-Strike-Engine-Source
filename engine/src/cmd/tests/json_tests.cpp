bcc21994723333ad0783ca2936accff7cc45bc3e
Roy Falk <roy@falk.co.il>
2022-05-28 20:56:29 +0300

    Remove CSV testing in JSON file

--- engine/src/cmd/tests/json_tests.cpp
+++ engine/src/cmd/tests/json_tests.cpp
@@ -1,5 +1,5 @@
 /*
- * unit_generic.cpp
+ * json_tests.cpp
  *
  * Copyright (C) 2021 Roy Falk
  * Copyright (C) 2022 Stephen G. Tuggy
@@ -45,44 +45,4 @@ TEST(JSON, Sanity) {
     std::map<std::string, std::map<std::string, std::string>> json_units;
 
     UnitJSONFactory::ParseJSON("../src/cmd/tests/units.csv");
-
-    std::ifstream ifs("../src/cmd/tests/units.csv", std::ifstream::in);
-    std::stringstream buffer;
-    buffer << ifs.rdbuf();
-
-    UnitCSVFactory factory;
-    factory.ProcessCSV(buffer.str(), false);
-
-    /*std::cerr << UnitCSVFactory::units.size() << "\n";
-    std::cerr << UnitCSVFactory::json_units.size() << "\n";
-
-    for(const auto& csv_unit : json_units) {
-        const std::string unit_name = csv_unit.first;
-        std::cerr << unit_name << "\n";
-    }*/
-
-    /*for(const auto& csv_unit : UnitCSVFactory::units) {
-        const std::string unit_name = csv_unit.first;
-        std::cerr << unit_name << "\n";
-
-        //if(UnitCSVFactory::json_units.count(unit_name) != 1) {
-        //    std::cerr << unit_name << " key is missing\n";
-        //    continue;
-        //}
-
-        for(const auto& attribute : csv_unit.second) {
-            //std::cerr << attribute.first << "\n";
-
-            std::string attribute_name = attribute.first;
-            std::string csv_attribute = attribute.second;
-
-            std::string json_attribute = json_units[unit_name][attribute_name];
-
-            if(csv_attribute != json_attribute) {
-                std::cerr << unit_name << " : " << attribute_name << " => " << csv_attribute << " != " << json_attribute << "\n";
-            } else {
-                //std::cerr << unit_name << " : " << attribute_name << " => " << csv_attribute << " == " << json_attribute << "\n";
-            }
-        }
-    }*/
 }
