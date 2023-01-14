ecfe0d3cbc94a3910d47c9a00f95e5546ae77eef
Roy Falk <roy@falk.co.il>
2023-01-14 09:48:30 +0200

    Fix windows build break
    Add copyright notice

--- engine/src/cmd/upgradeable_unit.h
+++ engine/src/cmd/upgradeable_unit.h
@@ -1,3 +1,29 @@
+/*
+ * upgradeable_unit.h
+ *
+ * Copyright (C) 2001-2023 Daniel Horn, Benjaman Meyer, Roy Falk, Stephen G. Tuggy,
+ * and other Vega Strike contributors.
+ *
+ * https://github.com/vegastrike/Vega-Strike-Engine-Source
+ *
+ * This file is part of Vega Strike.
+ *
+ * Vega Strike is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vega Strike is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Vega Strike. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+// -*- mode: c++; c-basic-offset: 4; indent-tabs-mode: nil -*-
+
 #ifndef UPGRADEABLEUNIT_H
 #define UPGRADEABLEUNIT_H
 
