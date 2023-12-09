af9964d4e3ba20e218a62eaecb8168171271b4bd
Stephen G Tuggy <sgt@stephengtuggy.com>
2023-12-09 04:38:44 -0800

    Add .github/dependabot.yml and script/bootstrap-on-macos.sh; update Brewfile and .github/workflows/macos-ci.yml

--- Brewfile
+++ Brewfile
@@ -1,3 +1,6 @@
+tap "homebrew/core"
+tap "homebrew/bundle"
+tap "homebrew/cask"
 brew "openssl@3", link:false
 brew "python@3.12", link:false
 brew "boost", link:false
@@ -9,4 +12,9 @@ brew "mesa"
 brew "mesa-glu"
 brew "freeglut"
 brew "openal-soft"
+brew "cmake"
+brew "expat"
+brew "jpeg"
+brew "libpng"
+brew "libvorbis"
 
---
+++ script/bootstrap-on-macos.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# bootstrap-on-macos.sh
+#
+# Copyright (C) 2023 Stephen G. Tuggy and other Vega Strike contributors
+#
+# This file is part of Vega Strike.
+#
+# Vega Strike is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Vega Strike is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Vega Strike.  If not, see <https://www.gnu.org/licenses/>.
+
+set -e
+
+export HOMEBREW_NO_AUTO_UPDATE=1
+export HOMEBREW_NO_INSTALL_UPGRADE=1
+export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=1
+
+declare -a keg_only_packages=("python@3.12" "boost" "boost-python3")
+
+for i in "${keg_only_packages[@]}"
+do
+    PACKAGE_INSTALLED_PREFIX=$(brew --prefix "$i")
+    PACKAGE_INSTALLED_BIN="$PACKAGE_INSTALLED_PREFIX/bin"
+    export PATH="$PACKAGE_INSTALLED_BIN:$PATH"
+    export CMAKE_PREFIX_PATH="$PACKAGE_INSTALLED_PREFIX:$CMAKE_PREFIX_PATH"
+done
+
+brew update
+brew bundle install --file=./Brewfile
+
+ln -s /usr/local/include/GL /usr/local/include/OpenGL
+ln -s /usr/local/include/GL /usr/local/include/GLUT
+export OPENALDIR=$(brew --prefix openal-soft)
