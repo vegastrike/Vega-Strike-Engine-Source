6340e27072dd6eb2f191bbf62e65b93d85e01f93
Roy Falk <roy@falk.co.il>
2024-12-26 07:47:13 +0200

    Use boost for JSON parsing (#923)
    * Use boost for JSON parsing
    
    - Add boost-json to bootstrap
    - Remove Suse 15.2 and 15.3 - too old.
    - Fedora versions before 39 are possibly too old to support boost json. There's no version history because these versions are out of support.
    - Remove support for Jammy and Focal
    - Make GitHub.ci run bootstrap
    - code ql - restrict to run on ubuntu 24.04. ubuntu-latest includes focal and jammy.
    - gh-actions-pr.yml - add sudo to bootstrap
    - vcpkg.json - add json. Prevent fail on windows
    - Upgrade from jammy to noble in github-actions.
    - Remove old OSes.
    - Fix cmake to use python38 instead of python3
    - Disable rocky Linux due to bug
    - Fix bookworm libboost-program-options to use version 1.81 and not 1.74

---
+++ engine/src/resource/json_utils.h
@@ -0,0 +1,57 @@
+/*
+ * json_utils.h
+ *
+ * Copyright (C) 2001-2024 Daniel Horn, Benjamen Meyer, Roy Falk, Stephen G. Tuggy,
+ * and other Vega Strike contributors.
+ *
+ * https://github.com/vegastrike/Vega-Strike-Engine-Source
+ *
+ * This file is part of Vega Strike.
+ *
+ * Vega Strike is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vega Strike is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Vega Strike. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef VEGA_STRIKE_ENGINE_RESOURCE_JSON_UTILS_H
+#define VEGA_STRIKE_ENGINE_RESOURCE_JSON_UTILS_H
+
+#include <string>
+#include <boost/json.hpp>
+
+// Use when default value already defined
+template<typename T>
+void ConditionalJsonGet(boost::json::object& object, 
+                          T& value, 
+                          const std::string& key) {
+    if (object.if_contains(key)) {
+        value = boost::json::value_to<T>(object.at(key));
+    }
+}
+
+// Use when default value is not defined
+template<typename T>
+const T JsonGetWithDefault(boost::json::object object, 
+                              const std::string& key, 
+                              const T default_value) {
+    if (object.if_contains(key)) {
+        return boost::json::value_to<T>(object.at(key));
+    }
+
+    return default_value;
+}
+
+const std::string JsonGetStringWithDefault(boost::json::object object, 
+                                           const std::string& key, 
+                                           const char* default_value);
+
+#endif //VEGA_STRIKE_ENGINE_RESOURCE_JSON_UTILS_H
