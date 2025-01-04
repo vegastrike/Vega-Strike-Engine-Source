3d21db4366e90e1b5995aec2ee62f60b57d87497
Stephen G. Tuggy <sgt@stephengtuggy.com>
2025-01-04 01:01:01 -0800

    faction_generic.cpp, gfxlib_struct.cpp: Fix some warnings

--- engine/src/python/init.cpp
+++ engine/src/python/init.cpp
@@ -1,7 +1,7 @@
 /*
  * init.cpp
  *
- * Copyright (C) 2001-2023 Daniel Horn, pyramid3d, Stephen G. Tuggy,
+ * Copyright (C) 2001-2025 Daniel Horn, pyramid3d, Stephen G. Tuggy,
  * and other Vega Strike contributors.
  *
  * https://github.com/vegastrike/Vega-Strike-Engine-Source
