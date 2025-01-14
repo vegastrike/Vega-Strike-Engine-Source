17fc89458fb5e9951b02e9b075ecfa720716a63a
Stephen G. Tuggy <sgt@stephengtuggy.com>
2025-01-13 20:09:43 -0800

    *.py: Name Daniel Horn as the `project creator`, not simply the `creator`

--- engine/objconv/blender_xmesh_export.py
+++ engine/objconv/blender_xmesh_export.py
@@ -4,7 +4,7 @@
 #
 # Vega Strike - Space Simulation, Combat and Trading
 # Copyright (C) 2001-2025 The Vega Strike Contributors:
-# Creator: Daniel Horn
+# Project creator: Daniel Horn
 # Original development team: As listed in the AUTHORS file
 # Current development team: Roy Falk, Benjamen R. Meyer, Stephen G. Tuggy
 #
