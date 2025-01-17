12c7b17283872a7524c49f4e985eecbfebe0fc67
Stephen G. Tuggy <sgt@stephengtuggy.com>
2025-01-16 18:04:01 -0800

    unit_json_factory.cpp: When reading, the root of the file JSON is an object now, not an array. Had to adjust for that

--- engine/src/cmd/unit_json_factory.cpp
+++ engine/src/cmd/unit_json_factory.cpp
@@ -24,15 +24,11 @@
 #include "unit_json_factory.h"
 #include "unit_csv_factory.h"
 
-#include <iostream>
 #include <fstream>
-#include <sstream>
 #include <vector>
 #include <string>
 #include <map>
 #include <boost/json.hpp>
-
-#include "resource/json_utils.h"
 
 
 void UnitJSONFactory::ParseJSON(VSFileSystem::VSFile &file, bool player_ship) {
@@ -44,13 +40,13 @@ void UnitJSONFactory::ParseJSON(VSFileSystem::VSFile &file, bool player_ship) {
     } catch (std::exception const& e) {
         VS_LOG_FLUSH_EXIT(fatal, (boost::format("Error parsing JSON in UnitJSONFactory::ParseJSON(): %1%") % e.what()), 42);
     }
-    boost::json::array root_array = json_value.get_array();
+    boost::json::object root_json_object = json_value.as_object();
 
-    for(boost::json::value& unit_value : root_array) {
-        boost::json::object unit_object = unit_value.get_object();
+    for (boost::json::key_value_pair & unit_kvp : root_json_object) {
+        boost::json::object unit_object = unit_kvp.value().get_object();
         std::map<std::string, std::string> unit_attributes{};
 
-        for(boost::json::key_value_pair& pair : unit_object) {
+        for (boost::json::key_value_pair& pair : unit_object) {
             const std::string value = boost::json::value_to<std::string>(pair.value());
             unit_attributes[pair.key()] = value;
         }
@@ -58,7 +54,7 @@ void UnitJSONFactory::ParseJSON(VSFileSystem::VSFile &file, bool player_ship) {
         // Add root
         unit_attributes["root"] = file.GetRoot();  
 
-        if(player_ship) {
+        if (player_ship) {
             UnitCSVFactory::units["player_ship"] = unit_attributes;
         } else {
             UnitCSVFactory::units[unit_attributes["Key"]] = unit_attributes;
