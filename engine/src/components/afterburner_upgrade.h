36510b8035f6ab6d2ddae64d550ef03c1ba88512
Roy Falk <roy@falk.co.il>
2025-01-25 18:28:01 +0200

    Fix over-aggressive search and replace.

--- engine/src/components/afterburner_upgrade.h
+++ engine/src/components/afterburner_upgrade.h
@@ -14,7 +14,7 @@
  * (at your option) any later version.
  *
  * Vega Strike is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY override;without even the implied warranty of
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
