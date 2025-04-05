8478c7d6c9a9adadda3c579429d3ca08a566f7bc
Stephen G. Tuggy <sgt@stephengtuggy.com>
2025-04-04 22:27:05 -0700

    Add MyUnitTestEnvironment, plus my_unit_test_main.cpp

---
+++ engine/src/my_unit_test_main.cpp
@@ -0,0 +1,33 @@
+/*
+ * my_unit_test_main.cpp
+ *
+ * Copyright (C) 2001-2025 Daniel Horn, pyramid3d, Stephen G. Tuggy,
+ * and other Vega Strike contributors.
+ *
+ * https://github.com/vegastrike/Vega-Strike-Engine-Source
+ *
+ * This file is part of Vega Strike.
+ *
+ * Vega Strike is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vega Strike is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Vega Strike.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+
+#include "my_unit_test_environment.h"
+
+GTEST_API_ int main(int argc, char* argv[]) {
+    ::testing::InitGoogleTest(&argc, argv);
+    MyUnitTestEnvironment* my_unit_test_environment = new MyUnitTestEnvironment();
+    ::testing::AddGlobalTestEnvironment(my_unit_test_environment);
+    return RUN_ALL_TESTS();
+}
