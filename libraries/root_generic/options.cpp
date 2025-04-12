3161e1704fb261dfe22bd2b231fc3dad993d7088
Stephen G Tuggy <sgt@stephengtuggy.com>
2025-04-12 15:35:58 -0700

    options.cpp/.h: Omit 4 settings that have moved to `configuration()`

--- libraries/root_generic/options.cpp
+++ libraries/root_generic/options.cpp
@@ -143,10 +143,6 @@ void vs_options::init() {
             XMLSupport::parse_bool(vs_config->getVariable("graphics", "separatespecularcolor", "false"));
     LockVertexArrays = XMLSupport::parse_bool(vs_config->getVariable("graphics", "LockVertexArrays", "true"));
     fogdetail = XMLSupport::parse_int(vs_config->getVariable("graphics", "fogdetail", "0"));
-    // fov = XMLSupport::parse_floatf(vs_config->getVariable("graphics", "fov", "78"));
-    // aspect = XMLSupport::parse_floatf(vs_config->getVariable("graphics", "aspect", "1.33"));
-    // znear = XMLSupport::parse_floatf(vs_config->getVariable("graphics", "znear", "1"));
-    // zfar = XMLSupport::parse_floatf(vs_config->getVariable("graphics", "zfar", "100000"));
     ModelDetail = XMLSupport::parse_floatf(vs_config->getVariable("graphics", "ModelDetail", "1"));
     UseTextures = XMLSupport::parse_bool(vs_config->getVariable("graphics", "UseTextures", "true"));
     UseShipTextures = XMLSupport::parse_bool(vs_config->getVariable("graphics", "UseShipTextures", "false"));
