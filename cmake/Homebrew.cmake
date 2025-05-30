c2ec98f83ae4ebc348f55eba9e566c262fa65bd4
Stephen G Tuggy <sgt@stephengtuggy.com>
2025-05-29 20:04:40 -0700

    Add Homebrew.cmake and MacPorts.cmake, and begin using them
    Signed-off-by: Stephen G Tuggy <sgt@stephengtuggy.com>

---
+++ cmake/Homebrew.cmake
@@ -0,0 +1,11 @@
+EXECUTE_PROCESS(COMMAND brew --prefix
+                OUTPUT_VARIABLE HomebrewPrefixOut
+                ERROR_VARIABLE HomebrewPrefixErr)
+SET(CMAKE_PREFIX_PATH $HomebrewPrefixOut)
+SET(CMAKE_FIND_FRAMEWORK CACHE "LAST")
+SET(PythonInstalledVia CACHE "Homebrew")
+
+EXECUTE_PROCESS(COMMAND homebrew --prefix libarchive
+                OUTPUT_VARIABLE HOMEBREW_LIBARCHIVE_PREFIX
+                ERROR_VARIABLE HOMEBREW_LIBARCHIVE_PREFIX_ERR)
+SET(PKG_CONFIG_PATH "${HOMEBREW_LIBARCHIVE_PREFIX}/lib/pkgconfig:${PKG_CONFIG_PATH}")
---
+++ cmake/MacPorts.cmake
@@ -0,0 +1,14 @@
+SET(CMAKE_PREFIX_PATH "/opt/local")
+SET(Boost_INCLUDE_DIR CACHE "/opt/local/libexec/boost/1.87/include")
+SET(Boost_DIR CACHE "/opt/local/libexec/boost/1.87/lib/cmake/Boost-1.87.0")
+SET(CMAKE_FIND_FRAMEWORK CACHE "LAST")
+SET(PythonInstalledVia CACHE "MacPorts")
+
+SET(INSTALL_GTEST CACHE "OFF")
+SET(USE_GTEST CACHE "OFF")
+
+# Is this correct?
+SET(MacPortsLibArchivePrefix "/opt/local/")
+
+SET(Boost_INCLUDE_DIR "/opt/local/libexec/boost/1.87/include")
+SET(Boost_DIR "/opt/local/libexec/boost/1.87/lib/cmake/Boost-1.87.0")
