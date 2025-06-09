1b30f716ef38e44fe1200d765b0874a25363770a
Stephen G Tuggy <sgt@stephengtuggy.com>
2025-06-09 12:37:49 -0700

    Update the copyleft notice at the top of each remaining source file under engine/src
    Signed-off-by: Stephen G Tuggy <sgt@stephengtuggy.com>

--- engine/src/common/common.h
+++ engine/src/common/common.h
@@ -1,20 +1,30 @@
-/***************************************************************************
-*                          common.h  -  description
-*                             -------------------
-*    begin                : Wed Jun 26 2002
-*    copyright            : (C) 2002 by jhunt
-*    email                : jhunt@jaja
-*    copyright            : (C) 2023 by Stephen G. Tuggy, Benjamen R. Meyer
-***************************************************************************/
-
-/***************************************************************************
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-***************************************************************************/
+/*
+ * common.h
+ *
+ * Vega Strike - Space Simulation, Combat and Trading
+ * Copyright (C) 2001-2025 The Vega Strike Contributors:
+ * Project creator: Daniel Horn
+ * Original development team: As listed in the AUTHORS file. Specifically: jhunt
+ * Current development team: Roy Falk, Benjamen R. Meyer, Stephen G. Tuggy
+ *
+ *
+ * https://github.com/vegastrike/Vega-Strike-Engine-Source
+ *
+ * This file is part of Vega Strike.
+ *
+ * Vega Strike is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vega Strike is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Vega Strike.  If not, see <https://www.gnu.org/licenses/>.
+ */
 // NO HEADER GUARD
 
 
