aa75708d99083f99062048886fc05c076b611877
Stephen G. Tuggy <sgt@stephengtuggy.com>
2025-08-28 13:37:00 -0700

    drawgalaxy.cpp: Peer review -- M_PI instead of kPi

--- engine/src/gfx/nav/drawgalaxy.cpp
+++ engine/src/gfx/nav/drawgalaxy.cpp
@@ -68,8 +68,6 @@
 using std::string;
 using std::vector;
 
-constexpr double kPi = 3.1415926536;
-
 float SYSTEM_DEFAULT_SIZE = 0.02;
 const int systemambiguous = 0;
 static GFXColor GrayColor(.5, .5, .5, .5);
@@ -291,7 +289,7 @@ QVector NavigationSystem::SystemIterator::Position() {
         }
         k %= 200000;
 //float y = (k-100000)/(200000.);
-        return QVector(ratio * cos(locatio * 2 * kPi), ratio * sin(locatio * 2 * kPi), 0)
+        return QVector(ratio * cos(locatio * 2 * M_PI), ratio * sin(locatio * 2 * M_PI), 0)
                 * screensmash;
     }
 }
