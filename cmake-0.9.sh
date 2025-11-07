4b6ec53ef4f45b217c7e3a4d08365d8e5b14cf02
pmx <pmx@free.fr>
2025-11-08 00:02:54 +0100

    Removed bash scripts used fot local management of the build system

--- cmake-0.9.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# CMAKE_MODULE_PATH="${HOME}/.local/lib/"
-# echo "CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}"
-# cmake -DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH} -S ./ -B ./build/linux-ninja-pie-disabled-glvnd-debug --preset linux-ninja-pie-disabled-glvnd-debug 
-cmake --fresh -S engine -B ./build/linux-pie-disabled-glvnd-debug --preset linux-pie-disabled-glvnd-debug 
-
-
