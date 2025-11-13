7ff4d64e9d1dd8cfd33762e2b04ff0c729d8b6c6
Stephen G. Tuggy <sgt@stephengtuggy.com>
2025-11-12 16:27:11 -0800

    engine/objconv/blender_xmesh_import.py: Fix a Python syntax error: `+except:` instead of just `except:`

--- engine/objconv/blender_xmesh_import.py
+++ engine/objconv/blender_xmesh_import.py
@@ -40,7 +40,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 from __future__ import print_function
 try:
 	range = xrange
-+except:
+except:
 	pass
 
 import Blender
