ccdaea17c19ffc9655b20c13cdd90a5e965b814b
Stephen G. Tuggy <sgt@stephengtuggy.com>
2025-11-15 00:25:35 -0800

    Update CMake and vcpkg files
    Signed-off-by: Stephen G Tuggy <sgt@stephengtuggy.com>

--- cmake/MacPorts.cmake
+++ cmake/MacPorts.cmake
@@ -3,10 +3,11 @@ SET(Boost_INCLUDE_DIR "/opt/local/libexec/boost/1.87/include" CACHE STRING "Boos
 SET(Boost_DIR "/opt/local/libexec/boost/1.87/lib/cmake/Boost-1.87.0" CACHE STRING "Boost root directory")
 SET(CMAKE_FIND_FRAMEWORK "LAST")
 SET(CMAKE_FIND_APPBUNDLE "LAST")
-SET(PythonInstalledVia "MacPorts" CACHE STRING "How Python was installed, such as via Homebrew or MacPorts")
 
 SET(INSTALL_GTEST CACHE "OFF")
-SET(USE_GTEST CACHE "OFF")
+SET(USE_GTEST CACHE "ON")
+SET(INSTALL_SDL3 CACHE "OFF")
+SET(INSTALL_SDL3_IMAGE CACHE "OFF")
 
 # Is this correct?
 SET(MacPortsLibArchivePrefix CACHE "/opt/local/libarchive/")
