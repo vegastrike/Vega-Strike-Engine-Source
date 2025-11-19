2cb2f9778d467f6952f2abdb8d838cd506df710d
Stephen G. Tuggy <sgt@stephengtuggy.com>
2025-11-19 09:34:05 -0800

    basecomputer.cpp: Reintroduce if block

--- engine/src/cmd/basecomputer.cpp
+++ engine/src/cmd/basecomputer.cpp
@@ -540,17 +540,19 @@ void BaseComputer::constructControls(void) {
         return;
     }
 
-    //Base info title.
-    StaticDisplay *baseTitle = vega_dynamic_cast_ptr<StaticDisplay>(getControl(controls["baseTitle"]));
-    window()->addControl(baseTitle);
+    if (m_displayModes.size() != 1) {
+        //Base info title.
+        StaticDisplay *baseTitle = vega_dynamic_cast_ptr<StaticDisplay>(getControl(controls["baseTitle"]));
+        window()->addControl(baseTitle);
 
-    //Player info title.
-    StaticDisplay *playerTitle = vega_dynamic_cast_ptr<StaticDisplay>(getControl(controls["playerTitle"]));
-    window()->addControl(playerTitle);
+        //Player info title.
+        StaticDisplay *playerTitle = vega_dynamic_cast_ptr<StaticDisplay>(getControl(controls["playerTitle"]));
+        window()->addControl(playerTitle);
 
-    //Options button.
-    NewButton *options = vega_dynamic_cast_ptr<NewButton>(getControl(controls["saveLoad"]));
-    window()->addControl(options);
+        //Options button.
+        NewButton *options = vega_dynamic_cast_ptr<NewButton>(getControl(controls["saveLoad"]));
+        window()->addControl(options);
+    }
 
     NewButton *done = vega_dynamic_cast_ptr<NewButton>(getControl(controls["done"]));
     window()->addControl(done);
