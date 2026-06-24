90dd8b959dd255f1e53fadf4c1a84002c3f8edeb
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-06-24 11:37:15 -0700

    vs_math.h: Add function `equal_within_ulps`

--- engine/src/vs_math.h
+++ engine/src/vs_math.h
@@ -55,5 +55,29 @@
 #define FINITE(x) ( std::isfinite( x ) )
 #define ISNAN(x) ( std:: isnan( x ) )
 
+// From example given at https://en.cppreference.com/cpp/types/numeric_limits/epsilon . Retrieved 2026-06-24.
+template <class T>
+std::enable_if_t<not std::numeric_limits<T>::is_integer, bool>
+equal_within_ulps(T x, T y, std::size_t n)
+{
+    // Since `epsilon()` is the gap size (ULP, unit in the last place)
+    // of floating-point numbers in interval [1, 2), we can scale it to
+    // the gap size in interval [2^e, 2^{e+1}), where `e` is the exponent
+    // of `x` and `y`.
+
+    // If `x` and `y` have different gap sizes (which means they have
+    // different exponents), we take the smaller one. Taking the bigger
+    // one is also reasonable, I guess.
+    const T m = std::min(std::fabs(x), std::fabs(y));
+
+    // Subnormal numbers have fixed exponent, which is `min_exponent - 1`.
+    const int exp = m < std::numeric_limits<T>::min()
+                  ? std::numeric_limits<T>::min_exponent - 1
+                  : std::ilogb(m);
+
+    // We consider `x` and `y` equal if the difference between them is
+    // within `n` ULPs.
+    return std::fabs(x - y) <= n * std::ldexp(std::numeric_limits<T>::epsilon(), exp);
+}
 
 #endif //VEGA_STRIKE_ENGINE_VS_MATH_H
