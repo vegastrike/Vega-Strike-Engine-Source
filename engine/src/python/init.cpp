a5af367dd7e858b2ee46f5e2988f67d4aa25f7b2
Stephen G Tuggy <sgt@stephengtuggy.com>
2026-07-13 16:45:27 -0700

    engine/src/python/init.cpp: Try `py_pre_config.coerce_c_locale = 1`

--- engine/src/python/init.cpp
+++ engine/src/python/init.cpp
@@ -175,8 +175,8 @@ void Python::init() {
     PyPreConfig py_pre_config;
     PyPreConfig_InitPythonConfig(&py_pre_config);
 
-    // Is this right? Shouldn't it be 1 or 2? -- Stephen G. Tuggy 2026-07-13
-    py_pre_config.coerce_c_locale = 0;
+    // Is this right? Should it be 0, 1, 2, or -1? -- Stephen G. Tuggy 2026-07-13
+    py_pre_config.coerce_c_locale = 1;
 
     PyStatus status;
 
