vegastrike/Vega-Strike-Engine-Source

commit ca3df7f9cd051afdd405bb695a5538ac8bfc1626

Implement hull, armor and shield components (#1016)
* Remove 8 facet support for armor and shield

Remove extra text in ship_view from the C++ end of things (basecomputer)

* Implement hull, armor and shield components

- Delete Health class. Very similar to Resource.
- Move functions to components - e.g. Percent, Get,
- Move shield functionality like AdjustPower from DamageableLayer to Shield class
- Disable enhance shields from collision. This should probably be handled by a python script.
- Delete many functions that are now unused.
- Delete x4 sparkling missile
- Delete planet.cpp fawg weird hull value
- Delete incorrect GetHullPercent by planet. For now it can be damaged and destroyed.
- Move some python API from macros to actual code.
- Fix issue in energy_container, where Capacitor and FTL Capacitor were not saved.
- Add two shield factors in configuration. Currently not read from file.

* Fix bug in units.json and ships.json reading.

If file doesn't open, game crashes.
Polish the code a little.

* Tweak the shield modifiers to make the game work better.

This stops the shields from eating all the reactor capacity.

* Make upgrade view take unit stats into account.

Upgrades installed on ship now display damage.
Fix bug #1019.
Convert Radar_Range to int before saving - prevent exponent format for upgrade view.
Fix issue in armor and shield, where Resource was initiated without max value. Use proper string constructor instead.
Fix issue in armor, hull and shield, where PercentOperational did not use actual libdamage damage.
Fix issue in EnergyContainer applied modifier in the wrong direction. Also, use Serialize to save all three values.
Fix issue in Reactor, where Serialize was not used.

* Add support for new lib component style upgrade repair

Add integral components to ship - hull, afterburner, drive and FTL drive.
Fix incorrect reporting of operational as 0-100 and not 0-1.

engine/src/components/shield.cpp
@@ -1,0 +1,402 @@
+/*
+ * shield.cpp
+ *
+ * Copyright (C) 2001-2023 Daniel Horn, Benjamen Meyer, Roy Falk, Stephen G. Tuggy,
+ * and other Vega Strike contributors.
+ *
+ * https://github.com/vegastrike/Vega-Strike-Engine-Source
+ *
+ * This file is part of Vega Strike.
+ *
+ * Vega Strike is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Vega Strike is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Vega Strike. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "shield.h"
+#include "ftl_drive.h"
+#include "cloak.h"
+#include "unit_csv_factory.h"
+#include "configuration/configuration.h"
+#include "vs_logging.h"
+#include "damage/damage.h"
+
+#include <boost/format.hpp>
+
+int Shield::front = 0;
+int Shield::back = 1;
+int Shield::left = 2;
+int Shield::right = 3;
+
+static const Damage normal_damage = Damage(1.0,0.0);
+
+Shield::Shield(EnergyContainer *source, FtlDrive *ftl_drive, Cloak *cloak, FacetConfiguration configuration) : 
+    Component(),
+    EnergyConsumer(source, true, 0),
+    DamageableLayer(2, configuration, 0.0, normal_damage, false),
+    ftl_drive(ftl_drive), cloak(cloak), 
+    regeneration(Resource<double>(0.0,0.0,0.0)), 
+    max_power(Resource<double>(1.0,0.0,1.0)) {
+    type = ComponentType::Shield;
+}
+
+
+// Component Methods
+void Shield::Load(std::string unit_key) {
+    Component::Load(unit_key);
+    
+    // Load shield
+    // Some basic shield variables
+    // TODO: lib_damage figure out how leak and efficiency work
+    //char leak = static_cast<char>(UnitCSVFactory::GetVariable(unit_key, "Shield_Leak", 0.0f) * 100);
+    //double efficiency = UnitCSVFactory::GetVariable(unit_key, "Shield_Efficiency", 1.0f );
+    
+    const std::string regen_string = UnitCSVFactory::GetVariable(unit_key, "Shield_Recharge", std::string());
+    regeneration = Resource<double>(regen_string);
+
+    // We support 3 options:
+    // 1. Minimized shield_strength = x (single value). 
+    // 2. New detailed shield (Front, back, left, right).
+    // 3. Old detailed (Front-left-top, ...). 4/2 facets converted to 4/2. 
+
+    // Get shield count
+    std::map<std::string, float> shield_sections{};
+    std::vector<std::string> shield_string_values{};
+    
+    const std::string shield_strength_string = UnitCSVFactory::GetVariable(unit_key, "shield", std::string());
+    const std::string shield_facets_string = UnitCSVFactory::GetVariable(unit_key, "shield_facets", std::string());
+
+    if(!shield_facets_string.empty()) {
+        try {
+            number_of_facets = std::stoi(shield_facets_string);
+        } catch (std::invalid_argument const& ex) {
+            VS_LOG(error, (boost::format("%1%: %2% trying to convert shield_facets_string '%3%' to int") % __FUNCTION__ % ex.what() % shield_facets_string));
+            number_of_facets = 1;
+        } catch (std::out_of_range const& ex) {
+            VS_LOG(error, (boost::format("%1%: %2% trying to convert shield_facets_string '%3%' to int") % __FUNCTION__ % ex.what() % shield_facets_string));
+            number_of_facets = 1;
+        }
+    }
+
+    // Single value (short form)
+    if(!shield_strength_string.empty()) {
+        try {
+            Resource<double> facet_strength = Resource<double>(shield_strength_string);
+            facets = std::vector<Resource<double>>(number_of_facets, 
+                facet_strength);
+            
+            CalculatePercentOperational();
+            return;
+        } catch (std::invalid_argument const& ex) {
+            VS_LOG(error, (boost::format("%1%: %2% trying to convert shield_strength_string '%3%' to int") % __FUNCTION__ % ex.what() % shield_strength_string));
+        } catch (std::out_of_range const& ex) {
+            VS_LOG(error, (boost::format("%1%: %2% trying to convert shield_strength_string '%3%' to int") % __FUNCTION__ % ex.what() % shield_strength_string));
+        }
+    } 
+    
+    // Try new longform
+    if(!shield_facets_string.empty() && 
+       (number_of_facets == 4 || number_of_facets == 2)) { 
+        std::vector<Resource<double>> shield_values{};
+        std::string shield_keys[] = {"shield_front", "shield_back",
+            "shield_left", "shield_right"};
+        
+        // Using old form for loop to support 2/4 shields.
+        for (int i = 0; i < number_of_facets; ++i) {
+            // TODO: this can be taken out to a separate utility function, shared to hull, armor and shield
+            // also below in old form. 
+            // Maybe. If we read numbers instead of strings... no need.
+            const std::string shield_string_value = UnitCSVFactory::GetVariable(unit_key, shield_keys[i], std::string());
+            if (shield_string_value.empty()) {
+                shield_values.push_back(Resource<double>(0.0));
+                continue;
+            } 
+
+            try {
+                Resource<double> facet_strength = Resource<double>(shield_string_value);
+                shield_values.push_back(facet_strength);
+                continue;
+            } catch (const std::invalid_argument& ex) {
+                VS_LOG(error, (boost::format("%1%: Unable to convert shield value '%2%' to a number: %3%") % __FUNCTION__ % shield_string_value % ex.what()));
+            } catch (std::out_of_range const& ex) {
+                VS_LOG(error, (boost::format("%1%: Unable to convert shield value '%2%' to a number: %3%") % __FUNCTION__ % shield_string_value % ex.what()));
+            }
+            shield_values.push_back(Resource<double>(0.0));
+        }
+
+        facets = shield_values;
+        CalculatePercentOperational();
+        return;
+    } 
+
+    // Fallback to old shield_keys
+    int shield_count = 0;
+    std::string shield_keys[] = {"Shield_Front_Top_Right", "Shield_Back_Top_Left", "Shield_Front_Bottom_Right", "Shield_Front_Bottom_Left"};
+    std::vector<Resource<double>> shield_values{};
+
+    try {
+        for (auto &key : shield_keys) {
+            std::string string_value = UnitCSVFactory::GetVariable(unit_key, key, std::string());
+
+            // If no shields present, we should break and go directly to end 
+            // of function
+            if(string_value.empty()) {
+                break;
+            }
+
+            double value = std::stod(string_value);
+            shield_values.push_back(Resource<double>(value, 0.0, value));
+            ++shield_count;
+        }
+
+        if (shield_count == 4 || shield_count == 2) {
+            number_of_facets = shield_count;
+            facets = shield_values;
+            CalculatePercentOperational();
+            return;
+        }
+    } catch (std::exception const& ex) {
+        VS_LOG(error, (boost::format("%1%: %2% Unable to parse shield facets for %3%") % __FUNCTION__ % ex.what() % unit_key));
+    }
+
+    // This should already be set, but good practice to do it anyway.
+    number_of_facets = 0;
+    facets.clear();
+    operational = 0.0;
+}      
+
+        
+
+void Shield::SaveToCSV(std::map<std::string, std::string>& unit) const {
+    // We always save in long form.
+    unit["shield_facets"] = std::to_string(number_of_facets);
+    unit["shield_front"] = facets[0].Serialize();
+    unit["shield_back"] = facets[1].Serialize();
+    unit["shield_left"] = facets[2].Serialize();
+    unit["shield_right"] = facets[3].Serialize();
+
+    //TODO: lib_damage shield leak and efficiency
+    unit["Shield_Leak"] = std::to_string(0); 
+    unit["Shield_Efficiency"] = std::to_string(1); 
+    unit["Shield_Recharge"] = regeneration.Serialize();
+} 
+
+bool Shield::CanDowngrade() const {
+    return installed && !integral;
+}
+
+bool Shield::Downgrade() {
+    if(!CanDowngrade()) {
+        return false;
+    }
+
+    // Component
+    Component::Downgrade();
+    
+    facets.clear();
+    return true;
+}
+
+bool Shield::CanUpgrade(const std::string upgrade_key) const {
+    const int facets = UnitCSVFactory::GetVariable(upgrade_key, "shield_facets", 0.0);
+    return facets == number_of_facets && !integral;
+}
+
+bool Shield::Upgrade(const std::string upgrade_key) {
+    Component::Upgrade(upgrade_key);
+
+    // We only support single value shield
+    const double regen = UnitCSVFactory::GetVariable(upgrade_key, "Shield_Recharge", 0.0);
+    if(regen > 0) {
+        regeneration = Resource<double>(regen, 0.0, regen);
+    } else {
+        return false;
+    }
+
+    facets.clear();
+    const double shield_single_value = UnitCSVFactory::GetVariable(upgrade_key, "shield", 0.0);
+
+    if(shield_single_value > 0.0) {
+        for (unsigned int i = 0; i < number_of_facets; i++) {
+            facets.push_back(Resource<double>(shield_single_value,0,shield_single_value));
+        }
+    }
+
+    return true;
+}
+
+double Shield::PercentOperational() const {
+    return operational.Value();
+}
+
+void Shield::CalculatePercentOperational() {
+    double percent = regeneration.Percent();
+
+    for (Resource<double> &facet : facets) {
+        percent += facet.Percent();
+    }
+
+    // A simple average of regeneration and facets
+    // 4 facet shields assign less importance to regeneration
+    operational = percent / (number_of_facets + 1);
+}
+
+void Shield::Damage() {
+    regeneration.RandomDamage();
+    double percent = regeneration.Percent();
+
+    for (Resource<double> &facet : facets) {
+        facet.RandomDamage();
+        percent += facet.Percent();
+    }
+
+    // A simple average of regeneration and facets
+    // 4 facet shields assign less importance to regeneration
+    operational = percent / (number_of_facets + 1);
+}
+
+void Shield::DamageByPercent(double percent) {
+    regeneration.DamageByPercent(percent);
+    double sum_of_percents = regeneration.Percent();
+
+    for (Resource<double> &facet : facets) {
+        facet.DamageByPercent(percent);
+        sum_of_percents += facet.Percent();
+    }
+
+    // A simple average of regeneration and facets
+    // 4 facet shields assign less importance to regeneration
+    operational = sum_of_percents / (number_of_facets + 1);
+}
+
+void Shield::Repair() {
+    regeneration.RepairFully();
+
+    for (Resource<double> &facet : facets) {
+        facet.RepairFully();
+    }
+
+    operational = 1.0;
+}
+
+bool Shield::Damaged() const {
+    return Percent() < 1;
+}
+
+
+void Shield::Regenerate(const bool player_ship) {
+    //const bool apply_difficulty_shields = configuration()->physics_config.difficulty_based_shield_recharge;
+
+    // Discharge numbers for nicer, gradual discharge
+    //const float discharge_per_second = configuration()->physics_config.speeding_discharge;
+    //approx
+    //const float discharge_rate = (1 - (1 - discharge_per_second) * simulation_atom_var);
+    //const float min_shield_discharge = configuration()->physics_config.min_shield_speeding_discharge;
+    
+    // Some basic sanity checks first
+    // No point in all this code if there are no shields.
+    if (number_of_facets < 2 || TotalMaxLayerValue() == 0) {
+        return;
+    }
+
+    // No shields in SPEC
+    if (ftl_drive->Enabled() && !configuration()->physics_config.shields_in_spec) {
+        Decrease();
+        return;
+    }
+
+    // No shields while cloaked
+    if (cloak->Active()) {
+        Decrease();
+        return;
+    }
+
+    /* A discussion of consumption
+    * First, you set the consumption for 1 second (not atom_var)
+    * Then you consume and get back actual consumption
+    * Finally, you adjust whatever used it to the value in question 
+    */
+
+    // Shield Maintenance 
+    // TODO: lib_damage restore efficiency by replacing with shield->efficiency
+    //const double efficiency = 1;
+
+    const double shield_maintenance_cost = TotalMaxLayerValue() * configuration()->fuel.shield_maintenance_factor;
+    SetConsumption(shield_maintenance_cost);
+    const double actual_maintenance_percent = Consume();
+    if(Percent() > actual_maintenance_percent) {
+        Decrease();
+        return;
+    }
+
+    // Manually throttle shield strength
+    if(Percent() > max_power) {
+        Decrease();
+        return;
+    }
+
+    // Shield Regeneration
+    if(TotalLayerValue() == TotalMaxLayerValue()) {
+        // Fully charged. No need for more action or energy consumption
+        return;
+    }
+
+    const double shield_regeneration_cost = regeneration.AdjustedValue() * configuration()->fuel.shield_regeneration_factor;
+    SetConsumption(shield_regeneration_cost);
+    const double actual_regeneration_percent = Consume();
+    double regen = actual_regeneration_percent * regeneration.AdjustedValue() * simulation_atom_var;
+    
+    for (Resource<double> &facet : facets) {
+        facet += regen;
+    }
+
+    // Difficulty settings
+    // TODO: enable
+    //double difficulty_shields = 1.0;
+    // if (apply_difficulty_shields) {
+    //     difficulty_shields = g_game.difficulty;
+    // }
+
+    /*if (unit->GetNebula() != nullptr) {
+        shield_recharge *= nebshields;
+    }
+
+    // Adjust other (enemy) ships for difficulty
+    if (!player_ship) {
+        shield_recharge *= difficulty;
+    }*/
+
+    //const float nebshields = configuration()->physics_config.nebula_shield_recharge;
+}
+
+void Shield::AdjustPower(double percent) {
+    max_power = percent;
+}
+
+
+void Shield::FullyCharge() {
+    for (Resource<double> &facet : facets) {
+        facet.Set(facet.AdjustedValue());
+    }
+}
+
+void Shield::Decrease() {
+    for(auto& facet : facets) {
+        facet--;
+    }
+}
+
+void Shield::Zero() {
+    for(auto& facet : facets) {
+        facet = 0;
+    }
+}

engine/CMakeLists.txt
@@ -460,7 +460,6 @@ SET(LIBCONFIG
 SET(LIBDAMAGE
     src/damage/damageable_layer.cpp
     src/damage/damageable_object.cpp
-    src/damage/health.cpp
 )
 
 SET(LIBRESOURCE
@@ -485,6 +484,10 @@ SET(LIBCOMPONENT
     src/components/energy_consumer.cpp
     src/components/energy_container.cpp
     src/components/reactor.cpp
+
+    src/components/armor.cpp
+    src/components/hull.cpp
+    src/components/shield.cpp
 
     src/components/afterburner.cpp
     src/components/afterburner_upgrade.cpp
@@ -1479,7 +1482,6 @@ IF (USE_GTEST)
         src/cmd/tests/csv_tests.cpp
         src/cmd/tests/json_tests.cpp
         src/configuration/tests/configuration_tests.cpp
-        src/damage/tests/health_tests.cpp
         src/damage/tests/layer_tests.cpp
         src/damage/tests/object_tests.cpp
         src/resource/tests/buy_sell.cpp

engine/src/cmd/ai/aggressive.cpp
@@ -260,16 +260,16 @@ void AggressiveAI::SetParent(Unit *parent1) {
     last_directive = "b";     //prevent escort race condition
 
     //INIT stored stuff
-    Fshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->FShieldData();
+    Fshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::front);
     Fshield_rate_old = 0.0;
     Fshield_prev_time = UniverseUtil::GetGameTime();
-    Bshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->BShieldData();
+    Bshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::back);
     Bshield_rate_old = 0.0;
     Bshield_prev_time = UniverseUtil::GetGameTime();
-    Lshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->LShieldData();
+    Lshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::left);
     Lshield_rate_old = 0.0;
     Lshield_prev_time = UniverseUtil::GetGameTime();
-    Rshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->RShieldData();
+    Rshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::right);
     Rshield_rate_old = 0.0;
     Rshield_prev_time = UniverseUtil::GetGameTime();
     Farmour_prev = 1.0;
@@ -284,7 +284,7 @@ void AggressiveAI::SetParent(Unit *parent1) {
     Rarmour_prev = 1.0;
     Rarmour_rate_old = 0.0;
     Rarmour_prev_time = UniverseUtil::GetGameTime();
-    Hull_prev = parent->GetHullPercent();
+    Hull_prev = parent->hull.Percent();
     Hull_rate_old = 0.0;
     Hull_prev_time = UniverseUtil::GetGameTime();
 }
@@ -333,30 +333,31 @@ bool AggressiveAI::ProcessLogicItem(const AIEvents::AIEvresult &item) {
             value = parent->GetComputerData().threatlevel;
             break;
         case FSHIELD:
-            value = parent->ftl_drive.Enabled() ? 1 : parent->FShieldData();
+            value = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::front);
             break;
         case BSHIELD:
-            value = parent->ftl_drive.Enabled() ? 1 : parent->BShieldData();
+            value = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::back);
             break;
         case HULL: {
-            value = parent->GetHullPercent();
+            value = parent->hull.Percent();
             break;
         }
         case LSHIELD:
-            value = parent->ftl_drive.Enabled() ? 1 : parent->LShieldData();
+            value = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::left);
             break;
         case RSHIELD:
-            value = parent->ftl_drive.Enabled() ? 1 : parent->RShieldData();
+            value = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::right);
             break;
         case FSHIELD_HEAL_RATE: {
+            // TODO: refactor this. We don't need to repeat this code
             double delta_t = UniverseUtil::GetGameTime() - Fshield_prev_time;
             if (delta_t > 0.5) {
                 //0.5 = reaction time limit for hit rate
-                double delta_v = parent->ftl_drive.Enabled() ? 1 : parent->FShieldData() - Fshield_prev;
+                double delta_v = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::front) - Fshield_prev;
                 value = delta_v / delta_t;
                 Fshield_rate_old = value;
                 Fshield_prev_time = UniverseUtil::GetGameTime();
-                Fshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->FShieldData();
+                Fshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::front);
             } else {
                 value = Fshield_rate_old;
             }
@@ -366,11 +367,11 @@ bool AggressiveAI::ProcessLogicItem(const AIEvents::AIEvresult &item) {
             double delta_t = UniverseUtil::GetGameTime() - Bshield_prev_time;
             if (delta_t > 0.5) {
                 //0.5 = reaction time limit for hit rate
-                double delta_v = parent->ftl_drive.Enabled() ? 1 : parent->BShieldData() - Bshield_prev;
+                double delta_v = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::back) - Bshield_prev;
                 value = delta_v / delta_t;
                 Bshield_rate_old = value;
                 Bshield_prev_time = UniverseUtil::GetGameTime();
-                Bshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->BShieldData();
+                Bshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::back);
             } else {
                 value = Bshield_rate_old;
             }
@@ -380,11 +381,11 @@ bool AggressiveAI::ProcessLogicItem(const AIEvents::AIEvresult &item) {
             double delta_t = UniverseUtil::GetGameTime() - Lshield_prev_time;
             if (delta_t > 0.5) {
                 //0.5 = reaction time limit for hit rate
-                double delta_v = parent->ftl_drive.Enabled() ? 1 : parent->LShieldData() - Lshield_prev;
+                double delta_v = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::left) - Lshield_prev;
                 value = delta_v / delta_t;
                 Lshield_rate_old = value;
                 Lshield_prev_time = UniverseUtil::GetGameTime();
-                Lshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->LShieldData();
+                Lshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::left);
             } else {
                 value = Lshield_rate_old;
             }
@@ -394,11 +395,11 @@ bool AggressiveAI::ProcessLogicItem(const AIEvents::AIEvresult &item) {
             double delta_t = UniverseUtil::GetGameTime() - Rshield_prev_time;
             if (delta_t > 0.5) {
                 //0.5 = reaction time limit for hit rate
-                double delta_v = parent->ftl_drive.Enabled() ? 1 : parent->RShieldData() - Rshield_prev;
+                double delta_v = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::right) - Rshield_prev;
                 value = delta_v / delta_t;
                 Rshield_rate_old = value;
                 Rshield_prev_time = UniverseUtil::GetGameTime();
-                Rshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->RShieldData();
+                Rshield_prev = parent->ftl_drive.Enabled() ? 1 : parent->shield.Percent(Shield::right);
             } else {
                 value = Rshield_rate_old;
             }
@@ -420,11 +421,11 @@ bool AggressiveAI::ProcessLogicItem(const AIEvents::AIEvresult &item) {
             double delta_t = UniverseUtil::GetGameTime() - Hull_prev_time;
             if (delta_t > 0.5) {
                 //0.5 = reaction time limit for hit rate
-                double delta_v = parent->GetHullPercent() - Hull_prev;
+                double delta_v = parent->hull.Percent() - Hull_prev;
                 value = delta_v / delta_t;
                 Hull_rate_old = value;
                 Hull_prev_time = UniverseUtil::GetGameTime();
-                Hull_prev = parent->GetHullPercent();
+                Hull_prev = parent->hull.Percent();
             } else {
                 value = Hull_rate_old;
             }
@@ -1190,7 +1191,7 @@ void AggressiveAI::ReCommandWing(Flightgroup *fg) {
             //computer won't override capital orders
             if (nullptr != (lead = fg->leader.GetUnit())) {
                 if (float ( rand())/RAND_MAX < simulation_atom_var / time_to_recommand_wing) {
-                    if (parent->Threat() && (parent->FShieldData() < .2 || parent->RShieldData() < .2)) {
+                    if (parent->Threat() && (parent->shield.Percent() < .2)) {
                         fg->directive = string("h");
                         LeadMe(parent, "h", "I need help here!", false);
                         if (verbose_debug) {

commit 7e1414e9fb711e07f2e948b52e430acfd8485286

Fix shield maintenance draining the capacitor at full shields
Regression from ca3df7f9c (Feb 2025, PR #1016): Shield::Regenerate charged
max_shield x maintenance_factor (5%/s) from the primary capacitor every atom,
even when shields were at full health. That made the capacitor unable to
refill after combat (stock Llama: 600x0.05=30/s maintenance == reactor 30/s,
so net 0; any ship with a big shield upgrade drains, e.g. Vendetta 4000 ->
200/s vs reactor 115/s).

Fix per Evert's principle: shields consume power only while building, and
the recharge rate is throttled by what the reactor can actually supply.

- Fully-charged gate moved to the top of Regenerate, before any consumption.
- Recharge cost now scales with the shield deficit being rebuilt
  ((max - current) x factor) instead of a flat always-on maintenance drain.
- Consume() returns the fraction of the cost the reactor supplied; regen runs
  at that fraction, so half the energy means half the recharge rate (twice
  as long to fill).
- The max_power throttle check runs before the full-shields gate so a
  throttled shield still decreases toward its power limit.
- Removed the unaffordable-maintenance Decrease() collapse (shields now slow
  their recharge instead of decaying when energy is short).

Fixes the reported 'capacitor never refills after firing' behavior.
See https://github.com/vegastrike/Vega-Strike-Engine-Source

engine/src/components/shield.cpp
@@ -340,34 +340,35 @@ void Shield::Regenerate(const bool player_ship) {
     * Finally, you adjust whatever used it to the value in question
     */
 
-    // Shield Maintenance
-    // TODO: lib_damage restore efficiency by replacing with shield->efficiency
-    //const double efficiency = 1;
-
-    const double shield_maintenance_cost = TotalMaxLayerValue() * configuration().components.shield.maintenance_factor_dbl;
-    SetConsumption(shield_maintenance_cost);
-    const double actual_maintenance_percent = Consume();
-    if(Percent() > actual_maintenance_percent) {
-        Decrease();
-        return;
-    }
-
-    // Manually throttle shield strength
+    // Manually throttle shield strength (must run before the fully-charged
+    // gate so a throttled shield still decreases toward its power limit)
     if(Percent() > max_power) {
         Decrease();
         return;
     }
 
-    // Shield Regeneration
+    // Fully charged shields need no energy - return before any consumption.
+    // (Regression fix: the maintenance drain below used to run even at full
+    // shields, so the capacitor could never refill after combat. See
+    // https://github.com/vegastrike/Vega-Strike-Engine-Source - shield
+    // maintenance drain bug; old pre-2025 model only spent energy while
+    // shields were actually recharging.)
     if(TotalLayerValue() == TotalMaxLayerValue()) {
-        // Fully charged. No need for more action or energy consumption
         return;
     }
 
-    const double shield_regeneration_cost = regeneration.AdjustedValue() * configuration().components.shield.regeneration_factor_dbl;
-    SetConsumption(shield_regeneration_cost);
-    const double actual_regeneration_percent = Consume();
-    double regen = actual_regeneration_percent * regeneration.AdjustedValue() * simulation_atom_var;
+    /* Shield Regeneration - energy-limited by what the reactor can supply.
+    * The energy cost scales with the shield deficit being rebuilt, so a
+    * heavily damaged shield demands proportionally more power. If the
+    * reactor can't supply the full cost, Consume() returns the fraction it
+    * could actually provide and regen slows by that same fraction - e.g.
+    * half the energy -> half the recharge rate -> twice as long to fill
+    * (matches the pre-2025 behavior).
+    */
+    const double shield_recharge_cost = (TotalMaxLayerValue() - TotalLayerValue()) * configuration().components.shield.maintenance_factor_dbl;
+    SetConsumption(shield_recharge_cost);
+    const double actual_recharge_percent = Consume();
+    double regen = actual_recharge_percent * regeneration.AdjustedValue() * simulation_atom_var;
 
     for (Resource<double> &facet : facets) {
         facet += regen;