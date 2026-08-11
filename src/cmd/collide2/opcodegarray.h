74af4631e7f68b6d25f3c88dde3a7fc1ea780e30
Evert Vorster <evorster@gmail.com>
2026-08-11 14:12:29 +0200

    Fix: replace raw realloc/memmove/memcpy in CS growable array with proper C++ ops
    The Crystal Space growable-array macro (opcodegarray.h) used realloc/memmove/
    memcpy on csCollisionPair (which contains Opcode::Point, a class with a
    user-defined ctor) — -Wclass-memaccess warnings and technically unsafe for
    non-trivial types.
    
    Rewrote the memory ops properly:
    - SetLimit: realloc/free -> new[]/delete[] + std::copy (preserving existing
      elements; SetLength now resizes before updating length so the copy count
      is correct)
    - Delete: memmove -> std::copy
    - Push: memcpy -> copy assignment
    - Insert: memmove+memcpy -> std::copy_backward + assignment
    - Added <algorithm> and <new> includes
    
    Behavior-preserving for the only instantiation (pairs of csCollisionPair,
    trivially-copyable POD). Eliminates 5 -Wclass-memaccess warnings.

--- src/cmd/collide2/opcodegarray.h
+++ src/cmd/collide2/opcodegarray.h
@@ -19,6 +19,8 @@
 
 #ifndef __CS_GARRAY_H__
 #define __CS_GARRAY_H__
+#include <algorithm>
+#include <new>
 
 // Common macro for declarations below
 #define CS_TYPEDEF_GROWING_ARRAY_EXT(Name, Type, ExtraConstructor, Extra) \
@@ -35,9 +37,18 @@
     {									\
       if (limit == iLimit) return;					\
       if ((limit = iLimit)!=0)						\
-        root = (ga_type *)realloc (root, limit * sizeof (ga_type));	\
+        { \
+        	int oldlength = length < limit ? length : limit;	\
+        	ga_type *newroot = new ga_type[limit]; \
+        	if (root) \
+        	{ \
+        		std::copy (root, root + oldlength, newroot);	\
+        		delete[] root; \
+        	} \
+        	root = newroot; \
+        } \
       else								\
-      { if (root) { free (root); root = NULL; } }			\
+      { if (root) { delete[] root; root = NULL; } }			\
     }									\
     Name ()								\
     { limit = length = 0; root = NULL; ExtraConstructor; }		\
@@ -46,11 +57,11 @@
     int Length () const							\
     { return length; }							\
     void SetLength (int iLength, int iGrowStep = 8)			\
-    {									\
-      length = iLength;							\
-      int newlimit = ((length + (iGrowStep - 1)) / iGrowStep) * iGrowStep;\
-      if (newlimit != limit) SetLimit (newlimit);			\
-    }									\
+	{ \
+	  int newlimit = ((iLength + (iGrowStep - 1)) / iGrowStep) * iGrowStep; \
+	  if (newlimit != limit) SetLimit (newlimit); \
+	  length = iLength; \
+	} \
     ga_type &operator [] (int n)					\
     { CS_ASSERT (n >= 0 && n < limit); return root [n]; }		\
     const ga_type &operator [] (int n) const				\
@@ -59,58 +70,58 @@
     { CS_ASSERT (n >= 0 && n < limit); return root [n]; }		\
     void Delete (int n)							\
     { CS_ASSERT (n >= 0 && n < limit);					\
-      memmove (root + n, root + n + 1, (limit - n - 1) * sizeof (ga_type)); \
+      std::copy (root + n + 1, root + limit, root + n); \
       SetLength (length-1); }						\
     ga_type *GetArray ()						\
     { return root; }							\
     int Push (const ga_type &val, int iGrowStep = 8)			\
     {									\
       SetLength (length + 1, iGrowStep);				\
-      memcpy (root + length - 1, &val, sizeof (ga_type));		\
+      root [length - 1] = val;		\
       return length-1;							\
     }									\
     void Insert (int pos, const ga_type &val, int iGrowStep = 8)	\
     {									\
       CS_ASSERT (pos>=0 && pos<=length);				\
       SetLength (length + 1, iGrowStep);				\
-      memmove (root+pos+1, root+pos, sizeof(ga_type) * (length-pos-1)); \
-      memcpy (root + pos, &val, sizeof (ga_type));			\
+      std::copy_backward (root + pos, root + length - 1, root + length); \
+      root [pos] = val;			\
     }									\
     Extra								\
-  }
+  } \
 
 /**
- * This is a macro that will declare a growable array variable that is able to 
- * contain a number of elements of given type.<p>
- * Methods:
- * <ul>
- * <li>void SetLimit (int) - set max number of values the array can hold
- * <li>int Limit () - query max number of values the array can hold
- * <li>void SetLength (int) - set the amount of elements that are actually used
- * <li>int Length () - query the amount of elements that are actually used
- * <li>operator [] (int) - return a reference to Nth element of the array
- * </ul>
- * Usage examples:
- * <pre>
- * CS_TYPEDEF_GROWING_ARRAY (csLightArray, csLight*);
- * CS_TYPEDEF_GROWING_ARRAY (csIntArray, int);
- * static csLightArray la;
- * static csIntArray ia;
- * </pre>
+ * This is a macro that will declare a growable array variable that is able to \
+ * contain a number of elements of given type.<p> \
+ * Methods: \
+ * <ul> \
+ * <li>void SetLimit (int) - set max number of values the array can hold \
+ * <li>int Limit () - query max number of values the array can hold \
+ * <li>void SetLength (int) - set the amount of elements that are actually used \
+ * <li>int Length () - query the amount of elements that are actually used \
+ * <li>operator [] (int) - return a reference to Nth element of the array \
+ * </ul> \
+ * Usage examples: \
+ * <pre> \
+ * CS_TYPEDEF_GROWING_ARRAY (csLightArray, csLight*); \
+ * CS_TYPEDEF_GROWING_ARRAY (csIntArray, int); \
+ * static csLightArray la; \
+ * static csIntArray ia; \
+ * </pre> \
  */
 #define CS_TYPEDEF_GROWING_ARRAY(Name, Type)				\
-  CS_TYPEDEF_GROWING_ARRAY_EXT (Name, Type, ;,  ;)
+  CS_TYPEDEF_GROWING_ARRAY_EXT (Name, Type, ;,  ;) \
 
 /**
- * Same as TYPEDEF_GROWING_ARRAY but contains additionally an reference
- * counter, so that the object can be shared among different clients.
- * If you do an IncRef each time you make use of it and an DecRef when you're 
- * done, the array will be automatically freed when there are no more 
- * references to it.<p> 
- * Methods:
- * <ul>
- * <li>void IncRef ()/void DecRef () - Reference counter management
- * </ul>
+ * Same as TYPEDEF_GROWING_ARRAY but contains additionally an reference \
+ * counter, so that the object can be shared among different clients. \
+ * If you do an IncRef each time you make use of it and an DecRef when you're \
+ * done, the array will be automatically freed when there are no more \
+ * references to it.<p> \
+ * Methods: \
+ * <ul> \
+ * <li>void IncRef ()/void DecRef () - Reference counter management \
+ * </ul> \
  */
 #define CS_TYPEDEF_GROWING_ARRAY_REF(Name, Type)			\
   CS_TYPEDEF_GROWING_ARRAY_EXT (Name, Type, RefCount = 0,		\
@@ -121,25 +132,25 @@
     {									\
       if (RefCount == 1) SetLimit (0);					\
       RefCount--;							\
-    })
+    }) \
 
 /**
- * This is a shortcut for above to declare a dummy class and a single
- * instance of that class.
- * <p>
- * Usage examples:
- * <pre>
- * CS_DECLARE_GROWING_ARRAY (la, csLight*);
- * CS_DECLARE_GROWING_ARRAY (ia, int);
- * </pre>
+ * This is a shortcut for above to declare a dummy class and a single \
+ * instance of that class. \
+ * <p> \
+ * Usage examples: \
+ * <pre> \
+ * CS_DECLARE_GROWING_ARRAY (la, csLight*); \
+ * CS_DECLARE_GROWING_ARRAY (ia, int); \
+ * </pre> \
  */
 #define CS_DECLARE_GROWING_ARRAY(Name, Type)				\
-  CS_TYPEDEF_GROWING_ARRAY(__##Name##_##Type,Type) Name
+  CS_TYPEDEF_GROWING_ARRAY(__##Name##_##Type,Type) Name \
 
 /**
- * Same as above but declares an object which has a reference counter.
+ * Same as above but declares an object which has a reference counter. \
  */
 #define CS_DECLARE_GROWING_ARRAY_REF(Name, Type)			\
-  CS_TYPEDEF_GROWING_ARRAY_REF(__##Name,Type) Name
+  CS_TYPEDEF_GROWING_ARRAY_REF(__##Name,Type) Name \
 
 #endif // __CS_GARRAY_H__
