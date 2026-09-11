01e5ace6f245b9b4ee9e03582ef4c821e3cc96f0
Evert Vorster <evorster@gmail.com>
2026-09-11 14:14:30 +0200

    vega_draw: credit Evert Vorster in the new files' headers
    The vega_draw library, its tests and the base-text bridge are new files
    added by this branch; add Evert Vorster to the current-development-team
    line in each. Only files added by this branch are touched.

--- engine/src/gui/vega_text.h
+++ engine/src/gui/vega_text.h
@@ -5,7 +5,7 @@
  * Copyright (C) 2001-2026 The Vega Strike Contributors:
  * Project creator: Daniel Horn
  * Original development team: As listed in the AUTHORS file.
- * Current development team: Roy Falk, Benjamen R. Meyer, Stephen G. Tuggy
+ * Current development team: Roy Falk, Benjamen R. Meyer, Stephen G. Tuggy, Evert Vorster
  *
  * https://github.com/vegastrike/Vega-Strike-Engine-Source
  *
